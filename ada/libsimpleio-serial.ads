609b1a994dd6ffce00ff5b989a0c3ea76d78b05a
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2016-03-04 18:53:30 +0000

    Added support for serial ports
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@10109 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/libsimpleio-serial.ads
@@ -0,0 +1,61 @@
+-- Minimal Ada wrapper for the Linux serial port services
+-- implemented in libsimpleio.so
+
+-- $Id$
+
+-- Copyright (C)2016, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+PACKAGE libsimpleio.Serial IS
+
+  PARITY_NONE : CONSTANT := 0;
+  PARITY_EVEN : CONSTANT := 1;
+  PARITY_ODD  : CONSTANT := 2;
+
+  PROCEDURE Open
+   (devname  : String;
+    baudrate : Integer;
+    parity   : Integer;
+    databits : Integer;
+    stopbits : Integer;
+    fd       : OUT Integer;
+    error    : OUT Integer);
+  PRAGMA Import(C, Open, "SERIAL_open");
+
+  PROCEDURE Close
+   (fd       : Integer;
+    error    : OUT Integer);
+  PRAGMA Import(C, Close, "SERIAL_close");
+
+  PROCEDURE Send
+   (fd       : Integer;
+    buf      : System.Address;
+    size     : Integer;
+    error    : OUT Integer);
+  PRAGMA Import(C, GetInfo, "SERIAL_send");
+
+  PROCEDURE Receive
+   (fd       : Integer;
+    buf      : System.Address;
+    size     : IN OUT Integer;
+    error    : OUT Integer);
+  PRAGMA Import(C, GetInfo, "SERIAL_receive");
+
+END libsimpleio.HIDRaw;
