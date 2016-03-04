b3230379a93369675f5f21d1362d54c31e341d38
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2016-03-04 12:30:03 +0000

    The libsimpleio parent package degenerates to nothing, for now.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@10102 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/libsimpleio.ads
+++ ada/libsimpleio.ads
@@ -22,8 +22,5 @@
 -- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
-WITH Interfaces;   USE Interfaces;
-WITH errno;        USE errno;
-
 PACKAGE libsimpleio IS
 END libsimpleio;
