69862ed5779552fad52a48cb6251742e40c33937
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2016-11-15 08:37:37 +0000

    Added abstract interface definitions for PWM and servo outputs.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@11493 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ c++/linx/pwm.h
@@ -0,0 +1,43 @@
+// LabView LINX Remote I/O Protocol
+// Pulse Width Modulation output abstract interface module
+
+// Copyright (C)2016, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef _PWM_INTERFACE_H
+#define _PWM_INTERFACE_H
+
+#include <stdint.h>
+
+class PWM_Interface
+{
+  public:
+    virtual void set_frequency(uint32_t frequency, int32_t *error) = 0;
+
+    virtual void write(uint8_t dutycycle, int32_t *error) = 0;
+};
+
+typedef PWM_Interface *PWM_Interface_Ptr;
+
+extern void pwm_add_channel(uint8_t number, PWM_Interface_Ptr object);
+
+extern void pwm_init(void);
+
+#endif
