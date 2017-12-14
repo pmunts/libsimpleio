f6d26e27de9f5cd3cb39eb714bee347fd096938a
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2017-12-14 14:54:23 +0000

    Added documentation comments.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@13909 06ee2f96-5407-11de-8bab-f3d7589a4122

--- csharp/libGPIO.cs
+++ csharp/libGPIO.cs
@@ -29,31 +29,92 @@ namespace libsimpleio
     /// </summary>
     public class libGPIO
     {
+        /// <summary>
+        /// Input data direction.
+        /// </summary>
         public const int DIRECTION_INPUT = 0;
+        /// <summary>
+        /// Out data direction.
+        /// </summary>
         public const int DIRECTION_OUTPUT = 1;
 
+        /// <summary>
+        /// Interrupts are disabled.
+        /// </summary>
         public const int EDGE_NONE = 0;
+        /// <summary>
+        /// Interrupt on rising edge.
+        /// </summary>
         public const int EDGE_RISING = 1;
+        /// <summary>
+        /// Interrupt on falling edge.
+        /// </summary>
         public const int EDGE_FALLING = 2;
+        /// <summary>
+        /// Interrupt on both edges.
+        /// </summary>
         public const int EDGE_BOTH = 3;
 
+        /// <summary>
+        /// Active low (inverted) polarity.
+        /// </summary>
         public const int POLARITY_ACTIVELOW = 0;
+        /// <summary>
+        /// Active high (normal) polarity.
+        /// </summary>
         public const int POLARITY_ACTIVEHIGH = 1;
 
+        /// <summary>
+        /// Configure a Linux GPIO pin.
+        /// </summary>
+        /// <param name="pin">Pin number.</param>
+        /// <param name="direction">Data direction.</param>
+        /// <param name="state">Initial state for output pin.</param>
+        /// <param name="edge">Interrupt edge for input pin.</param>
+        /// <param name="polarity">Polarity</param>
+        /// <param name="error">Error code.  Zero upon success or an <code>errno</code>
+        /// value upon failure.</param>
         [DllImport("simpleio")]
         public static extern void GPIO_configure(int pin, int direction,
           int state, int edge, int polarity, out int error);
 
+        /// <summary>
+        /// Open a Linux GPIO pin device.
+        /// </summary>
+        /// <param name="pin">Pin number.</param>
+        /// <param name="fd">File descriptor.</param>
+        /// <param name="error">Error code.  Zero upon success or an <code>errno</code>
+        /// value upon failure.</param>
         [DllImport("simpleio")]
         public static extern void GPIO_open(int pin, out int fd, out int error);
 
+        /// <summary>
+        /// Read a Linux GPIO pin.
+        /// </summary>
+        /// <param name="fd">File descriptor.</param>
+        /// <param name="state">Pin state.</param>
+        /// <param name="error">Error code.  Zero upon success or an <code>errno</code>
+        /// value upon failure.</param>
         [DllImport("simpleio")]
         public static extern void GPIO_read(int fd, out int state,
             out int error);
 
+        /// <summary>
+        /// Write a Linux GPIO pin.
+        /// </summary>
+        /// <param name="fd">File descriptor.</param>
+        /// <param name="state">Pin state.</param>
+        /// <param name="error">Error code.  Zero upon success or an <code>errno</code>
+        /// value upon failure.</param>
         [DllImport("simpleio")]
         public static extern void GPIO_write(int fd, int state, out int error);
 
+        /// <summary>
+        /// Close a Linux GPIO pin device.
+        /// </summary>
+        /// <param name="fd">File descriptor.</param>
+        /// <param name="error">Error code.  Zero upon success or an <code>errno</code>
+        /// value upon failure.</param>
         [DllImport("simpleio")]
         public static extern void GPIO_close(int fd, out int error);
     }
