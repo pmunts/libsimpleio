48060b6e632b12499a048a32deb2456a30c2470e
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-01-04 07:36:47 +0000

    Folded objects/i2c.device.cs into interfaces/i2c.cs
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14003 06ee2f96-5407-11de-8bab-f3d7589a4122

--- csharp/interfaces/i2c.cs
+++ csharp/interfaces/i2c.cs
@@ -53,4 +53,61 @@ namespace IO.Interfaces.I2C
         /// <param name="resplen">Number of bytes to read.</param>
         void Transaction(int slaveaddr, byte[] cmd, int cmdlen, byte[] resp, int resplen);
     }
+
+    /// <summary>
+    /// Encapsulates I<sup>2</sup>C slave devices.
+    /// </summary>
+    public class Device
+    {
+        private Bus bus;
+        private int addr;
+
+        /// <summary>
+        /// Create an I<sup>2</sup>C slave device.
+        /// </summary>
+        /// <param name="bus">I<sup>2</sup>C bus controller object.</param>
+        /// <param name="slaveaddr">I<sup>2</sup>C slave address.</param>
+        public Device(Bus bus, int slaveaddr)
+        {
+            if ((slaveaddr < 0) || (slaveaddr > 127))
+            {
+                throw new System.Exception("Invalid slave address");
+            }
+
+            this.bus = bus;
+            this.addr = slaveaddr;
+        }
+
+        /// <summary>
+        /// Read bytes from an I<sup>2</sup>C slave device.
+        /// </summary>
+        /// <param name="resp">Response buffer.</param>
+        /// <param name="resplen">Number of bytes to read.</param>
+        public void Read(byte[] resp, int resplen)
+        {
+            this.bus.Read(this.addr, resp, resplen);
+        }
+
+        /// <summary>
+        /// Write bytes to an I<sup>2</sup>C slave device.
+        /// </summary>
+        /// <param name="cmd">Command buffer.</param>
+        /// <param name="cmdlen">Number of bytes to write.</param>
+        public void Write(byte[] cmd, int cmdlen)
+        {
+            this.bus.Write(this.addr, cmd, cmdlen);
+        }
+
+        /// <summary>
+        /// Write and read bytes to and from an I<sup>2</sup>C slave device.
+        /// </summary>
+        /// <param name="cmd">Command buffer.</param>
+        /// <param name="cmdlen">Number of bytes to write.</param>
+        /// <param name="resp">Response buffer.</param>
+        /// <param name="resplen">Number of bytes to read.</param>
+        public void Transaction(byte[] cmd, int cmdlen, byte[] resp, int resplen)
+        {
+            this.bus.Transaction(this.addr, cmd, cmdlen, resp, resplen);
+        }
+    }
 }
