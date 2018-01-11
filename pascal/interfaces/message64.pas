31b91c9a67791c8d2fd5702227e082300af45be3
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-01-11 09:14:35 +0000

    Updated copyright year.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14039 06ee2f96-5407-11de-8bab-f3d7589a4122

--- pascal/interfaces/message64.pas
+++ pascal/interfaces/message64.pas
@@ -1,6 +1,6 @@
 { Abstract interface for sending and receiving 64-byte messages               }
 
-{ Copyright (C)2017, Philip Munts, President, Munts AM Corp.                  }
+{ Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.             }
 {                                                                             }
 { Redistribution and use in source and binary forms, with or without          }
 { modification, are permitted provided that the following conditions are met: }
