68c2379f536d114008d9e3dae569eb72a78b50c2
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-01-11 08:56:13 +0000

    Updated the copyright year.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14037 06ee2f96-5407-11de-8bab-f3d7589a4122

--- csharp/objects/watchdog.cs
+++ csharp/objects/watchdog.cs
@@ -1,6 +1,6 @@
 // Watchdog timer services using libsimpleio
 
-// Copyright (C)2017, Philip Munts, President, Munts AM Corp.
+// Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
