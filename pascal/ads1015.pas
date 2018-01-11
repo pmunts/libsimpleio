ad481183f0b262af42ddd906fe4f03bef2411e9e
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-01-11 12:35:25 +0000

    Moved to libsimpleio/pascal/
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14044 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ pascal/ads1015.pas
@@ -0,0 +1,195 @@
+{ Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.             }
+{                                                                             }
+{ Redistribution and use in source and binary forms, with or without          }
+{ modification, are permitted provided that the following conditions are met: }
+{                                                                             }
+{ * Redistributions of source code must retain the above copyright notice,    }
+{   this list of conditions and the following disclaimer.                     }
+{                                                                             }
+{ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" }
+{ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   }
+{ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  }
+{ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   }
+{ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         }
+{ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        }
+{ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    }
+{ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     }
+{ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     }
+{ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
+{ POSSIBILITY OF SUCH DAMAGE.                                                 }
+
+UNIT ADS1015;
+
+INTERFACE
+
+  USES
+    ADC,
+    I2C,
+    Voltage;
+
+  { Intrinsic characteristics of the ADS1015 A/D converter }
+
+  CONST
+    MaxChannels = 4;
+    Resolution  = 12;
+    Steps       = 4096;
+
+  TYPE
+
+    { Input channel selectors }
+
+    Channels = (AIN0, AIN1, AIN2, AIN3, DIFF01, DIFF03, DIFF13, DIFF23);
+
+    { Full scale range (in millivolts) selectors }
+
+    Ranges   = (FSR6144, FSR4096, FSR2048, FSR1024, FSR512, FSR256);
+
+    { ADS1015 device class }
+
+    Device = CLASS
+      CONSTRUCTOR Create
+       (bus  : I2C.Bus;
+        addr : I2C.Address);
+    PRIVATE
+      bus  : I2C.Bus;
+      addr : I2C.Address;
+
+      { Private methods }
+
+      FUNCTION ReadRegister(regaddr : Byte) : Word;
+
+      PROCEDURE WriteRegister(regaddr : Byte; regdata : Word);
+    END;
+
+    { ADS1015 analog input class }
+
+    InputSubclass = CLASS(TInterfacedObject, ADCInput, VoltageInput)
+      CONSTRUCTOR Create
+       (dev     : Device;
+        channel : Channels;
+        range   : Ranges;
+        gain    : Real = 1.0;
+        offset  : Real = 0.0);
+
+      { Public methods }
+
+      FUNCTION Read : Integer;
+
+      FUNCTION ReadVoltage : Real;
+    PRIVATE
+      dev     : Device;
+      channel : Cardinal;
+      range   : Cardinal;
+      gain    : Real;
+      offset  : Real;
+    END;
+
+IMPLEMENTATION
+
+  { ADS1015 register addresses }
+
+  CONST
+    CONVERSION     = 0;
+    CONFIG         = 1;
+    LOW_THRESHOLD  = 2;
+    HIGH_THRESHOLD = 3;
+
+  { ADS1015 device constructor }
+
+  CONSTRUCTOR Device.Create
+   (bus  : I2C.Bus;
+    addr : I2C.Address);
+
+  BEGIN
+    Self.bus := Bus;
+    Self.addr := addr;
+  END;
+
+  { ADS1015 device read register method }
+
+  FUNCTION Device.ReadRegister(regaddr : Byte) : Word;
+
+  VAR
+    cmd  : ARRAY [0 .. 0] OF Byte;
+    resp : ARRAY [0 .. 1] OF Byte;
+
+  BEGIN
+    IF regaddr > HIGH_THRESHOLD THEN
+      RAISE ADC_Error.create('ERROR: Invalid register address');
+
+    cmd[0] := regaddr;
+    Self.bus.Transaction(Self.addr, cmd, SizeOf(cmd), resp, SizeOf(resp));
+    ReadRegister := (resp[0] SHL 8) + resp[1];
+  END;
+
+  { ADS1015 device write register method }
+
+  PROCEDURE Device.WriteRegister(regaddr : Byte; regdata : Word);
+
+  VAR
+    cmd : ARRAY [0 .. 2] OF Byte;
+
+  BEGIN
+    IF regaddr > HIGH_THRESHOLD THEN
+      RAISE ADC_Error.create('ERROR: Invalid register address');
+
+    cmd[0] := regaddr;
+    cmd[1] := regdata DIV 256;
+    cmd[2] := regdata MOD 256;
+
+    Self.bus.Write(Self.addr, cmd, SizeOf(cmd));
+  END;
+
+  { ADS1015 analog input constructor }
+
+  CONSTRUCTOR InputSubclass.Create
+   (dev     : Device;
+    channel : Channels;
+    range   : Ranges;
+    gain    : Real;
+    offset  : Real);
+
+  BEGIN
+    Self.dev := dev;
+    IF (channel >= AIN0) AND (channel <= AIN3) THEN
+      Self.channel := Ord(channel) + 4
+    ELSE
+      Self.channel := Ord(channel) - 4;
+    Self.range := Ord(range);
+    Self.gain := gain;
+    Self.offset := offset;
+  END;
+
+  { Method implementing ADC.ADCInput.Read }
+
+  FUNCTION InputSubclass.Read : Integer;
+
+  BEGIN
+    Read := 1234;
+
+    { Start conversion }
+
+    Self.dev.WriteRegister(CONFIG, (1 SHL 15) + (Self.channel SHL 12) +
+      (Ord(Self.range) SHL 9) + (1 SHL 8));
+
+    { Wait until conversion complete }
+
+    REPEAT
+    UNTIL (Self.dev.ReadRegister(CONFIG) AND $8000) <> 0;
+
+    Read := SmallInt(Self.dev.ReadRegister(CONVERSION) SHR 4);
+  END;
+
+  { Method implementing Voltage.VoltageInput.ReadVoltage }
+
+  FUNCTION InputSubclass.ReadVoltage : Real;
+
+  CONST
+    RangeValues : ARRAY [0 .. 5] OF Real =
+     (6.144, 4.096, 2.048, 1.024, 0.512, 0.256);
+
+  BEGIN
+    ReadVoltage := Self.Read*RangeValues[Self.range]/Steps*2.0/gain - offset;
+  END;
+
+END.
---
+++ pascal/remoteio.pas
@@ -0,0 +1,152 @@
+{ Remote I/O Protocol Implementation                                          }
+
+{ Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.             }
+{                                                                             }
+{ Redistribution and use in source and binary forms, with or without          }
+{ modification, are permitted provided that the following conditions are met: }
+{                                                                             }
+{ * Redistributions of source code must retain the above copyright notice,    }
+{   this list of conditions and the following disclaimer.                     }
+{                                                                             }
+{ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" }
+{ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   }
+{ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  }
+{ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   }
+{ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         }
+{ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        }
+{ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    }
+{ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     }
+{ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     }
+{ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
+{ POSSIBILITY OF SUCH DAMAGE.                                                 }
+
+UNIT RemoteIO;
+
+INTERFACE
+
+  USES
+    Message64,
+    SysUtils;
+
+  TYPE
+    RemoteIO_Error = CLASS(Exception);
+
+    MessageTypes =
+     (LOOPBACK_REQUEST,
+      LOOPBACK_RESPONSE,
+      VERSION_REQUEST,
+      VERSION_RESPONSE,
+      CAPABILITY_REQUEST,
+      CAPABILITY_RESPONSE,
+      GPIO_PRESENT_REQUEST,
+      GPIO_PRESENT_RESPONSE,
+      GPIO_CONFIGURE_REQUEST,
+      GPIO_CONFIGURE_RESPONSE,
+      GPIO_READ_REQUEST,
+      GPIO_READ_RESPONSE,
+      GPIO_WRITE_REQUEST,
+      GPIO_WRITE_RESPONSE,
+      I2C_PRESENT_REQUEST,
+      I2C_PRESENT_RESPONSE,
+      I2C_CONFIGURE_REQUEST,
+      I2C_CONFIGURE_RESPONSE,
+      I2C_TRANSACTION_REQUEST,
+      I2C_TRANSACTION_RESPONSE,
+      SPI_PRESENT_REQUEST,
+      SPI_PRESENT_RESPONSE,
+      SPI_CONFIGURE_REQUEST,
+      SPI_CONFIGURE_RESPONSE,
+      SPI_TRANSACTION_REQUEST,
+      SPI_TRANSACTION_RESPONSE);
+
+    Device = CLASS
+      CONSTRUCTOR Create(m : IMessenger);
+
+      PROCEDURE Transaction(cmd : Message; VAR resp : Message);
+
+      FUNCTION Version : String;
+
+      FUNCTION Capability : String;
+    PRIVATE
+      msg : IMessenger;
+    END;
+
+IMPLEMENTATION
+
+  USES
+    errno;
+
+  { Create a Remote I/O Protocol device object }
+
+  CONSTRUCTOR Device.Create(m : IMessenger);
+
+  BEGIN
+    Self.msg := m;
+  END;
+
+  { Perform a Remote I/O Protocol operation }
+
+  PROCEDURE Device.Transaction(cmd : Message; VAR resp : Message);
+
+  BEGIN
+    Self.msg.Transaction(cmd, resp);
+
+    IF resp[0] <> cmd[0] + 1 THEN
+      RAISE RemoteIO_Error.create('ERROR: Incorrect response message type');
+
+    IF resp[1] <> cmd[1] THEN
+      RAISE RemoteIO_Error.create('ERROR: Incorrect response message number');
+
+    IF resp[2] <> EOK THEN
+      RAISE RemoteIO_Error.create('ERROR: Command failed, ' + strerror(resp[2]));
+  END;
+
+  { Fetch the Remote I/O Protocol device version string }
+
+  FUNCTION Device.Version : String;
+
+  VAR
+    cmd  : Message;
+    resp : Message;
+    vers : ARRAY [0 .. 60] OF Char;
+    i    : Integer;
+
+  BEGIN
+    FillChar(cmd, SizeOf(cmd), 0);
+
+    cmd[0] := Ord(VERSION_REQUEST);
+    cmd[1] := 1;
+
+    Self.Transaction(cmd, resp);
+
+    FOR i := 0 TO 60 DO
+      vers[i] := Char(resp[3 + i]);
+
+    Version := vers;
+  END;
+
+  { Fetch the Remote I/O Protocol device capability string }
+
+  FUNCTION Device.Capability : String;
+
+  VAR
+    cmd  : Message;
+    resp : Message;
+    caps : ARRAY [0 .. 60] OF Char;
+    i    : Integer;
+
+  BEGIN
+    FillChar(cmd, SizeOf(cmd), 0);
+
+    cmd[0] := Ord(CAPABILITY_REQUEST);
+    cmd[1] := 1;
+
+    Self.Transaction(cmd, resp);
+
+    FOR i := 0 TO 60 DO
+      caps[i] := Char(resp[3 + i]);
+
+    Capability := caps;
+  END;
+
+END.
