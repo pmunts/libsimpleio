6f7e64bd1b99eb247862417a986d6890a2073f23
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-01-16 09:00:53 +0000

    Corrected copyright year.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14071 06ee2f96-5407-11de-8bab-f3d7589a4122

--- c++/interfaces/pwm-interface.cpp
+++ c++/interfaces/pwm-interface.cpp
@@ -1,6 +1,6 @@
 // Abstract interface for PWM (Pulse Width Modulated) outputs
 
-// Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
