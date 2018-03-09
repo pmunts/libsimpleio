eb043d4074f2de2802c2c8918fa42799af6039ef
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-03-09 12:33:55 +0000

    Added an interface for variable speed motors.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14377 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ pascal/interfaces/motor.pas
@@ -0,0 +1,56 @@
+{ Abstract interface for variable speed motor control output devices }
+
+{ Copyright (C)2018, Philip Munts, President, Munts AM Corp.                  }
+{                                                                             }
+{ Redistribution and use in source and binary forms, with or without          }
+{ modification, are permitted provided that the following conditions are met: }
+{                                                                             }
+{ * Redistributions of source code must retain the above copyright notice,    }
+{   this list of conditions and the following disclaimer.                     }
+{                                                                             }
+{ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" }
+{ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   }
+{ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  }
+{ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   }
+{ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         }
+{ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        }
+{ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    }
+{ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     }
+{ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     }
+{ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
+{ POSSIBILITY OF SUCH DAMAGE.                                                 }
+
+UNIT Motor;
+
+INTERFACE
+
+  USES
+    SysUtils;
+
+  CONST
+    VELOCITY_MIN  = -1.0;  { Full speed reverse }
+    VELOCITY_MAX  = 1.0;   { Full speed forward }
+    VELOCITY_STOP = 0.0;
+
+  TYPE
+
+    { Define an exception for motor errors }
+
+    Motor_Error = CLASS(Exception);
+
+    { Define an interface for motor control outputs }
+
+    Device = INTERFACE
+
+      { Write method }
+
+      PROCEDURE Write(velocity : Real);
+
+      { Velocity property }
+
+      PROPERTY velocity : Real WRITE Write;
+    END;
+
+IMPLEMENTATION
+
+END.
