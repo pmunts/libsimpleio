c227988ed86fd57e1d463b14e463a57e22378d98
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-03-09 12:34:06 +0000

    Added an interface for motors.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14378 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ pascal/interfaces/servo.pas
@@ -0,0 +1,56 @@
+{ Abstract interface for servo output devices }
+
+{ Copyright (C)2018, Philip Munts, President, Munts AM Corp.                  }
+{                                                                             }
+{ Redistribution and use in source and binary forms, with or without          }
+{ modification, are permitted provided that the following conditions are met: }
+{                                                                             }
+{ * Redistributions of source code must retain the above copyright notice,    }
+{   this list of conditions and the following disclaimer.                     }
+{                                                                             }
+{ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" }
+{ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   }
+{ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  }
+{ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   }
+{ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         }
+{ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        }
+{ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    }
+{ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     }
+{ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     }
+{ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
+{ POSSIBILITY OF SUCH DAMAGE.                                                 }
+
+UNIT Servo;
+
+INTERFACE
+
+  USES
+    SysUtils;
+
+  CONST
+    POSITION_MIN     = -1.0;  { Full reverse }
+    POSITION_MAX     = 1.0;   { Full forward }
+    POSITION_NEUTRAL = 0.0;
+
+  TYPE
+
+    { Define an exception for servo errors }
+
+    Servo_Error = CLASS(Exception);
+
+    { Define an interface for servo outputs }
+
+    Device = INTERFACE
+
+      { Write method }
+
+      PROCEDURE Write(position : Real);
+
+      { Velocity property }
+
+      PROPERTY position : Real WRITE Write;
+    END;
+
+IMPLEMENTATION
+
+END.
