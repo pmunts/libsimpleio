622640b85b2c0ea9c6b7c84173b11f6af61d90cb
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-03-13 09:35:46 +0000

    Added object for controlling motors with a servo (e.g. a continuous rotations servo)
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14428 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ c++/objects/motor-servo.h
@@ -0,0 +1,46 @@
+// Motor services using servo outputs (e.g. continuous rotation servos)
+
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef _MOTOR_SERVO_H
+#define _MOTOR_SERVO_H
+
+#include <motor-interface.h>
+#include <servo-interface.h>
+
+namespace Motor::Servo
+{
+  struct Output_Class: public Interfaces::Motor::Output_Interface
+  {
+    Output_Class(Interfaces::Servo::Output servo,
+      const double velocity = Interfaces::Motor::VELOCITY_STOP);
+
+    // Motor output methods
+
+    virtual void write(const double velocity);
+
+  private:
+
+    Interfaces::Servo::Output servo;
+  };
+}
+
+#endif
