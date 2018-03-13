aa8aa4976a6ff84abfd0e8b83f85089ef4240d63
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-03-13 08:37:47 +0000

    Added servo services using libsimpleio
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14423 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ c++/objects/servo-libsimpleio.cpp
@@ -0,0 +1,78 @@
+// Servo output services using libsimpleio
+
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <cerrno>
+
+#include <servo-libsimpleio.h>
+#include <libpwm.h>
+
+// Constructor
+
+libsimpleio::Servo::Output_Class::Output_Class(unsigned chip, unsigned channel,
+  unsigned frequency, double position)
+{
+  // Validate parameters
+
+  if (frequency < 1) throw(EINVAL);
+  if (frequency > 400) throw(EINVAL);
+  if (position < Interfaces::Servo::POSITION_MIN) throw(EINVAL);
+  if (position > Interfaces::Servo::POSITION_MAX) throw(EINVAL);
+
+  // Calculate the PWM pulse frequency and initial pulse width in nanoseconds
+
+  const unsigned period = unsigned(1.0E9/frequency + 0.5);
+  const unsigned ontime = 1500000 + unsigned(500000.0*position + 0.5);
+
+  // Configure the PWM output
+
+  int fd;
+  int error;
+
+  PWM_configure(chip, channel, period, ontime, PWM_POLARITY_ACTIVEHIGH, &error);
+  if (error) throw(error);
+
+  PWM_open(chip, channel, &fd, &error);
+  if (error) throw(error);
+
+  this->fd = fd;
+}
+
+// Servo output methods
+
+void libsimpleio::Servo::Output_Class::write(const double position)
+{
+  // Validate parameters
+
+  if (position < Interfaces::Servo::POSITION_MIN) throw(EINVAL);
+  if (position > Interfaces::Servo::POSITION_MAX) throw(EINVAL);
+
+  // Calculate the required PWM pulse width in nanoseconds
+
+  const unsigned ontime = 1500000 + unsigned(500000.0*position + 0.5);
+
+  // Set the PWM pulse width
+
+  int error;
+
+  PWM_write(this->fd, ontime, &error);
+  if (error) throw(error);
+}
---
+++ c++/objects/servo-libsimpleio.h
@@ -0,0 +1,47 @@
+// Servo output services using libsimpleio
+
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef _SERVO_LIBSIMPLEIO_H
+#define _SERVO_LIBSIMPLEIO_H
+
+#include <servo-interface.h>
+
+namespace libsimpleio::Servo
+{
+  struct Output_Class: public Interfaces::Servo::Output_Interface
+  {
+    // Constructor
+
+    Output_Class(unsigned chip, unsigned pin, unsigned frequency = 50,
+      double position = Interfaces::Servo::POSITION_NEUTRAL);
+
+    // Servo output methods
+
+    virtual void write(const double position);
+
+  private:
+
+    int fd;
+  };
+}
+
+#endif
