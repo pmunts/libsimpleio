8223522f8ef2fb4f0c8e9fae39f342c6e954b832
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-03-15 12:21:09 +0000

    Added raw HID services
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14488 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ c++/objects/hid-libsimpleio.cpp
@@ -0,0 +1,106 @@
+// Raw HID device services using libsimpleio
+
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <cerrno>
+#include <cstdint>
+
+#include <hid-libsimpleio.h>
+#include <libhidraw.h>
+#include <liblinux.h>
+
+// Constructors
+
+libsimpleio::HID::Messenger_Class::Messenger_Class(const char *name)
+{
+  int32_t fd;
+  int32_t error;
+
+  HIDRAW_open(name, &fd, &error);
+  if (error) throw error;
+
+  this->fd = fd;
+}
+
+libsimpleio::HID::Messenger_Class::Messenger_Class(uint16_t VID, uint16_t PID)
+{
+  int32_t fd;
+  int32_t error;
+
+  HIDRAW_open_id(VID, PID, &fd, &error);
+  if (error) throw error;
+
+  this->fd = fd;
+}
+
+// Methods
+
+void libsimpleio::HID::Messenger_Class::Send(
+  Interfaces::Message64::Message cmd)
+{
+  int32_t count;
+  int32_t error;
+
+  HIDRAW_send(this->fd, cmd->payload, Interfaces::Message64::Size, &count,
+    &error);
+
+  if (error) throw error;
+  if (count != Interfaces::Message64::Size) throw EIO;
+}
+
+void libsimpleio::HID::Messenger_Class::Receive(
+  Interfaces::Message64::Message cmd)
+{
+  int32_t count;
+  int32_t error;
+
+  HIDRAW_receive(this->fd, cmd->payload, Interfaces::Message64::Size, &count,
+    &error);
+
+  if (error) throw error;
+  if (count != Interfaces::Message64::Size) throw EIO;
+}
+
+void libsimpleio::HID::Messenger_Class::Transaction(
+  Interfaces::Message64::Message cmd, Interfaces::Message64::Message resp,
+  unsigned timeoutms)
+{
+  // Send the command
+
+  this->Send(cmd);
+
+  // Wait for the response
+
+  if (timeoutms > 0)
+  {
+    int32_t files[1]   = { this->fd };
+    int32_t events[1]  = { POLLIN };
+    int32_t results[1] = { 0 };
+    int32_t error;
+
+    LINUX_poll(1, files, events, results, timeoutms, &error);
+    if (error) throw error;
+  }
+
+  // Receive the response
+
+  this->Receive(resp);
+}
---
+++ c++/objects/hid-libsimpleio.h
@@ -0,0 +1,53 @@
+// Raw HID device services using libsimpleio
+
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef _HID_LIBSIMPLEIO_H
+#define _HID_LIBSIMPLEIO_H
+
+#include <cstdint>
+
+#include <message64-interface.h>
+
+namespace libsimpleio::HID
+{
+  // Messenger class definition
+
+  struct Messenger_Class: public Interfaces::Message64::Messenger_Interface
+  {
+    Messenger_Class(const char *name);
+
+    Messenger_Class(uint16_t VID, uint16_t PID);
+
+    virtual void Send(Interfaces::Message64::Message cmd);
+
+    virtual void Receive(Interfaces::Message64::Message resp);
+
+    virtual void Transaction(Interfaces::Message64::Message cmd,
+      Interfaces::Message64::Message resp, unsigned timeoutms = 0);
+
+  private:
+
+    int fd;
+  };
+}
+
+#endif
