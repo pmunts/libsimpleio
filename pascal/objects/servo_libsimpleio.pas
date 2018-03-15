e42cf3704a45b120bf51827e91167526df9ca62d
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-03-15 15:06:32 +0000

    Added servo output services using libsimpleio
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14500 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ pascal/objects/servo_libsimpleio.pas
@@ -0,0 +1,129 @@
+{ Servo output services using libsimpleio                                     }
+
+{ Copyright (C)2018, Philip Munts, President, Munts AM Corp.                  }
+{                                                                             }
+{ Redistribution and use in source and binary forms, with or without          }
+{ modification, are permitted provided that the following conditions are met: }
+{                                                                             }
+{ * Redistributions of source code must retain the above copyright notice,    }
+{   this list of conditions and the following disclaimer.                     }
+{                                                                             }
+{ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" }
+{ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   }
+{ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  }
+{ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   }
+{ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         }
+{ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        }
+{ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    }
+{ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     }
+{ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     }
+{ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
+{ POSSIBILITY OF SUCH DAMAGE.                                                 }
+
+UNIT Servo_libsimpleio;
+
+INTERFACE
+
+  USES Servo;
+
+  TYPE
+
+    { Define a class implementing Servo.PositionOutput }
+
+    OutputSubclass = CLASS(TInterfacedObject, PositionOutput)
+
+      CONSTRUCTOR Create
+       (chip      : Cardinal;
+        channel   : Cardinal;
+        frequency : Cardinal;
+        position  : Real = POSITION_NEUTRAL);
+
+      DESTRUCTOR Destroy; OVERRIDE;
+
+      PROCEDURE WritePosition(position : Real);
+
+      PROPERTY position : Real WRITE WritePosition;
+
+    PRIVATE
+      fd     : Integer;
+    END;
+
+IMPLEMENTATION
+
+  USES
+    errno,
+    libPWM;
+
+  { Servo_libsimpleio.OutputSubclass constructor }
+
+  CONSTRUCTOR OutputSubclass.Create
+   (chip      : Cardinal;
+    channel   : Cardinal;
+    frequency : Cardinal;
+    position  : Real);
+
+  VAR
+    period : Integer;
+    ontime : Integer;
+    error  : Integer;
+
+  BEGIN
+    IF (position < POSITION_MIN) OR (position > POSITION_MAX) THEN
+      RAISE Servo_Error.create('ERROR: Invalid position parameter, ' +
+        strerror(EINVAL));
+
+    period := Round(1.0E9/frequency);
+    ontime := Round(1500000.0 + 500000.0*position);
+
+    libPWM.Configure(chip, channel, period, ontime, 1, error);
+
+    IF error <> 0 THEN
+      RAISE Servo_Error.create('ERROR: libPWM.Configure() failed, ' +
+        strerror(error));
+
+    libPWM.Open(chip, channel, Self.fd, error);
+
+    IF error <> 0 THEN
+      RAISE Servo_Error.create('ERROR: libPWM.Open() failed, ' +
+        strerror(error));
+  END;
+
+  { Servo_libsimpleio.OutputSubclass destructor }
+
+  DESTRUCTOR OutputSubclass.Destroy;
+
+  VAR
+    error  : Integer;
+
+  BEGIN
+
+    { Close the PWM output device file handle }
+
+    libPWM.Close(Self.fd, error);
+
+    IF error <> 0 THEN
+      RAISE Servo_Error.create('ERROR: libPWM.Close() failed, ' +
+        strerror(error));
+
+    INHERITED;
+  END;
+
+  {  Servo_libsimpleio.OutputSubclass write method }
+
+  PROCEDURE OutputSubclass.WritePosition(position : Real);
+
+  VAR
+    ontime : Integer;
+    error  : Integer;
+
+  BEGIN
+    ontime := Round(1500000.0 + 500000.0*position);
+
+    libPWM.Write(Self.fd, ontime, error);
+
+    IF error <> 0 THEN
+      RAISE Servo_Error.create('ERROR: libPWM.Write() failed, ' +
+        strerror(error));
+  END;
+
+END.
