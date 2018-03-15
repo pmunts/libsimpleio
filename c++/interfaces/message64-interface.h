c8f10f05a984c01ae81867813d8ba1368b1fc724
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-03-15 12:02:19 +0000

    Added an abstract interface for 64-byte message services
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@14485 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ c++/interfaces/message64-interface.h
@@ -0,0 +1,55 @@
+// Abstract interface for 64-byte messages and messengers
+
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef _MESSAGE64_INTERFACE_H
+#define _MESSAGE64_INTERFACE_H
+
+#include <cstdint>
+
+namespace Interfaces::Message64
+{
+  static const unsigned Size = 64;
+
+  // Message class
+
+  struct Message_Class
+  {
+    uint8_t payload[Size];
+  };
+
+  typedef Message_Class *Message;
+
+  // Messenger abstract interface
+
+  struct Messenger_Interface
+  {
+    virtual void Send(Message cmd) = 0;
+
+    virtual void Receive(Message resp) = 0;
+
+    virtual void Transaction(Message cmd, Message resp) = 0;
+  };
+
+  typedef Messenger_Interface *Messenger;
+}
+
+#endif
