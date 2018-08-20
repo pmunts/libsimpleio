badcfe290f506d868241a9e7f5ea220018b4fbc3
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-08-20 12:09:43 +0000

    Added goop to bundle a .Net Core application into either a tarball or an architecture independent Debian package file. Both of these will work with regular Debian, or MuntsOS Embedded Linux.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15423 06ee2f96-5407-11de-8bab-f3d7589a4122

--- csharp/include/coreapp.mk
+++ csharp/include/coreapp.mk
@@ -22,8 +22,62 @@
 
 COREAPPNAME	?= $(shell basename *.csproj .csproj)
 COREAPPPROJ	?= $(COREAPPNAME).csproj
+COREAPPPUB	?= bin/$(CONFIGURATION)/netcoreapp2.1/publish
+COREAPPDEST	?= /usr/local
+COREAPPLIB	?= $(COREAPPDEST)/lib/$(COREAPPNAME)
+COREAPPBIN	?= $(COREAPPDEST)/bin
+
+PKGNAME		= $(shell echo $(COREAPPNAME) | tr '[_]' '[\-]')
+PKGVERSION	= $(shell date +%Y.%j)
+PKGARCH		= $(DEBARCH)
+PKGDIR		= $(PKGNAME)-$(PKGVERSION)
+PKGFILE		= $(PKGDIR).deb
+
+TAR		?= tar
+TARROOT		?= fakeroot
+TARFLAGS	?= --owner=root --group=root --mode=ugo-w
+
+# Compile the application
 
 coreapp_mk_build:
 	dotnet publish $(COREAPPPROJ) -c $(CONFIGURATION)
 
+# Pack the application into a Debian package file
+
+$(PKGDIR): coreapp_mk_build
+	mkdir -p						$(PKGDIR)/DEBIAN
+	install -cm 0644 $(CSHARPSRC)/include/coreapp.control	$(PKGDIR)/DEBIAN/control
+	sed -i s/@@NAME@@/$(PKGNAME)/g				$(PKGDIR)/DEBIAN/control
+	sed -i s/@@VERSION@@/$(PKGVERSION)/g			$(PKGDIR)/DEBIAN/control
+	mkdir -p 						$(PKGDIR)/$(COREAPPBIN)
+	echo "exec dotnet $(COREAPPLIB)/$(COREAPPNAME).dll"	>$(PKGDIR)/$(COREAPPBIN)/$(COREAPPNAME)
+	chmod 755						$(PKGDIR)/$(COREAPPBIN)/$(COREAPPNAME)
+	mkdir -p 						$(PKGDIR)/$(COREAPPLIB)
+	cp -R -P -p $(COREAPPPUB)/*				$(PKGDIR)/$(COREAPPLIB)
+	find $(PKGDIR)/$(COREAPPLIB) -type d -exec chmod 755 "{}" ";"
+	find $(PKGDIR)/$(COREAPPLIB) -type f -exec chmod 644 "{}" ";"
+	touch $@
+
+$(PKGFILE): $(PKGDIR)
+	chmod -R ugo-w $(PKGDIR)/usr
+	fakeroot dpkg-deb -Zgzip --build $(PKGDIR)
+	chmod -R u+w $(PKGDIR)/usr
+
+coreapp_mk_deb: $(PKGFILE)
+
+# Pack the application into a distribution tarball
+
+coreapp_mk_tarball: coreapp_mk_build
+	mkdir -p 						$(TARROOT)/$(COREAPPBIN)
+	echo "exec dotnet $(COREAPPLIB)/$(COREAPPNAME).dll"	>$(TARROOT)/$(COREAPPBIN)/$(COREAPPNAME)
+	chmod 755						$(TARROOT)/$(COREAPPBIN)/$(COREAPPNAME)
+	mkdir -p 						$(TARROOT)/$(COREAPPLIB)
+	cp -R -P -p $(COREAPPPUB)/*				$(TARROOT)/$(COREAPPLIB)
+	find $(TARROOT)/$(COREAPPLIB) -type d -exec chmod 755 "{}" ";"
+	find $(TARROOT)/$(COREAPPLIB) -type f -exec chmod 644 "{}" ";"
+	cd $(TARROOT) && $(TAR) czf ../$(COREAPPNAME).tgz * $(TARFLAGS)
+
+# Remove working files
+
 coreapp_mk_clean: csharp_mk_clean
+	rm -rf $(PKGDIR) $(TARROOT) *.tgz *.deb
