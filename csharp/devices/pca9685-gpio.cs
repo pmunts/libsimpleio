6ed6160db45f06f6d1be28ff92b15fc58862a9c5
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-08-23 11:37:59 +0000

    Split up pca9685.cs into pca9685-device.cs, pca9685-gpio.cs, and pca9685-pwm.cs.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15450 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ csharp/devices/pca9685-gpio.cs
@@ -0,0 +1,85 @@
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+namespace IO.Devices.PCA9685.GPIO
+{
+  /// <summary>
+  /// Encapsulates PCA9685 GPIO outputs.
+  /// </summary>
+  public class Pin : IO.Interfaces.GPIO.Pin
+  {
+    private readonly static byte[] ON = { 0x00, 0x10, 0x00, 0x00 };
+    private readonly static byte[] OFF = { 0x00, 0x00, 0x00, 0x10 };
+    private readonly Device dev;
+    private readonly byte channel;
+    private byte[] data;
+
+    private bool Compare(byte[] x, byte[] y)
+    {
+      if (x[0] != y[0]) return false;
+      if (x[1] != y[1]) return false;
+      if (x[2] != y[2]) return false;
+      if (x[3] != y[3]) return false;
+      return true;
+    }
+
+    /// <summary>
+    /// Constructor for a single GPIO output pin.
+    /// </summary>
+    /// <param name="dev">PCA9685 device object.</param>
+    /// <param name="channel">GPIO output channel number.</param>
+    /// <param name="state">Initial output state.</param>
+    public Pin(Device dev, int channel, bool state = false)
+    {
+      // Validate parameters
+
+      if ((channel < Device.MIN_CHANNEL) || (channel > Device.MAX_CHANNEL))
+        throw new System.Exception("Invalid channel number");
+
+      this.dev = dev;
+      this.channel = System.Convert.ToByte(channel);
+      this.data = new byte[4];
+      this.state = state;
+    }
+
+    /// <summary>
+    /// Read/Write GPIO output state property.
+    /// </summary>
+    public bool state
+    {
+      get
+      {
+        dev.ReadChannel(channel, out data);
+
+        if (Compare(data, ON))
+          return true;
+        else if (Compare(data, OFF))
+          return false;
+        else
+          throw new System.Exception("Unexpected channel data");
+      }
+
+      set
+      {
+        dev.WriteChannel(channel, value ? ON : OFF);
+      }
+    }
+  }
+}
---
+++ csharp/devices/pca9685-pwm.cs
@@ -0,0 +1,81 @@
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+namespace IO.Devices.PCA9685.PWM
+{
+  /// <summary>
+  /// Encapsulates PCA9685 PWM outputs.
+  /// </summary>
+  public class Output : IO.Interfaces.PWM.Output
+  {
+    private readonly Device dev;
+    private readonly byte channel;
+    private byte[] data;
+
+    /// <summary>
+    /// Constructor for a single PWM output.
+    /// </summary>
+    /// <param name="dev">PCA9685 device object.</param>
+    /// <param name="channel">PWM output channel number.</param>
+    /// <param name="dutycycle">Initial PWM output duty cycle.
+    /// Allowed values are 0.0 to 100.0 percent.</param>
+    public Output(Device dev, int channel, double dutycycle = 0.0)
+    {
+      // Validate parameters
+
+      if ((channel < Device.MIN_CHANNEL) || (channel > Device.MAX_CHANNEL))
+        throw new System.Exception("Invalid channel number");
+
+      if ((dutycycle < IO.Interfaces.PWM.DutyCycles.Minimum) ||
+          (dutycycle > IO.Interfaces.PWM.DutyCycles.Maximum))
+        throw new System.Exception("Invalid duty cycle");
+
+      this.dev = dev;
+      this.channel = System.Convert.ToByte(channel);
+      this.data = new byte[4];
+      this.dutycycle = dutycycle;
+    }
+
+    /// <summary>
+    /// Write-only property for setting the PWM output duty cycle.
+    /// Allowed values are 0.0 to 100.0 percent.
+    /// </summary>
+    public double dutycycle
+    {
+      set
+      {
+        // Validate parameters
+
+        if ((value < IO.Interfaces.PWM.DutyCycles.Minimum) ||
+            (value > IO.Interfaces.PWM.DutyCycles.Maximum))
+          throw new System.Exception("Invalid duty cycle");
+
+        System.UInt16 offtime = System.Convert.ToUInt16(value*40.95);
+
+        data[0] = 0;
+        data[1] = 0;
+        data[2] = System.Convert.ToByte(offtime % 256);
+        data[3] = System.Convert.ToByte(offtime / 256);
+
+        dev.WriteChannel(channel, data);
+      }
+    }
+  }
+}
