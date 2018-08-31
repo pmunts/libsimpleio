d0b991b32725361709aeaa0ee6db52beac0c0063
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-08-31 06:45:22 +0000

    Add support for I2C buses that support clock stretching.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15527 06ee2f96-5407-11de-8bab-f3d7589a4122

--- csharp/spiagent/transport_i2c.cs
+++ csharp/spiagent/transport_i2c.cs
@@ -4,12 +4,14 @@ namespace SPIAgent
   {
     private const byte LPC1114_I2C_ADDRESS = 0x44;
     private readonly IO.Interfaces.I2C.Device dev;
+    private readonly bool clockstretch;
     private byte[] cmd_bytes;
     private byte[] resp_bytes;
 
-    public Transport_I2C(IO.Interfaces.I2C.Bus bus)
+    public Transport_I2C(IO.Interfaces.I2C.Bus bus, bool clockstretch = false)
     {
-      this.dev = dev = new IO.Interfaces.I2C.Device(bus, LPC1114_I2C_ADDRESS);
+      this.dev = new IO.Interfaces.I2C.Device(bus, LPC1114_I2C_ADDRESS);
+      this.clockstretch = clockstretch;
       this.cmd_bytes = new byte[12];
       this.resp_bytes = new byte[16];
     }
@@ -22,6 +24,11 @@ namespace SPIAgent
     private static int Build32(byte b0, byte b1, byte b2, byte b3)
     {
       return (b3 << 24) | (b2 << 16) | (b1 << 8) | b0;
+    }
+
+    private void usleep(int usecs)
+    {
+      System.Threading.Thread.Sleep(1);
     }
 
     public void Command(SPIAGENT_COMMAND_MSG_t cmd,
@@ -42,14 +49,31 @@ namespace SPIAgent
       cmd_bytes[10] = Split32(cmd.data, 2);
       cmd_bytes[11] = Split32(cmd.data, 3);
 
-      dev.Write(cmd_bytes, cmd_bytes.Length);
-
       if (cmd.command == (int)Commands.SPIAGENT_CMD_PUT_LEGORC)
-        System.Threading.Thread.Sleep(20);
-      else
-        System.Threading.Thread.Sleep(1);
+      {
+        // The LEGO infrared protocol takes a LONG time...
 
-      dev.Read(resp_bytes, resp_bytes.Length);
+        dev.Write(cmd_bytes, cmd_bytes.Length);
+        System.Threading.Thread.Sleep(20);
+        dev.Read(resp_bytes, resp_bytes.Length);
+      }
+      else if (clockstretch)
+      {
+        // The I2C bus supports clock stretching, so we can send the commmand
+        // and receive the response in a single I2C transaction.
+
+        dev.Transaction(cmd_bytes, cmd_bytes.Length, resp_bytes,
+          resp_bytes.Length);
+      }
+      else
+      {
+        // The I2C bus does not support clock stretching, so we have to pause
+        // between sending the command and receiving the response.
+
+        dev.Write(cmd_bytes, cmd_bytes.Length);
+        usleep(100);
+        dev.Read(resp_bytes, resp_bytes.Length);
+      }
 
       resp.command = Build32(resp_bytes[0],  resp_bytes[1],  resp_bytes[2],  resp_bytes[3]);
       resp.pin     = Build32(resp_bytes[4],  resp_bytes[5],  resp_bytes[6],  resp_bytes[7]);
