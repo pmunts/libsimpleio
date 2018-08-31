fab61ae416d83237693c6038378aec968d924748
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-08-31 08:27:47 +0000

    Added code to busy-wait between the I2C write and read operations.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15528 06ee2f96-5407-11de-8bab-f3d7589a4122

--- csharp/spiagent/transport_i2c.cs
+++ csharp/spiagent/transport_i2c.cs
@@ -5,6 +5,8 @@ namespace SPIAgent
     private const byte LPC1114_I2C_ADDRESS = 0x44;
     private readonly IO.Interfaces.I2C.Device dev;
     private readonly bool clockstretch;
+    private readonly System.Diagnostics.Stopwatch timer;
+    private readonly long ClocksPerMicrosecond;
     private byte[] cmd_bytes;
     private byte[] resp_bytes;
 
@@ -12,28 +14,43 @@ namespace SPIAgent
     {
       this.dev = new IO.Interfaces.I2C.Device(bus, LPC1114_I2C_ADDRESS);
       this.clockstretch = clockstretch;
+      this.timer = new System.Diagnostics.Stopwatch();
+      this.ClocksPerMicrosecond = System.Diagnostics.Stopwatch.Frequency/1000000L;
       this.cmd_bytes = new byte[12];
       this.resp_bytes = new byte[16];
     }
+
+    // Busy wait for the specified number of microseconds
+
+    private void usleep(int usecs)
+    {
+      timer.Restart();
+
+      long limit = timer.ElapsedTicks + usecs*ClocksPerMicrosecond;
+      while (timer.ElapsedTicks < limit);
+
+      timer.Stop();
+    }
+
+    // Split a 32-bit integer into individual bytes
 
     private static byte Split32(int n, int b)
     {
       return System.Convert.ToByte((n >> (b*8)) & 0xFF);
     }
 
+    // Combine 4 bytes into a 32-bit integer
+
     private static int Build32(byte b0, byte b1, byte b2, byte b3)
     {
       return (b3 << 24) | (b2 << 16) | (b1 << 8) | b0;
     }
 
-    private void usleep(int usecs)
-    {
-      System.Threading.Thread.Sleep(1);
-    }
-
     public void Command(SPIAGENT_COMMAND_MSG_t cmd,
       ref SPIAGENT_RESPONSE_MSG_t resp)
     {
+      // Split the command message into a byte array
+
       cmd_bytes[0]  = Split32(cmd.command, 0);
       cmd_bytes[1]  = Split32(cmd.command, 1);
       cmd_bytes[2]  = Split32(cmd.command, 2);
@@ -49,9 +66,11 @@ namespace SPIAgent
       cmd_bytes[10] = Split32(cmd.data, 2);
       cmd_bytes[11] = Split32(cmd.data, 3);
 
+      // Dispatch the command
+
       if (cmd.command == (int)Commands.SPIAGENT_CMD_PUT_LEGORC)
       {
-        // The LEGO infrared protocol takes a LONG time...
+        // The LEGO infrared protocol takes a REALLY LONG time...
 
         dev.Write(cmd_bytes, cmd_bytes.Length);
         System.Threading.Thread.Sleep(20);
@@ -75,10 +94,19 @@ namespace SPIAgent
         dev.Read(resp_bytes, resp_bytes.Length);
       }
 
-      resp.command = Build32(resp_bytes[0],  resp_bytes[1],  resp_bytes[2],  resp_bytes[3]);
-      resp.pin     = Build32(resp_bytes[4],  resp_bytes[5],  resp_bytes[6],  resp_bytes[7]);
-      resp.data    = Build32(resp_bytes[8],  resp_bytes[9],  resp_bytes[10], resp_bytes[11]);
-      resp.error   = Build32(resp_bytes[12], resp_bytes[13], resp_bytes[14], resp_bytes[15]);
+      // Assemble the response message from the response byte array
+
+      resp.command = Build32(resp_bytes[0],  resp_bytes[1],
+        resp_bytes[2],  resp_bytes[3]);
+
+      resp.pin     = Build32(resp_bytes[4],  resp_bytes[5],
+        resp_bytes[6],  resp_bytes[7]);
+
+      resp.data    = Build32(resp_bytes[8],  resp_bytes[9],
+        resp_bytes[10], resp_bytes[11]);
+
+      resp.error   = Build32(resp_bytes[12], resp_bytes[13],
+        resp_bytes[14], resp_bytes[15]);
     }
   }
 }
