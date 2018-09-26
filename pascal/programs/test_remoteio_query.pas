5c54d4e68b2478f20ab50456681cc217fa495dcf
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-09-26 03:37:38 +0000

    Renamed test_remoteio_hid_query to test_remoteio_query
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15651 06ee2f96-5407-11de-8bab-f3d7589a4122

--- pascal/programs/test_remoteio_query.pas
+++ pascal/programs/test_remoteio_query.pas
@@ -20,7 +20,7 @@
 { ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
 { POSSIBILITY OF SUCH DAMAGE.                                                 }
 
-PROGRAM test_remoteio_hid_query;
+PROGRAM test_remoteio_query;
 
 USES
   HID_libsimpleio,
