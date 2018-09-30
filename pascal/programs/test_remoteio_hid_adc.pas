b6a54b119960a480b1003487e6c1e11663defb8c
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-09-30 15:55:21 +0000

    Added support for Remote I/O analog inputs
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15682 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ pascal/programs/test_remoteio_hid_adc.pas
@@ -0,0 +1,67 @@
+{ Copyright (C)2018, Philip Munts, President, Munts AM Corp.                  }
+{                                                                             }
+{ Redistribution and use in source and binary forms, with or without          }
+{ modification, are permitted provided that the following conditions are met: }
+{                                                                             }
+{ * Redistributions of source code must retain the above copyright notice,    }
+{   this list of conditions and the following disclaimer.                     }
+{                                                                             }
+{ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" }
+{ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   }
+{ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  }
+{ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   }
+{ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         }
+{ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        }
+{ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    }
+{ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     }
+{ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     }
+{ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
+{ POSSIBILITY OF SUCH DAMAGE.                                                 }
+
+PROGRAM test_remoteio_hid_adc;
+
+USES
+  ADC,
+  HID_libsimpleio,
+  Message64,
+  RemoteIO,
+  RemoteIO_ADC,
+  SysUtils;
+
+VAR
+  hidmsg   : Message64.Messenger;
+  remdev   : RemoteIO.Device;
+  chanlist : RemoteIO.ChannelArray;
+  numchans : Cardinal;
+  samplers : ARRAY OF ADC.Sample;
+  chan     : Cardinal;
+
+BEGIN
+  WriteLn;
+  WriteLn('HID Remote Analog Input Test');
+  Writeln;
+
+  { Create objects }
+
+  hidmsg   := HID_libsimpleio.MessengerSubclass.Create;
+  remdev   := RemoteIO.Device.Create(hidmsg);
+
+  { Configure analog inputs }
+
+  chanlist := remdev.ADC_Inputs;
+  numchans := Length(chanlist);
+
+  SetLength(samplers, numchans);
+
+  FOR chan := 0 TO numchans - 1 DO
+    samplers[chan] := RemoteIO_ADC.SampleSubclass.Create(remdev, chan);
+
+  REPEAT
+    FOR chan := 0 TO numchans - 1 DO
+      Write(samplers[chan].sample : 5, ' ');
+
+    Writeln;
+
+    sleep(1000);
+  UNTIL False;
+END.
---
+++ pascal/remoteio/remoteio_adc.pas
@@ -0,0 +1,106 @@
+{ A/D converter input services using the Remote I/O Protocol                  }
+
+{ Copyright (C)2018, Philip Munts, President, Munts AM Corp.                  }
+{                                                                             }
+{ Redistribution and use in source and binary forms, with or without          }
+{ modification, are permitted provided that the following conditions are met: }
+{                                                                             }
+{ * Redistributions of source code must retain the above copyright notice,    }
+{   this list of conditions and the following disclaimer.                     }
+{                                                                             }
+{ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" }
+{ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   }
+{ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  }
+{ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   }
+{ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         }
+{ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        }
+{ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    }
+{ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     }
+{ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     }
+{ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
+{ POSSIBILITY OF SUCH DAMAGE.                                                 }
+
+UNIT RemoteIO_ADC;
+
+INTERFACE
+
+  USES
+    ADC,
+    RemoteIO;
+
+  TYPE
+    SampleSubclass = CLASS(TInterfacedObject, ADC.Sample)
+      CONSTRUCTOR Create
+       (dev : RemoteIO.Device;
+        num : RemoteIO.Channels);
+
+      FUNCTION sample : Integer;
+
+      FUNCTION resolution : Cardinal;
+    PRIVATE
+      mydev  : RemoteIO.Device;
+      mynum  : RemoteIO.Channels;
+      myres  : Cardinal;
+    END;
+
+IMPLEMENTATION
+
+  USES
+    errno,
+    Message64;
+
+  CONSTRUCTOR SampleSubclass.Create
+   (dev : RemoteIO.Device;
+    num : RemoteIO.Channels);
+
+  VAR
+    cmd  : Message64.Message;
+    resp : Message64.Message;
+
+  BEGIN
+    Self.mydev := dev;
+    Self.mynum := num;
+
+    FillChar(cmd, SizeOf(cmd), #0);
+
+    cmd[0] := Ord(RemoteIO.ADC_CONFIGURE_REQUEST);
+    cmd[2] := num;
+
+    Self.mydev.Transaction(cmd, resp);
+
+    IF resp[2] <> 0 THEN
+      RAISE RemoteIO.Error.Create
+       ('ERROR: Remote IO transaction failed, ' + errno.strerror(resp[2]));
+
+    Self.myres := resp[3];
+  END;
+
+  FUNCTION SampleSubclass.sample : Integer;
+
+  VAR
+    cmd  : Message64.Message;
+    resp : Message64.Message;
+
+  BEGIN
+    FillChar(cmd, SizeOf(cmd), #0);
+
+    cmd[0] := Ord(RemoteIO.ADC_READ_REQUEST);
+    cmd[2] := mynum;
+
+    Self.mydev.Transaction(cmd, resp);
+
+    IF resp[2] <> 0 THEN
+      RAISE RemoteIO.Error.Create
+       ('ERROR: Remote IO transaction failed, ' + errno.strerror(resp[2]));
+
+    sample := Integer((resp[3] SHL 24) + (resp[4] SHL 16) + (resp[5] SHL 8) +
+      resp[6]);
+  END;
+
+  FUNCTION SampleSubclass.resolution : Cardinal;
+
+  BEGIN
+    resolution := Self.myres;
+  END;
+
+END.
