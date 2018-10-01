edb176c1456ae51ffe9986ddf308e14df78a92db
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-10-01 12:57:20 +0000

    Added test_remoteio_hid_74x595_gpio
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15690 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ pascal/programs/test_remoteio_hid_74x595_gpio.pas
@@ -0,0 +1,56 @@
+{ HID Remote I/O 74HC595 Shift Register GPIO Pin Test }
+
+{ Copyright (C)2018, Philip Munts, President, Munts AM Corp.                  }
+{                                                                             }
+{ Redistribution and use in source and binary forms, with or without          }
+{ modification, are permitted provided that the following conditions are met: }
+{                                                                             }
+{ * Redistributions of source code must retain the above copyright notice,    }
+{   this list of conditions and the following disclaimer.                     }
+{                                                                             }
+{ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" }
+{ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   }
+{ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  }
+{ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   }
+{ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         }
+{ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        }
+{ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    }
+{ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     }
+{ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     }
+{ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
+{ POSSIBILITY OF SUCH DAMAGE.                                                 }
+
+PROGRAM test_remoteio_hid_74x595_gpio;
+
+USES
+  GPIO,
+  RemoteIO,
+  RemoteIO_SPI,
+  SPI,
+  SPI_Shift_Register,
+  SPI_Shift_Register_GPIO;
+
+VAR
+  remdev : RemoteIO.Device;
+  spidev : SPI.Device;
+  regdev : SPI_Shift_Register.Device;
+  pin    : GPIO.Pin;
+
+BEGIN
+  WriteLn;
+  WriteLn('HID Remote I/O 74HC595 Shift Register GPIO Pin Test');
+  WriteLn;
+
+  { Create objects }
+
+  remdev := RemoteIO.Device.Create;
+  spidev := RemoteIO_SPI.DeviceSubclass.Create(remdev, 0, 0, 8, 4000000);
+  regdev := SPI_Shift_Register.Device.Create(spidev);
+  pin    := SPI_Shift_Register_GPIO.PinSubclass.Create(regdev, 0);
+
+  { Toggle pin }
+
+  REPEAT
+    pin.State := NOT pin.State;
+  UNTIL False;
+END.
