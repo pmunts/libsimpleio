15b1aa02e4609940c495624d8ed3e60d640ce372
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-10-16 13:25:55 +0000

    Added support for MCP3204 SPI A/D converter
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15765 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ c++/devices/mcp3204.cpp
@@ -0,0 +1,69 @@
+// MCP3204 ADC (Analog to Digital Converter) input services
+
+// Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <cerrno>
+
+#include <mcp3204.h>
+
+// Device class constructor
+
+MCP3204::Device_Class::Device_Class(Interfaces::SPI::Device dev)
+{
+  // Validate parameters
+
+  if (dev == nullptr) throw EINVAL;
+
+  this->dev = dev;
+}
+
+// Sample_Subclass constructor
+
+MCP3204::Sample_Subclass::Sample_Subclass(Device dev, unsigned channel,
+  bool differential)
+{
+  // Validate parameters
+
+  if (dev == nullptr) throw EINVAL;
+  if (channel >= MaxChannels) throw EINVAL;
+
+  this->dev = dev;
+  this->channel = channel;
+  this->differential = differential;
+}
+
+// Sample_Subclass methods
+
+int MCP3204::Sample_Subclass::sample(void)
+{
+  uint8_t cmd[1];
+  uint8_t resp[2];
+
+  cmd[0] = (differential ? 0x40 : 0x60) + (channel << 2);
+  this->dev->dev->Transaction(cmd, 1, resp, 2);
+
+  return (resp[0] << 4) + (resp[1] >> 4);
+}
+
+unsigned MCP3204::Sample_Subclass::resolution(void)
+{
+  return MCP3204::Resolution;
+}
---
+++ c++/devices/mcp3204.h
@@ -0,0 +1,78 @@
+// MCP3204 ADC (Analog to Digital Converter) input services
+
+// Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef _MCP3204_H
+#define _MCP3204_H
+
+#include <cstdint>
+
+#include <adc-interface.h>
+#include <spi-interface.h>
+
+namespace MCP3204
+{
+  static const unsigned MaxChannels = 4;
+  static const unsigned Resolution = 12;
+  static const unsigned Steps = 4096;
+  static const bool SingleEnded = false;
+  static const bool Differential = true;
+
+  // MCP3204 device class (incomplete)
+
+  struct Device_Class;
+
+  typedef Device_Class *Device;
+
+  // MCP3204 analog input class
+
+  struct Sample_Subclass: public Interfaces::ADC::Sample_Interface
+  {
+    Sample_Subclass(Device dev, unsigned channel, bool differential = false);
+
+    // ADC input methods
+
+    virtual int sample(void);
+
+    virtual unsigned resolution(void);
+
+  private:
+
+    Device dev;
+    unsigned channel;
+    bool differential;
+  };
+
+  // MCP3204 device class (completed)
+
+  struct Device_Class
+  {
+    Device_Class(Interfaces::SPI::Device dev);
+
+  private:
+
+    Interfaces::SPI::Device dev;
+
+    friend class Sample_Subclass;
+  };
+}
+
+#endif
