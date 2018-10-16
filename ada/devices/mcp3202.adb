fbece8fb3f66aafd1b959e84ab96dce7754aecc2
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-10-16 06:02:51 +0000

    Added support for the MCP3202 SPI A/D converter
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15762 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/devices/mcp3202.adb
@@ -0,0 +1,68 @@
+-- MCP3202 Analog to Digital Converter services
+
+-- Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Interfaces;
+
+USE TYPE Interfaces.Unsigned_32;
+
+PACKAGE BODY MCP3202 IS
+
+  -- Constructors
+
+  FUNCTION Create
+   (spidev  : SPI.Device;
+    chan    : Channel;
+    config  : Configuration := SingleEnded) RETURN Analog.Input IS
+
+  BEGIN
+    CASE config IS
+      WHEN SingleEnded =>
+        RETURN NEW InputSubClass'(spidev, SPI.Byte(16#18# + Natural(chan)*4));
+
+      WHEN Differential =>
+        RETURN NEW InputSubClass'(spidev, SPI.Byte(16#10# + Natural(chan)*4));
+    END CASE;
+  END Create;
+
+  -- Methods
+
+  FUNCTION Get(Self : IN OUT InputSubclass) RETURN Analog.Sample IS
+
+    cmd  : SPI.Command(0 .. 0);
+    resp : SPI.Response(0 .. 1);
+
+  BEGIN
+    cmd(0) := Self.cmd;
+    Self.spidev.Transaction(cmd, cmd'Length, resp, resp'Length);
+
+    RETURN Analog.Sample(Standard.Interfaces.Shift_Left(
+      Standard.Interfaces.Unsigned_32(resp(0)), 4) +
+      Standard.Interfaces.Shift_Right(Standard.Interfaces.Unsigned_32(resp(1)), 4));
+  END Get;
+
+  FUNCTION GetResolution(Self : IN OUT InputSubclass) RETURN Positive IS
+
+  BEGIN
+    RETURN Resolution;
+  END GetResolution;
+
+END MCP3202;
---
+++ ada/devices/mcp3202.ads
@@ -0,0 +1,65 @@
+-- MCP3202 Analog to Digital Converter services
+
+-- Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Analog;
+WITH SPI;
+
+PACKAGE MCP3202 IS
+
+  -- Configuration constants
+
+  Resolution : CONSTANT Positive := 12;
+
+  -- Define a subclass of Analog.InputInterface
+
+  TYPE InputSubclass IS NEW Analog.InputInterface WITH PRIVATE;
+
+  -- Define some types for analog input channels
+
+  TYPE Configuration IS (SingleEnded, Differential);
+
+  TYPE Channel IS NEW Natural RANGE 0 .. 1;
+
+  -- Constructors
+
+  -- If the input configuration is Differential, the channel parameter
+  -- selects IN+.  IN- is inferred to be the other input of the pair.
+
+  FUNCTION Create
+   (spidev : SPI.Device;
+    chan   : Channel;
+    config : Configuration := SingleEnded) RETURN Analog.Input;
+
+  -- Methods
+
+  FUNCTION Get(Self : IN OUT InputSubclass) RETURN Analog.Sample;
+
+  FUNCTION GetResolution(Self : IN OUT InputSubclass) RETURN Positive;
+
+PRIVATE
+
+  TYPE InputSubclass IS NEW Analog.InputInterface WITH RECORD
+    spidev : SPI.Device;
+    cmd    : SPI.Byte;
+  END RECORD;
+
+END MCP3202;
