576187362a8ac340b40f66d52b8fe97d7dc12691
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-10-16 06:03:17 +0000

    Added tests for MCP3202, MCP3204, and MCP3208.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15763 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/programs/test_mcp3202.adb
@@ -0,0 +1,67 @@
+-- MCP3202 SPI A/D Converter Test
+
+-- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Command_Line;
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+WITH Analog;
+WITH MCP3202;
+WITH SPI.libsimpleio;
+
+PROCEDURE test_mcp3202 IS
+
+  spidev : SPI.Device;
+  inputs : ARRAY (MCP3202.Channel) OF Analog.Input;
+
+BEGIN
+  New_Line;
+  Put_Line("MCP3202 SPI A/D Converter Test");
+  New_Line;
+
+  IF Ada.Command_Line.Argument_Count /= 1 THEN
+    Put_Line("Usage: test_mcp3202 <device>");
+    New_Line;
+    RETURN;
+  END IF;
+
+  spidev := SPI.libsimpleio.Create(Ada.Command_Line.Argument(1), 0, 8,
+    1_000_000);
+
+  FOR c IN MCP3202.Channel LOOP
+    inputs(c) := MCP3202.Create(spidev, c);
+  END LOOP;
+
+  Put_Line("Press CONTROL-C to exit...");
+  New_Line;
+
+  LOOP
+    Put("Samples:");
+
+    FOR i OF inputs LOOP
+      Analog.Sample_IO.Put(i.Get, 5);
+    END LOOP;
+
+    New_Line;
+
+    DELAY 2.0;
+  END LOOP;
+END test_mcp3202;
