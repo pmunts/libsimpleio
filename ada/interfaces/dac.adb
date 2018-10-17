668450c9a50110795beac95c1db620b796bee4ef
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-10-17 11:56:01 +0000

    Added DAC voltage output class.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15775 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/interfaces/dac.adb
@@ -0,0 +1,57 @@
+-- Abstract Digital to Analog Converter interface definitions
+
+-- Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Analog;
+WITH Voltage;
+
+USE TYPE Voltage.Volts;
+
+PACKAGE BODY DAC IS
+
+  -- Constructor
+
+  FUNCTION Create
+   (output    : Analog.Output;
+    reference : Voltage.Volts;
+    gain      : Voltage.Volts := 1.0) RETURN Voltage.Interfaces.Output IS
+
+  BEGIN
+    IF reference = 0.0 THEN
+      RAISE DAC_Error WITH "ERROR: reference voltage cannot be zero";
+    END IF;
+
+    IF gain = 0.0 THEN
+      RAISE DAC_Error WITH "ERROR: gain cannot be zero";
+    END IF;
+
+    RETURN NEW OutputSubclass'(output, reference/2.0**output.GetResolution/gain);
+  END Create;
+
+  -- Methods
+
+  PROCEDURE Put(Self : IN OUT OutputSubclass; V : Voltage.Volts) IS
+
+  BEGIN
+    Self.output.Put(Analog.Sample(V/Self.stepsize));
+  END Put;
+
+END DAC;
---
+++ ada/interfaces/dac.ads
@@ -0,0 +1,50 @@
+-- Abstract Digital to Analog Converter interface definitions
+
+-- Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Analog;
+WITH Voltage;
+
+PACKAGE DAC IS
+
+  DAC_Error : EXCEPTION;
+
+  TYPE OutputSubclass IS NEW Voltage.Interfaces.OutputInterface WITH PRIVATE;
+
+  -- Constructor
+
+  FUNCTION Create
+   (output    : Analog.Output;
+    reference : Voltage.Volts;
+    gain      : Voltage.Volts := 1.0) RETURN Voltage.Interfaces.Output;
+
+  -- Methods
+
+  PROCEDURE Put(Self : IN OUT OutputSubclass; V : Voltage.Volts);
+
+PRIVATE
+
+  TYPE OutputSubclass IS NEW Voltage.Interfaces.OutputInterface WITH RECORD
+    output   : Analog.Output;
+    stepsize : Voltage.Volts;
+  END RECORD;
+
+END DAC;
