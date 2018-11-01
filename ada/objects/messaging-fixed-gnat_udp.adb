727dab3a1d2ce210671a3ca96ee299cdb8966190
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-11-01 07:12:23 +0000

    Moved UDP messaging packages to libsimpleio
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15907 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/objects/messaging-fixed-gnat_udp.adb
@@ -0,0 +1,102 @@
+-- Fixed length message services implementing using GNAT.Sockets UDP
+-- Must be instantiated for each message size.
+
+-- Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Streams;
+WITH GNAT.Sockets;
+
+USE TYPE Ada.Streams.Stream_Element_Offset;
+USE TYPE GNAT.Sockets.Sock_Addr_Type;
+
+PACKAGE BODY Messaging.Fixed.GNAT_UDP IS
+
+  FUNCTION Create(hostname : String; port : Positive) RETURN Messenger IS
+
+    socket : GNAT.Sockets.Socket_Type;
+    peer   : GNAT.Sockets.Sock_Addr_Type;
+
+  BEGIN
+    GNAT.Sockets.Create_Socket(socket, GNAT.Sockets.Family_Inet,
+      GNAT.Sockets.Socket_Datagram);
+
+    BEGIN
+      peer.Addr := GNAT.Sockets.Inet_Addr(hostname);
+    EXCEPTION
+      WHEN OTHERS => peer.Addr :=
+        GNAT.Sockets.Addresses(GNAT.Sockets.Get_Host_By_Name(hostname));
+    END;
+
+    peer.Port := GNAT.Sockets.Port_Type(port);
+
+    RETURN NEW MessengerSubclass'(socket, peer);
+  END Create;
+
+  PROCEDURE Send(self : MessengerSubclass; msg : Message) IS
+
+    Data : Ada.Streams.Stream_Element_Array(0 .. Message'Length - 1);
+    Last : Ada.Streams.Stream_Element_Offset;
+
+  BEGIN
+    FOR i IN Data'Range LOOP
+      Data(i) := Ada.Streams.Stream_Element(msg(Integer(i)));
+    END LOOP;
+
+    GNAT.Sockets.Send_Socket(self.socket, Data, Last, self.peer);
+  END Send;
+
+  PROCEDURE Receive(self : MessengerSubclass; msg : OUT Message) IS
+
+    Data : Ada.Streams.Stream_Element_Array(0 .. Message'Length - 1);
+    Last : Ada.Streams.Stream_Element_Offset;
+    From : GNAT.Sockets.Sock_Addr_Type;
+
+  BEGIN
+    GNAT.Sockets.Receive_Socket(self.socket, Data, Last, From);
+
+    IF From /= self.peer THEN
+      RAISE GNAT.Sockets.Socket_Error WITH "Message not from peer node";
+    END IF;
+
+    FOR i IN Data'Range LOOP
+      msg(Integer(i)) := Byte(Data(i));
+    END LOOP;
+  END Receive;
+
+  PROCEDURE Transaction
+   (self     : MessengerSubclass;
+    cmd      : IN Message;
+    resp     : OUT Message) IS
+
+  BEGIN
+    self.Send(cmd);
+    self.Receive(resp);
+  END Transaction;
+
+  FUNCTION fd(self : MessengerSubclass) RETURN Integer IS
+
+  BEGIN
+    RETURN GNAT.Sockets.To_C(self.socket);
+  END fd;
+
+BEGIN
+  GNAT.Sockets.Initialize;
+END Messaging.Fixed.GNAT_UDP;
---
+++ ada/objects/messaging-fixed-gnat_udp.ads
@@ -0,0 +1,59 @@
+-- Fixed length message services implementing using GNAT.Sockets UDP
+-- Must be instantiated for each message size.
+
+-- Copyright (C)2017-2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+PRIVATE WITH GNAT.Sockets;
+
+GENERIC
+
+PACKAGE Messaging.Fixed.GNAT_UDP IS
+
+  TYPE MessengerSubclass IS NEW MessengerInterface WITH PRIVATE;
+
+  FUNCTION Create
+   (hostname : String;
+    port     : Positive) RETURN Messenger;
+
+  PROCEDURE Send
+   (self     : MessengerSubclass;
+    msg      : Message);
+
+  PROCEDURE Receive
+   (self     : MessengerSubclass;
+    msg      : OUT Message);
+
+  PROCEDURE Transaction
+   (self     : MessengerSubclass;
+    cmd      : IN Message;
+    resp     : OUT Message);
+
+  FUNCTION fd
+   (self     : MessengerSubclass) RETURN Integer;
+
+PRIVATE
+
+  TYPE MessengerSubclass IS NEW MessengerInterface WITH RECORD
+    socket : GNAT.Sockets.Socket_Type;
+    peer   : GNAT.Sockets.Sock_Addr_type;
+  END RECORD;
+
+END Messaging.Fixed.GNAT_UDP;
