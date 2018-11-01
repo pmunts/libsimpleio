2264f71d8fa25a710b9b87bb91eb47a81c5c05d9
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-11-01 08:15:17 +0000

    Moved Remote I/O client and server packages into separate subdirecories remoteio/client/ and remoteio/server/
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15909 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/include/libsimpleio.mk
+++ ada/include/libsimpleio.mk
@@ -24,5 +24,4 @@ ADA_INCLUDES	+= -I$(LIBSIMPLEIO)/ada/bindings
 ADA_INCLUDES	+= -I$(LIBSIMPLEIO)/ada/devices
 ADA_INCLUDES	+= -I$(LIBSIMPLEIO)/ada/interfaces
 ADA_INCLUDES	+= -I$(LIBSIMPLEIO)/ada/objects
-ADA_INCLUDES	+= -I$(LIBSIMPLEIO)/ada/remoteio
 ADA_INCLUDES	+= -I$(LIBSIMPLEIO)/ada/packages
