656244fca3019c91f4fd5eb105d8f2b86dba7843
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-11-05 15:04:20 +0000

    Started adding support for fixed-length messaging over Stream Framing Protocol channels.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@15951 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/objects/messaging-fixed-libsimpleio_stream.adb
@@ -0,0 +1,121 @@
+-- Fixed length message services using the libsimpleio Stream Framing Protocol
+-- library.  Must be instantiated for each message size.
+
+-- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH errno;
+WITH libLinux;
+WITH libStream;
+
+PACKAGE BODY Messaging.Fixed.libsimpleio_stream IS
+
+  MaxFrameSize : CONSTANT Natural := 2*MessageSize + 8;
+
+  TYPE FrameBuffer IS ARRAY (1 .. MaxFrameSize) OF Byte;
+
+  -- Constructor
+
+  FUNCTION Create
+   (fd        : Integer;
+    timeoutms : Integer := 1000) RETURN Messenger IS
+
+  BEGIN
+    RETURN NEW MessengerSubclass'(fd, timeoutms);
+  END Create;
+
+  -- Send a message
+
+  PROCEDURE Send(Self : MessengerSubclass; msg : Message) IS
+
+    frame     : FrameBuffer;
+    framesize : Integer;
+    error     : Integer;
+    count     : Integer;
+
+  BEGIN
+    libStream.Encode(msg'Address, msg'Length, frame'Address, Frame'Length,
+      framesize, error);
+
+    IF error /= 0 THEN
+      RAISE Message_Error WITH "libStream.Encode() failed, " &
+        errno.strerror(error);
+    END IF;
+
+    libLinux.Write(Self.fd, frame'Address, framesize, count, error);
+
+    IF error /= 0 THEN
+      RAISE Message_Error WITH "libLinux.Write() failed, " &
+        errno.strerror(error);
+    END IF;
+  END Send;
+
+  -- Receive a message
+
+  PROCEDURE Receive(Self : MessengerSubclass; msg : OUT Message) IS
+
+    error : Integer;
+    count : Integer;
+
+  BEGIN
+    IF Self.timeoutms > 0 THEN
+      DECLARE
+
+        files   : libLinux.FilesType(0 .. 0)   := (OTHERS => Self.fd);
+        events  : libLinux.EventsType(0 .. 0)  := (OTHERS => libLinux.POLLIN);
+        results : libLinux.ResultsType(0 .. 0) := (OTHERS =>0);
+
+      BEGIN
+        libLinux.Poll(1, files, events, results, Self.timeoutms, error);
+
+        IF error = errno.EAGAIN THEN
+          RAISE Timeout_Error WITH "libLinux.Poll() timed out";
+        END IF;
+
+        IF error /= 0 THEN
+          RAISE Message_Error WITH "libLinux.Poll() failed, " &
+            errno.strerror(error);
+        END IF;
+      END;
+    END IF;
+
+    libLinux.Read(Self.fd, msg'Address, msg'Length, count, error);
+
+    IF error /= 0 THEN
+      RAISE Message_Error WITH "libLinux.Read() failed, " &
+        errno.strerror(error);
+    END IF;
+
+    IF count /= msg'Length THEN
+      RAISE Message_Error WITH "libLinux.Read() failed, " &
+        "expected" & Integer'Image(msg'Length) & " bytes " &
+        "but read" & Integer'Image(count) & " bytes";
+    END IF;
+  END Receive;
+
+ -- Retrieve the underlying Linux file descriptor
+
+  FUNCTION fd(Self : MessengerSubclass) RETURN Integer IS
+
+  BEGIN
+    RETURN Self.fd;
+  END fd;
+
+END Messaging.Fixed.libsimpleio_stream;
