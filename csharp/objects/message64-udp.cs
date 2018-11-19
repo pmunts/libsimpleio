45cf169abf88d63f35875942ed08dce342ea0298
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-11-19 09:22:41 +0000

    Added support for 64-byte message services over UDP transport.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16086 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ csharp/objects/message64-udp.cs
@@ -0,0 +1,101 @@
+// 64-byte Message Services using UDP
+
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace IO.Objects.Message64.UDP
+{
+    /// <summary>
+    /// 64-Byte Message Transport Client Services using UDP
+    /// (User Datagram Protocol).
+    /// </summary>
+    public class Messenger : IO.Interfaces.Message64.Messenger
+    {
+        private readonly Socket s;
+        private readonly IPEndPoint server;
+
+        /// <summary>
+        /// Constructor for a 64-byte Messenger instance using UDP.
+        /// </summary>
+        /// <param name="host">UDP server domain name or IP address.</param>
+        /// <param name="port">UDP server port number.</param>
+        /// <param name="timeoutms">Receive timeout in milliseconds.  Zero
+        /// indicates wait forever.</param>
+        public Messenger(string host, int port, int timeoutms = 1000)
+        {
+            // Validate parameters
+
+            if ((port < 0) || (port > 65535))
+                throw new System.Exception("The port parameter is out of range");
+
+            if (timeoutms < 0)
+                throw new System.Exception("The timeoutms parameter is out of range");
+
+            IPHostEntry hostinfo = Dns.GetHostEntry(host);
+            IPAddress hostaddr = hostinfo.AddressList[0];
+
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+                ProtocolType.Udp);
+
+            s.ReceiveTimeout = timeoutms;
+        
+            server = new IPEndPoint(hostaddr, port);
+        }
+
+        /// <summary>
+        /// Send a 64-byte command message to a raw HID device.
+        /// </summary>
+        /// <param name="cmd">64-byte command message.</param>
+        public void Send(IO.Interfaces.Message64.Message cmd)
+        {
+            this.s.SendTo(cmd.payload, this.server);
+        }
+
+        /// <summary>
+        /// Receive a 64-byte response message from a raw HID device.
+        /// </summary>
+        /// <param name="resp">64-byte response message.</param>
+        public void Receive(IO.Interfaces.Message64.Message resp)
+        {
+            EndPoint sender = new IPEndPoint(0, 0);
+
+            this.s.ReceiveFrom(resp.payload, ref sender);
+
+            if (!this.server.Equals(sender))
+                throw new System.Exception("Message not from server node");
+        }
+
+        /// <summary>
+        /// Send a 64-byte command message and receive a 64-byte response
+        /// message.
+        /// </summary>
+        /// <param name="cmd">64-byte command message.</param>
+        /// <param name="resp">64-byte response message.</param>
+        public void Transaction(IO.Interfaces.Message64.Message cmd,
+            IO.Interfaces.Message64.Message resp)
+        {
+            this.Send(cmd);
+            this.Receive(resp);
+        }
+    }
+}
---
+++ csharp/programs/libremoteio/core/test_query_udp/test_query_udp.cs
@@ -0,0 +1,85 @@
+// UDP Remote I/O Device Information Query Test
+
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+
+namespace test_query
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nUDP Remote I/O Device Information Query Test\n");
+
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: test_query_udp <hostname>");
+                Environment.Exit(1);
+            }
+
+            IO.Interfaces.Message64.Messenger m =
+                new IO.Objects.Message64.UDP.Messenger(args[0], 8087);
+
+            IO.Remote.Device dev = new IO.Remote.Device(m);
+
+            Console.WriteLine(dev.Version);
+            Console.WriteLine(dev.Capabilities);
+            Console.WriteLine();
+
+            // Display the available ADC inputs
+
+            Console.Write("ADC inputs:  ");
+
+            foreach (int input in dev.ADC_Available())
+                Console.Write(input.ToString() + " ");
+
+            Console.WriteLine();
+
+            // Display the available GPIO pins
+
+            Console.Write("GPIO Pins:   ");
+
+            foreach (int pin in dev.GPIO_Available())
+              Console.Write(pin.ToString() + " ");
+
+            Console.WriteLine();
+
+            // Display the available I2C buses
+
+            Console.Write("I2C buses:   ");
+
+            foreach (int bus in dev.I2C_Available())
+                Console.Write(bus.ToString() + " ");
+
+            Console.WriteLine();
+
+            // Display the available SPI devices
+
+            Console.Write("SPI devices: ");
+
+            foreach (int bus in dev.SPI_Available())
+                Console.Write(bus.ToString() + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
