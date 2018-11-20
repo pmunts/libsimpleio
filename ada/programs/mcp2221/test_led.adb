84d9901d3cf909f4c2dddaef75be3689cf5bb07b
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-11-20 09:09:47 +0000

    Added LED toggle test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16100 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/programs/mcp2221/test_led.adb
@@ -0,0 +1,55 @@
+-- MCP2221 LED toggle test
+
+-- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+WITH GPIO;
+WITH MCP2221.GPIO;
+WITH MCP2221.hidapi;
+
+PROCEDURE test_led IS
+
+  dev : MCP2221.Device;
+  LED : GPIO.Pin;
+
+BEGIN
+  New_Line;
+  Put_Line("MCP2221 LED Toggle Test");
+  New_Line;
+
+  -- Open the MCP2221 device
+
+  dev := MCP2221.hidapi.Create;
+
+  -- Create GPIO pin object
+
+  LED := MCP2221.GPIO.Create(dev, 0, GPIO.Output, False);
+
+  -- Toggle the LED
+
+  Put_Line("Press CONTROL-C to exit...");
+
+  LOOP
+    LED.Put(NOT LED.Get);
+    DELAY 0.5;
+  END LOOP;
+END test_led;
