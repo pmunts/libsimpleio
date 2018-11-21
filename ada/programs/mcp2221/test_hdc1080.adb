04e0b258f97ada6bf9ec9314bf2cbb526d317393
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-11-21 14:28:36 +0000

    Added HDC1080 temperature/humidity sensor test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16136 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/programs/mcp2221/test_hdc1080.adb
@@ -0,0 +1,70 @@
+-- HDC1080 Temperature/Humidity Sensor Test
+
+-- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+WITH Ada.Integer_Text_IO; USE Ada.Integer_Text_IO;
+
+WITH HDC1080;
+WITH HID.hidapi;
+WITH Humidity;
+WITH I2C;
+WITH MCP2221.hidapi;
+WITH MCP2221.I2C;
+WITH Temperature;
+
+PROCEDURE test_hdc1080 IS
+
+  dev    : MCP2221.Device;
+  bus    : I2C.Bus;
+  sensor : HDC1080.Device;
+
+BEGIN
+  New_Line;
+  Put_Line("HDC1080 Temperature/Humidity Sensor Test");
+  New_Line;
+
+  dev    := MCP2221.hidapi.Create;
+  bus    := MCP2221.I2C.Create(dev);
+  sensor := HDC1080.Create(bus);
+
+  Put("Manufacturer ID: ");
+  Put(sensor.ManufacturerID, 8, 16);
+  New_Line;
+
+  Put("Device ID:       ");
+  Put(sensor.DeviceID, 8, 16);
+  New_Line;
+
+  New_Line;
+  Put_Line("Press CONTROL-C to exit...");
+  New_Line;
+
+  LOOP
+    Put("Temperature: ");
+    Temperature.Celsius_IO.Put(sensor.Get, 0, 1, 0);
+    Put("  Humidity: ");
+    Humidity.Relative_IO.Put(sensor.Get, 0, 1, 0);
+    New_Line;
+
+    DELAY 1.0;
+  END LOOP;
+END test_hdc1080;
