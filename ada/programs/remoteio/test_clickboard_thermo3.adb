56f802d91fc81e8b001d6a1a1d163513d446600a
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-12-04 11:16:09 +0000

    Copied correct source
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16269 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/programs/remoteio/test_clickboard_thermo3.adb
+++ ada/programs/remoteio/test_clickboard_thermo3.adb
@@ -1,4 +1,4 @@
--- Mikroelektronika Thermo3 Click Temperature Sensor Test
+-- Mikroelektronika Thermo3 Click Temperature/Humidity Sensor Test
 
 -- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
 --
@@ -22,24 +22,20 @@
 
 WITH Ada.Text_IO; USE Ada.Text_IO;
 
-WITH ClickBoard.Thermo3.RemoteIO;
-WITH HID.hidapi;
-WITH RemoteIO.Client;
+WITH ClickBoard.Thermo3.SimpleIO;
 WITH TMP102;
 WITH Temperature;
 
 PROCEDURE test_clickboard_thermo3 IS
 
-  remdev : RemoteIO.Client.Device;
   sensor : TMP102.Device;
 
 BEGIN
   New_Line;
-  Put_Line("Mikroelektronika Thermo3 Click Temperature Sensor Test");
+  Put_Line("Mikroelektronika Thermo3 Click Temperature/Humidity Sensor Test");
   New_Line;
 
-  remdev := RemoteIO.Client.Create(HID.hidapi.Create);
-  sensor := ClickBoard.Thermo3.RemoteIO.Create(remdev, socknum => 1);
+  sensor := ClickBoard.Thermo3.SimpleIO.Create(socknum => 1);
 
   Put_Line("Press CONTROL-C to exit...");
   New_Line;
