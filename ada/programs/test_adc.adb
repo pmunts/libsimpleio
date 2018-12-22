846404229f263125987834c0045bc4f17f1b42fb
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-12-22 14:01:51 +0000

    Added ADC input test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16443 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/programs/test_adc.adb
@@ -0,0 +1,56 @@
+-- ADC input test using kernel and libsimpleio ADC services
+
+-- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+WITH Ada.Integer_Text_IO; USE Ada.Integer_Text_IO;
+
+WITH Analog;
+WITH ADC.libsimpleio;
+
+PROCEDURE test_adc IS
+
+  chip    : Natural;
+  channel : Natural;
+
+  ADC0 : Analog.Input;
+
+BEGIN
+  Put_Line("ADC Input Test");
+  New_Line;
+
+  Put("Enter ADC chip number:    ");
+  Get(chip);
+
+  Put("Enter ADC channel number: ");
+  Get(channel);
+
+  -- Create ADC input object
+
+  ADC0 := ADC.libsimpleio.Create(chip, channel, 12);
+
+  -- Sample the analog input
+
+  LOOP
+    Put_Line("Sample:" & Analog.Sample'Image(ADC0.Get));
+    DELAY 1.0;
+  END LOOP;
+END test_adc;
