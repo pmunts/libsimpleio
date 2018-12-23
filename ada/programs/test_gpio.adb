900d37eab8849d952f5f3951395c7c20e269a818
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-12-23 16:46:29 +0000

    Added GPIO output toggle test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16450 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/programs/test_gpio.adb
@@ -0,0 +1,54 @@
+-- GPIO output test using kernel and libsimpleio GPIO services
+
+-- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+WITH Ada.Integer_Text_IO; USE Ada.Integer_Text_IO;
+
+WITH GPIO.libsimpleio;
+
+PROCEDURE test_gpio IS
+
+  chip    : Natural;
+  channel : Natural;
+
+  GPIO0 : GPIO.Pin;
+
+BEGIN
+  Put_Line("GPIO Output Test");
+  New_Line;
+
+  Put("Enter GPIO chip number:     ");
+  Get(chip);
+
+  Put("Enter GPIO channel number:  ");
+  Get(channel);
+
+  -- Create GPIO output object
+
+  GPIO0 := GPIO.libsimpleio.Create(chip, channel, GPIO.Output);
+
+  -- Toggle the GPIO output
+
+  LOOP
+    GPIO0.Put(NOT GPIO0.Get);
+  END LOOP;
+END test_gpio;
