a6cf3cfa1b0bf587d2b83313040c0484dba0dd0c
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2018-12-28 17:26:18 +0000

    Added package Servo.PWM
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16471 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/objects/servo-pwm.adb
@@ -0,0 +1,56 @@
+-- Servo output services using a PWM output
+
+-- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH PWM;
+
+PACKAGE BODY Servo.PWM IS
+
+  -- Servo output object constructor
+
+  FUNCTION Create
+   (output    : Standard.PWM.Interfaces.Output;
+    frequency : Positive := 50;
+    position  : Servo.Position := Servo.NeutralPosition)
+    RETURN Servo.Interfaces.Output IS
+
+    outp : Servo.Interfaces.Output;
+
+  BEGIN
+    outp := NEW OutputSubclass'(output, 1000000000/frequency);
+    outp.Put(position);
+    RETURN outp;
+  END Create;
+
+  -- Servo output write method
+
+  PROCEDURE Put
+   (Self      : IN OUT OutputSubclass;
+    position  : Servo.Position) IS
+
+    ontime : Positive; -- nanoseconds
+
+  BEGIN
+    ontime := 1500000 + Positive(500000.0*position + 0.5);
+    Self.output.Put(Standard.PWM.DutyCycle(Float(ontime)/Float(Self.period)*100.0));
+  END Put;
+
+END Servo.PWM;
---
+++ ada/objects/servo-pwm.ads
@@ -0,0 +1,52 @@
+-- Servo output services using a PWM output
+
+-- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH PWM;
+
+PACKAGE Servo.PWM IS
+
+  -- Type definitions
+
+  TYPE OutputSubclass IS NEW Servo.Interfaces.OutputInterface WITH PRIVATE;
+
+  -- Servo output object constructor
+
+  FUNCTION Create
+   (output    : Standard.PWM.Interfaces.Output;
+    frequency : Positive := 50;
+    position  : Servo.Position := Servo.NeutralPosition)
+    RETURN Servo.Interfaces.Output;
+
+  -- Servo output write method
+
+  PROCEDURE Put
+   (Self      : IN OUT OutputSubclass;
+    position  : Servo.Position);
+
+PRIVATE
+
+  TYPE OutputSubclass IS NEW Standard.Servo.Interfaces.OutputInterface WITH RECORD
+    output : Standard.PWM.Interfaces.Output;
+    period : Positive; -- nanoseconds
+  END RECORD;
+
+END Servo.PWM;
