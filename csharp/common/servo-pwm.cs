b3ee394174e069d0b2ea5cb241a4acdeb9b0d842
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2019-01-05 18:59:24 +0000

    Added support for Remote I/O Protocol PWM and servo outputs.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16496 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ csharp/common/servo-pwm.cs
@@ -0,0 +1,58 @@
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+namespace IO.Objects.Servo.PWM
+{
+    /// <summary>
+    /// Encapsulates servo outputs using PWM outputs.
+    /// </summary>
+    public class Output : IO.Interfaces.Servo.Output
+    {
+        private IO.Interfaces.PWM.Output pwm;
+        private int period;
+
+        /// <summary>
+        /// Constructor for a single servo output.
+        /// </summary>
+        /// <param name="pwm">PWM output instance.</param>
+        /// <param name="freq">PWM pulse frequency.</param>
+        /// <param name="position">Initial servo position.</param>
+        public Output(IO.Interfaces.PWM.Output pwm, int freq = 50,
+          double position = IO.Interfaces.Servo.Positions.Neutral)
+        {
+            this.pwm = pwm;
+            this.period = 1000000000 / freq;
+            this.position = position;
+        }
+
+        /// <summary>
+        /// Write-only property for setting the servo position.
+        /// Allowed values are -1.0 to +1.0.
+        /// </summary>
+        public double position
+        {
+            set
+            {
+                int ontime = 1500000 + (int)(500000.0 * value);
+                this.pwm.dutycycle = ((double)ontime) / ((double)this.period) * 100.0;
+            }
+        }
+    }
+}
