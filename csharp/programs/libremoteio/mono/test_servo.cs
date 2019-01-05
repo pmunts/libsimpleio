f5bd1aa0d5ed27478e454144a1e522679aeea271
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2019-01-05 19:34:03 +0000

    Added PWM and servo output tests.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16499 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ csharp/programs/libremoteio/mono/test_servo.cs
@@ -0,0 +1,63 @@
+// Servo output test
+
+// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+using System.Collections;
+
+namespace test_servo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("\nServo Output Test\n");
+
+      IO.Remote.Device dev =
+        new IO.Remote.Device(new IO.Objects.USB.HID.Messenger());
+
+      Console.Write("Channels:");
+
+      foreach (int output in dev.PWM_Available())
+        Console.Write(" " + output.ToString());
+
+      Console.WriteLine();
+
+      ArrayList S = new ArrayList();
+
+      foreach (int c in dev.PWM_Available())
+        S.Add(new IO.Objects.Servo.PWM.Output(new IO.Remote.PWM(dev, c, 50), 50));
+
+      for (;;)
+      {
+        int n;
+
+        for (n = -300; n <= 300; n++)
+          foreach (IO.Interfaces.Servo.Output output in S)
+            output.position = n/300.0;
+
+        for (n = 300; n >= -300; n--)
+          foreach (IO.Interfaces.Servo.Output output in S)
+            output.position = n/300.0;
+      }
+    }
+  }
+}
