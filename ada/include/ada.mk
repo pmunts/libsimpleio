67a499fff435356d59c9620b61891de241a02726
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2019-01-11 07:39:58 +0000

    Cosmetic rearranging
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16574 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/include/ada.mk
+++ ada/include/ada.mk
@@ -24,12 +24,6 @@
 
 .SECONDARY:
 
-# General toolchain definitions
-
-ADA_OBJ		?= $(shell pwd)/obj
-
-GNATENV		+= ADA_SRC=$(ADA_SRC) ADA_OBJ=$(ADA_OBJ)
-
 # Definitions for MuntsOS
 
 ifneq ($(BOARDNAME),)
@@ -37,9 +31,6 @@ ifeq  ($(BOARDBASE),)
 EMBLINUXBASE	?= $(HOME)/arm-linux-mcu
 include $(EMBLINUXBASE)/include/$(BOARDNAME).mk
 endif
-else
-# Native compile
-LIBSIMPLEIO	?= /usr/local/share/libsimpleio
 endif
 
 # Definitions for Microsoft Windows
@@ -47,6 +38,11 @@ endif
 ifeq ($(OS),Windows_NT)
 EXESUFFIX	= .exe
 endif
+
+# General toolchain definitions
+
+ADA_OBJ		?= $(shell pwd)/obj
+GNATENV		+= ADA_SRC=$(ADA_SRC) ADA_OBJ=$(ADA_OBJ)
 
 # Definitions for gnatmake
 
