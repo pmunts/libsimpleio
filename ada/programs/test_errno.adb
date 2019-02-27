8aa897d777105a6b999fd14bd5f197aa159662dd
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2019-02-27 11:14:18 +0000

    Added errno package test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@16859 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/programs/test_errno.adb
@@ -0,0 +1,46 @@
+-- Test the errno package
+
+-- Copyright (C)2019, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+WITH errno;
+
+PROCEDURE test_errno IS
+
+  FUNCTION unlink(filename : String) RETURN Integer;
+    PRAGMA Import(C, unlink);
+
+  ret : Integer;
+  err : Integer;
+
+BEGIN
+  New_Line;
+  Put_Line("errno Test");
+  New_Line;
+
+  ret := unlink("bogus" & ASCII.NUL);
+  err := errno.Get;
+
+  Put_Line("ret is " & Integer'Image(ret));
+  Put_Line("err is " & Integer'Image(err));
+  Put_Line(errno.strerror(err));
+END test_errno;
