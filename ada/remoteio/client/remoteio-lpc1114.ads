2833e82e1098dd3194afc756c2657deb8341cd00
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2019-05-18 12:47:33 +0000

    Added LPC1114 timer constants
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17283 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/remoteio/client/remoteio-lpc1114.ads
+++ ada/remoteio/client/remoteio-lpc1114.ads
@@ -22,7 +22,7 @@
 -- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
-WITH Interfaces;
+WITH Interfaces; USE Interfaces;
 
 WITH Message64;
 WITH RemoteIO.Abstract_Device;
@@ -70,94 +70,129 @@ PACKAGE RemoteIO.LPC1114 IS
   -- Raspberry Pi LPC1114 I/O Processor SPI Agent Firmware command structure
 
   TYPE SPIAGENT_COMMAND_MSG_t IS RECORD
-    command : Interfaces.Unsigned_32;
-    pin     : Interfaces.Unsigned_32;
-    data    : Interfaces.Unsigned_32;
+    command : Unsigned_32;
+    pin     : Unsigned_32;
+    data    : Unsigned_32;
   END record;
 
   -- Raspberry Pi LPC1114 I/O Processor SPI Agent Firmware response structure
 
   TYPE SPIAGENT_RESPONSE_MSG_t IS RECORD
-    command : Interfaces.Unsigned_32;
-    pin     : Interfaces.Unsigned_32;
-    data    : Interfaces.Unsigned_32;
-    error   : Interfaces.Unsigned_32;
+    command : Unsigned_32;
+    pin     : Unsigned_32;
+    data    : Unsigned_32;
+    error   : Unsigned_32;
   END record;
 
   -- Raspberry Pi LPC1114 I/O Processor SPI Agent Firmware commands
 
-  SPIAGENT_CMD_NOP                          : CONSTANT Interfaces.Unsigned_32 := 0;
-  SPIAGENT_CMD_LOOPBACK                     : CONSTANT Interfaces.Unsigned_32 := 1;
-  SPIAGENT_CMD_CONFIGURE_ANALOG_INPUT       : CONSTANT Interfaces.Unsigned_32 := 2;
-  SPIAGENT_CMD_CONFIGURE_GPIO_INPUT         : CONSTANT Interfaces.Unsigned_32 := 3;
-  SPIAGENT_CMD_CONFIGURE_GPIO_OUTPUT        : CONSTANT Interfaces.Unsigned_32 := 4;
-  SPIAGENT_CMD_CONFIGURE_PWM_OUTPUT         : CONSTANT Interfaces.Unsigned_32 := 5;
-  SPIAGENT_CMD_GET_ANALOG                   : CONSTANT Interfaces.Unsigned_32 := 6;
-  SPIAGENT_CMD_GET_GPIO                     : CONSTANT Interfaces.Unsigned_32 := 7;
-  SPIAGENT_CMD_PUT_GPIO                     : CONSTANT Interfaces.Unsigned_32 := 8;
-  SPIAGENT_CMD_PUT_PWM                      : CONSTANT Interfaces.Unsigned_32 := 9;
-  SPIAGENT_CMD_CONFIGURE_GPIO_INTERRUPT     : CONSTANT Interfaces.Unsigned_32 := 10;
-  SPIAGENT_CMD_CONFIGURE_GPIO               : CONSTANT Interfaces.Unsigned_32 := 11;
-  SPIAGENT_CMD_PUT_LEGORC                   : CONSTANT Interfaces.Unsigned_32 := 12;
-  SPIAGENT_CMD_GET_SFR                      : CONSTANT Interfaces.Unsigned_32 := 13;
-  SPIAGENT_CMD_PUT_SFR                      : CONSTANT Interfaces.Unsigned_32 := 14;
-  SPIAGENT_CMD_CONFIGURE_TIMER_MODE         : CONSTANT Interfaces.Unsigned_32 := 15;
-  SPIAGENT_CMD_CONFIGURE_TIMER_PRESCALER    : CONSTANT Interfaces.Unsigned_32 := 16;
-  SPIAGENT_CMD_CONFIGURE_TIMER_CAPTURE      : CONSTANT Interfaces.Unsigned_32 := 17;
-  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH0       : CONSTANT Interfaces.Unsigned_32 := 18;
-  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH1       : CONSTANT Interfaces.Unsigned_32 := 19;
-  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH2       : CONSTANT Interfaces.Unsigned_32 := 20;
-  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH3       : CONSTANT Interfaces.Unsigned_32 := 21;
-  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH0_VALUE : CONSTANT Interfaces.Unsigned_32 := 22;
-  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH1_VALUE : CONSTANT Interfaces.Unsigned_32 := 23;
-  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH2_VALUE : CONSTANT Interfaces.Unsigned_32 := 24;
-  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH3_VALUE : CONSTANT Interfaces.Unsigned_32 := 25;
-  SPIAGENT_CMD_GET_TIMER_VALUE              : CONSTANT Interfaces.Unsigned_32 := 26;
-  SPIAGENT_CMD_GET_TIMER_CAPTURE            : CONSTANT Interfaces.Unsigned_32 := 27;
-  SPIAGENT_CMD_GET_TIMER_CAPTURE_DELTA      : CONSTANT Interfaces.Unsigned_32 := 28;
-  SPIAGENT_CMD_INIT_TIMER                   : CONSTANT Interfaces.Unsigned_32 := 29;
+  SPIAGENT_CMD_NOP                          : CONSTANT Unsigned_32 := 0;
+  SPIAGENT_CMD_LOOPBACK                     : CONSTANT Unsigned_32 := 1;
+  SPIAGENT_CMD_CONFIGURE_ANALOG_INPUT       : CONSTANT Unsigned_32 := 2;
+  SPIAGENT_CMD_CONFIGURE_GPIO_INPUT         : CONSTANT Unsigned_32 := 3;
+  SPIAGENT_CMD_CONFIGURE_GPIO_OUTPUT        : CONSTANT Unsigned_32 := 4;
+  SPIAGENT_CMD_CONFIGURE_PWM_OUTPUT         : CONSTANT Unsigned_32 := 5;
+  SPIAGENT_CMD_GET_ANALOG                   : CONSTANT Unsigned_32 := 6;
+  SPIAGENT_CMD_GET_GPIO                     : CONSTANT Unsigned_32 := 7;
+  SPIAGENT_CMD_PUT_GPIO                     : CONSTANT Unsigned_32 := 8;
+  SPIAGENT_CMD_PUT_PWM                      : CONSTANT Unsigned_32 := 9;
+  SPIAGENT_CMD_CONFIGURE_GPIO_INTERRUPT     : CONSTANT Unsigned_32 := 10;
+  SPIAGENT_CMD_CONFIGURE_GPIO               : CONSTANT Unsigned_32 := 11;
+  SPIAGENT_CMD_PUT_LEGORC                   : CONSTANT Unsigned_32 := 12;
+  SPIAGENT_CMD_GET_SFR                      : CONSTANT Unsigned_32 := 13;
+  SPIAGENT_CMD_PUT_SFR                      : CONSTANT Unsigned_32 := 14;
+  SPIAGENT_CMD_CONFIGURE_TIMER_MODE         : CONSTANT Unsigned_32 := 15;
+  SPIAGENT_CMD_CONFIGURE_TIMER_PRESCALER    : CONSTANT Unsigned_32 := 16;
+  SPIAGENT_CMD_CONFIGURE_TIMER_CAPTURE      : CONSTANT Unsigned_32 := 17;
+  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH0       : CONSTANT Unsigned_32 := 18;
+  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH1       : CONSTANT Unsigned_32 := 19;
+  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH2       : CONSTANT Unsigned_32 := 20;
+  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH3       : CONSTANT Unsigned_32 := 21;
+  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH0_VALUE : CONSTANT Unsigned_32 := 22;
+  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH1_VALUE : CONSTANT Unsigned_32 := 23;
+  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH2_VALUE : CONSTANT Unsigned_32 := 24;
+  SPIAGENT_CMD_CONFIGURE_TIMER_MATCH3_VALUE : CONSTANT Unsigned_32 := 25;
+  SPIAGENT_CMD_GET_TIMER_VALUE              : CONSTANT Unsigned_32 := 26;
+  SPIAGENT_CMD_GET_TIMER_CAPTURE            : CONSTANT Unsigned_32 := 27;
+  SPIAGENT_CMD_GET_TIMER_CAPTURE_DELTA      : CONSTANT Unsigned_32 := 28;
+  SPIAGENT_CMD_INIT_TIMER                   : CONSTANT Unsigned_32 := 29;
 
   -- Raspberry Pi LPC1114 I/O Processor General Purpose Input/Output pins
 
-  LPC1114_GPIO0       : CONSTANT Interfaces.Unsigned_32 := 12; -- aka P1.0
-  LPC1114_GPIO1       : CONSTANT Interfaces.Unsigned_32 := 13; -- aka P1.1
-  LPC1114_GPIO2       : CONSTANT Interfaces.Unsigned_32 := 14; -- aka P1.2
-  LPC1114_GPIO3       : CONSTANT Interfaces.Unsigned_32 := 15; -- aka P1.3
-  LPC1114_GPIO4       : CONSTANT Interfaces.Unsigned_32 := 16; -- aka P1.4
-  LPC1114_GPIO5       : CONSTANT Interfaces.Unsigned_32 := 17; -- aka P1.5
-  LPC1114_GPIO6       : CONSTANT Interfaces.Unsigned_32 := 20; -- aka P1.8
-  LPC1114_GPIO7       : CONSTANT Interfaces.Unsigned_32 := 21; -- aka P1.9
-  LPC1114_LED         : CONSTANT Interfaces.Unsigned_32 := 7;  -- aka P0.7
+  LPC1114_GPIO0       : CONSTANT Unsigned_32 := 12; -- aka P1.0
+  LPC1114_GPIO1       : CONSTANT Unsigned_32 := 13; -- aka P1.1
+  LPC1114_GPIO2       : CONSTANT Unsigned_32 := 14; -- aka P1.2
+  LPC1114_GPIO3       : CONSTANT Unsigned_32 := 15; -- aka P1.3
+  LPC1114_GPIO4       : CONSTANT Unsigned_32 := 16; -- aka P1.4
+  LPC1114_GPIO5       : CONSTANT Unsigned_32 := 17; -- aka P1.5
+  LPC1114_GPIO6       : CONSTANT Unsigned_32 := 20; -- aka P1.8
+  LPC1114_GPIO7       : CONSTANT Unsigned_32 := 21; -- aka P1.9
+  LPC1114_LED         : CONSTANT Unsigned_32 := 7;  -- aka P0.7
 
    -- Raspberry Pi LPC1114 I/O Processor Analog input pins
 
-  LPC1114_AD1         : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO0;
-  LPC1114_AD2         : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO1;
-  LPC1114_AD3         : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO2;
-  LPC1114_AD4         : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO3;
-  LPC1114_AD5         : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO4;
+  LPC1114_AD1         : CONSTANT Unsigned_32 := LPC1114_GPIO0;
+  LPC1114_AD2         : CONSTANT Unsigned_32 := LPC1114_GPIO1;
+  LPC1114_AD3         : CONSTANT Unsigned_32 := LPC1114_GPIO2;
+  LPC1114_AD4         : CONSTANT Unsigned_32 := LPC1114_GPIO3;
+  LPC1114_AD5         : CONSTANT Unsigned_32 := LPC1114_GPIO4;
 
    -- Raspberry Pi LPC1114 I/O Processor PWM output pins
 
-  LPC1114_PWM1        : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO1;
-  LPC1114_PWM2        : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO2;
-  LPC1114_PWM3        : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO3;
-  LPC1114_PWM4        : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO7;
+  LPC1114_PWM1        : CONSTANT Unsigned_32 := LPC1114_GPIO1;
+  LPC1114_PWM2        : CONSTANT Unsigned_32 := LPC1114_GPIO2;
+  LPC1114_PWM3        : CONSTANT Unsigned_32 := LPC1114_GPIO3;
+  LPC1114_PWM4        : CONSTANT Unsigned_32 := LPC1114_GPIO7;
 
   -- Raspberry Pi LPC1114 I/O Processor Timer pins
 
-  LPC1114_CT32B1_CAP0 : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO0;
-  LPC1114_CT32B1_MAT0 : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO1;
-  LPC1114_CT32B1_MAT1 : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO2;
-  LPC1114_CT32B1_MAT2 : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO3;
-  LPC1114_CT32B1_MAT3 : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO4;
-  LPC1114_CT32B0_CAP0 : CONSTANT Interfaces.Unsigned_32 := LPC1114_GPIO5;
+  LPC1114_CT32B1_CAP0 : CONSTANT Unsigned_32 := LPC1114_GPIO0;
+  LPC1114_CT32B1_MAT0 : CONSTANT Unsigned_32 := LPC1114_GPIO1;
+  LPC1114_CT32B1_MAT1 : CONSTANT Unsigned_32 := LPC1114_GPIO2;
+  LPC1114_CT32B1_MAT2 : CONSTANT Unsigned_32 := LPC1114_GPIO3;
+  LPC1114_CT32B1_MAT3 : CONSTANT Unsigned_32 := LPC1114_GPIO4;
+  LPC1114_CT32B0_CAP0 : CONSTANT Unsigned_32 := LPC1114_GPIO5;
 
-   -- LPC1114 special function registers
+  -- LPC1114 special function registers
 
-  LPC1114_DEVICEID    : CONSTANT Interfaces.Unsigned_32 := 16#400483F4#;
-  LPC1114_GPIO1DATA   : CONSTANT Interfaces.Unsigned_32 := 16#50010CFC#;
-  LPC1114_U0SCR       : CONSTANT Interfaces.Unsigned_32 := 16#4000801C#;
+  LPC1114_DEVICEID    : CONSTANT Unsigned_32 := 16#400483F4#;
+  LPC1114_GPIO1DATA   : CONSTANT Unsigned_32 := 16#50010CFC#;
+  LPC1114_U0SCR       : CONSTANT Unsigned_32 := 16#4000801C#;
+
+  -- LPC1114 timer identifiers
+
+  LPC1114_CT32B0      : CONSTANT Unsigned_32 := 0;
+  LPC1114_CT32B1      : CONSTANT Unsigned_32 := 1;
+
+  -- LPC1114 timer modes
+
+  LPC1114_TIMER_MODE_DISABLED     : CONSTANT Unsigned_32 := 0;
+  LPC1114_TIMER_MODE_RESET        : CONSTANT Unsigned_32 := 1;
+  LPC1114_TIMER_MODE_PCLK         : CONSTANT Unsigned_32 := 2;
+  LPC1114_TIMER_MODE_CAP0_RISING  : CONSTANT Unsigned_32 := 3;
+  LPC1114_TIMER_MODE_CAP0_FALLING : CONSTANT Unsigned_32 := 4;
+  LPC1114_TIMER_MODE_CAP0_BOTH    : CONSTANT Unsigned_32 := 5;
+
+  -- LPC1114 timer capture edges
+
+  LPC1114_TIMER_CAPTURE_EDGE_DISABLED     : CONSTANT Unsigned_32 := 0;
+  LPC1114_TIMER_CAPTURE_EDGE_CAP0_RISING  : CONSTANT Unsigned_32 := 1;
+  LPC1114_TIMER_CAPTURE_EDGE_CAP0_FALLING : CONSTANT Unsigned_32 := 2;
+  LPC1114_TIMER_CAPTURE_EDGE_CAP0_BOTH    : CONSTANT Unsigned_32 := 3;
+
+  -- LPC1114 timer match registers
+ 
+  LPC1114_TIMER_MATCH0 : CONSTANT Unsigned_32 := 0;
+  LPC1114_TIMER_MATCH1 : CONSTANT Unsigned_32 := 1;
+  LPC1114_TIMER_MATCH2 : CONSTANT Unsigned_32 := 2;
+  LPC1114_TIMER_MATCH3 : CONSTANT Unsigned_32 := 3;
+
+  -- LPC1114 timer match output actions
+
+  LPC1114_TIMER_MATCH_OUTPUT_DISABLED : CONSTANT Unsigned_32 := 0;
+  LPC1114_TIMER_MATCH_OUTPUT_CLEAR    : CONSTANT Unsigned_32 := 1;
+  LPC1114_TIMER_MATCH_OUTPUT_SET      : CONSTANT Unsigned_32 := 2;
+  LPC1114_TIMER_MATCH_OUTPUT_TOGGLE   : CONSTANT Unsigned_32 := 3;
 
   -- Instantiate RemoteIO.Abstract_Device
 
