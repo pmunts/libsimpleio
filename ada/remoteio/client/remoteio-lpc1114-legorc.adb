d02db1a6bbf0630545f887207b19eb9c7845874d
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2019-05-18 08:25:04 +0000

    Added package RemoteIO.LPC1114.LEGORC
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17280 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/remoteio/client/remoteio-lpc1114-legorc.adb
@@ -0,0 +1,69 @@
+-- Copyright (C)2019, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Interfaces; USE Interfaces;
+
+WITH LEGORC;
+
+PACKAGE BODY RemoteIO.LPC1114.LEGORC IS
+
+  -- Constructors
+
+  FUNCTION Create
+   (dev : Abstract_Device.Device;
+    pin : Unsigned_32) RETURN Standard.LEGORC.Output IS
+
+    scmd  : SPIAGENT_COMMAND_MSG_t;
+    sresp : SPIAGENT_RESPONSE_MSG_t;
+
+  BEGIN
+    scmd.Command := SPIAGENT_CMD_CONFIGURE_GPIO_OUTPUT;
+    scmd.Pin     := pin;
+    scmd.Data    := 0;
+
+    dev.Operation(scmd, sresp);
+
+    RETURN NEW OutputSubclass'(dev, pin);
+  END Create;
+
+  -- Methods
+
+  PROCEDURE Put
+   (Self : OutputSubclass;
+    chan : Standard.LEGORC.Channel;
+    cmd  : Standard.LEGORC.Command;
+    data : Standard.LEGORC.Speed;
+    dir  : Standard.LEGORC.Direction) IS
+
+    scmd  : SPIAGENT_COMMAND_MSG_t;
+    sresp : SPIAGENT_RESPONSE_MSG_t;
+
+  BEGIN
+    scmd.Command := SPIAGENT_CMD_PUT_LEGORC;
+    scmd.Pin     := Self.pin;
+    scmd.Data    := Shift_Left(Unsigned_32(chan), 24) OR
+      Shift_Left(Unsigned_32(Standard.LEGORC.Command'Pos(cmd)), 16) OR
+      Shift_left(Unsigned_32(Standard.LEGORC.Direction'Pos(dir)), 8) OR
+      Unsigned_32(data);
+
+    Self.dev.Operation(scmd, sresp);
+  END Put;
+
+END RemoteIO.LPC1114.LEGORC;
---
+++ ada/remoteio/client/remoteio-lpc1114-legorc.ads
@@ -0,0 +1,51 @@
+-- Copyright (C)2019, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Interfaces;
+
+WITH LEGORC;
+
+PACKAGE RemoteIO.LPC1114.LEGORC IS
+
+  TYPE OutputSubclass IS NEW Standard.LEGORC.OutputInterface WITH PRIVATE;
+
+  -- Constructors
+
+  FUNCTION Create
+   (dev : RemoteIO.LPC1114.Abstract_Device.Device;
+    pin : Interfaces.Unsigned_32) RETURN Standard.LEGORC.Output;
+
+  -- Methods
+
+  PROCEDURE Put
+   (Self : OutputSubclass;
+    chan : Standard.LEGORC.Channel;
+    cmd  : Standard.LEGORC.Command;
+    data : Standard.LEGORC.Speed;
+    dir  : Standard.LEGORC.Direction);
+
+PRIVATE
+
+  TYPE OutputSubclass IS NEW Standard.LEGORC.OutputInterface WITH RECORD
+    dev : RemoteIO.LPC1114.Abstract_Device.Device;
+    pin : Interfaces.Unsigned_32;
+  END RECORD;
+
+END RemoteIO.LPC1114.LEGORC;
