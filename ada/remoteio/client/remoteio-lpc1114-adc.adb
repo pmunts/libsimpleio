8fb9b2a5377840c49ff36f3cd2941c7fef4473cf
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2019-05-21 09:17:56 +0000

    Added support for LPC1114 I/O Processor analog inputs
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17303 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/remoteio/client/remoteio-lpc1114-adc.adb
@@ -0,0 +1,73 @@
+-- LPC1114 I/O Processor analog input services
+
+-- Copyright (C)2019, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Interfaces;
+
+USE TYPE Interfaces.Unsigned_32;
+
+PACKAGE BODY RemoteIO.LPC1114.ADC IS
+
+  -- Configure analog input with SPIAGENT_CMD_CONFIGURE_ANALOG_INPUT
+
+  FUNCTION Create
+   (absdev : RemoteIO.LPC1114.Abstract_Device.Device;
+    desg   : Interfaces.Unsigned_32) RETURN Analog.Input IS
+
+    cmd  : RemoteIO.LPC1114.SPIAGENT_COMMAND_MSG_t;
+    resp : RemoteIO.LPC1114.SPIAGENT_RESPONSE_MSG_t;
+
+  BEGIN
+    cmd.Command := SPIAGENT_CMD_CONFIGURE_ANALOG_INPUT;
+    cmd.Pin     := desg;
+    cmd.Data    := 0;
+
+    absdev.Operation(cmd, resp);
+
+    RETURN NEW InputSubclass'(absdev, desg);
+  END Create;
+
+  -- Read analog input
+
+  FUNCTION Get(Self : IN OUT InputSubclass) RETURN Analog.Sample IS
+
+    cmd  : RemoteIO.LPC1114.SPIAGENT_COMMAND_MSG_t;
+    resp : RemoteIO.LPC1114.SPIAGENT_RESPONSE_MSG_t;
+
+  BEGIN
+    cmd.Command := SPIAGENT_CMD_GET_ANALOG;
+    cmd.Pin     := Self.pin;
+    cmd.Data    := 0;
+
+    Self.dev.Operation(cmd, resp);
+
+    RETURN Analog.Sample(resp.Data);
+  END Get;
+
+  -- Retrieve analog input resolution
+
+  FUNCTION GetResolution(Self : IN OUT InputSubclass) RETURN Positive IS
+
+  BEGIN
+    RETURN 10;
+  END GetResolution;
+
+END RemoteIO.LPC1114.ADC;
---
+++ ada/remoteio/client/remoteio-lpc1114-adc.ads
@@ -0,0 +1,51 @@
+-- LPC1114 I/O Processor analog input services
+
+-- Copyright (C)2019, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Analog;
+WITH RemoteIO.Client;
+
+PACKAGE RemoteIO.LPC1114.ADC IS
+
+  TYPE InputSubclass IS NEW Analog.InputInterface WITH PRIVATE;
+
+  -- Configure analog input with SPIAGENT_CMD_CONFIGURE_ANALOG_INPUT
+
+  FUNCTION Create
+   (absdev : RemoteIO.LPC1114.Abstract_Device.Device;
+    desg   : Interfaces.Unsigned_32) RETURN Analog.Input;
+
+  -- Read analog input
+
+  FUNCTION Get(Self : IN OUT InputSubclass) RETURN Analog.Sample;
+
+  -- Retrieve analog input resolution
+
+  FUNCTION GetResolution(Self : IN OUT InputSubclass) RETURN Positive;
+
+PRIVATE
+
+  TYPE InputSubclass IS NEW Analog.InputInterface WITH RECORD
+    dev : RemoteIO.LPC1114.Abstract_Device.Device;
+    pin : Interfaces.Unsigned_32;
+  END RECORD;
+
+END RemoteIO.LPC1114.ADC;
