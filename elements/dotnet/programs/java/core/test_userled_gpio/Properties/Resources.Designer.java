3317e1911666fb904762feaefc3c21cfa02105c1
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2019-08-05 10:43:14 +0000

    More reorganizing
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17538 06ee2f96-5407-11de-8bab-f3d7589a4122

--- elements/dotnet/programs/java/core/test_userled_gpio/Properties/Resources.Designer.java
+++
@@ -1,2 +0,0 @@
-﻿/* The .Designer.java file will be automatically generated when the parent file changes.
-   You can also right-click the parent file and select 'Run Custom Tool' to update it now. */
