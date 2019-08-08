4b95d198400a90eafd7456bba9f5af7998de5486
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2019-08-08 14:37:18 +0000

    Added GPIO output toggle test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17552 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ elements/dotnet/programs/java/test_simpleio_gpio/Program.java
@@ -0,0 +1,48 @@
+﻿// GPIO pin toggle test using libsimpleio
+
+// Copyright (C)2019, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package test_simpleio_gpio;
+
+public class Program
+{
+    public static int Main(String []args)
+    {
+        Console.WriteLine("\nGPIO Pin Toggle Test using libsimpleio\n");
+
+        // Create GPIO pin object
+
+        Console.Write("GPIO chip number? ");
+        int chip = int.Parse(Console.ReadLine());
+
+        Console.Write("GPIO line number? ");
+        int line = int.Parse(Console.ReadLine());
+
+        IO.Interfaces.GPIO.Pin Output =
+            new IO.Objects.libsimpleio.GPIO.Pin(chip, line,
+            IO.Interfaces.GPIO.Direction.Output, false);
+
+        for (;;)
+        {
+            Output.state = !Output.state;
+        }
+    }
+}
---
+++ elements/dotnet/programs/swift/test_simpleio_gpio/Program.swift
@@ -0,0 +1,41 @@
+﻿// GPIO Pin Toggle Test using libsimpleio
+
+// Copyright (C)2019, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+print("\nGPIO Pin Toggle Test using libsimpleio\n")
+
+// Create GPIO pin object
+
+print("GPIO chip number? ", terminator:"")
+var chip = readLine().toInt()
+
+print("GPIO line number? ", terminator:"")
+var line = readLine().toInt()
+
+var Output : IO.Interfaces.GPIO.Pin = IO.Objects.libsimpleio.GPIO.Pin(chip,
+  line, IO.Interfaces.GPIO.Direction.Output, false)
+
+// Toggle the GPIO output
+
+while true
+{
+  Output.state = !Output.state
+}
