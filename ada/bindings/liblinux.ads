e9a20a6e4a0b2d6d7dd682666826ff54acc432c4
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-01-08 21:44:53 +0000

    Updated copyright year for 2020
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17865 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/bindings/liblinux.ads
+++ ada/bindings/liblinux.ads
@@ -1,7 +1,7 @@
 -- Minimal Ada wrapper for the Linux syscall services
 -- implemented in libsimpleio.so
 
--- Copyright (C)2016-2018, Philip Munts, President, Munts AM Corp.
+-- Copyright (C)2016-2020, Philip Munts, President, Munts AM Corp.
 --
 -- Redistribution and use in source and binary forms, with or without
 -- modification, are permitted provided that the following conditions are met:
