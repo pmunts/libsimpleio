d7ef24adae000f98672424a025830b350232f956
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-01-17 02:57:56 +0000

    Added package Modbus.InputRegisters
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17885 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/objects/modbus-inputregisters.adb
@@ -0,0 +1,88 @@
+-- Copyright (C)2020, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+-- Modbus input registers are READ ONLY
+
+PACKAGE BODY Modbus.InputRegisters IS
+
+  -- Input register constructor
+
+  FUNCTION Create
+   (cont  : Bus;
+    slave : Natural;
+    addr  : Natural) RETURN Register IS
+
+    Self : RegisterClass := Destroyed;
+
+  BEGIN
+    Self.Initialize(cont, slave, addr);
+    RETURN NEW RegisterClass'(Self);
+  END Create;
+
+  -- Input register initializer
+
+  PROCEDURE Initialize
+   (Self  : IN OUT RegisterClass;
+    cont  : Bus;
+    slave : Natural;
+    addr  : Natural) IS
+
+    dummy : RegisterData;
+
+  BEGIN
+    Self.Destroy;
+    Self := RegisterClass'(cont.ctx, slave, addr);
+    dummy := Self.Get;
+  EXCEPTION
+    WHEN Error =>
+      Self.Destroy;
+      RAISE;
+  END Initialize;
+
+  -- Input register destructor
+
+  PROCEDURE Destroy(Self : IN OUT RegisterClass) IS
+
+  BEGIN
+    IF Self = Destroyed THEN
+      RETURN;
+    END IF;
+
+    Self := Destroyed;
+  END Destroy;
+
+  -- Input register methods
+
+  FUNCTION Get(Self : IN OUT RegisterClass) RETURN RegisterData IS
+
+    buf : libModbus.wordarray(0 .. 0);
+
+  BEGIN
+    SelectSlave(Self.ctx, Self.slave);
+
+    IF libModbus.modbus_read_input_registers(Self.ctx, Self.addr, 1, buf) /= 1 THEN
+      RAISE Error WITH "modbus_read_input_registers() failed, " &
+        libModbus.error_message;
+    END IF;
+
+    RETURN RegisterData(buf(0));
+  END Get;
+
+END ModBus.InputRegisters;
---
+++ ada/objects/modbus-inputregisters.ads
@@ -0,0 +1,71 @@
+-- Copyright (C)2020, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+-- Modbus input registers are READ ONLY
+
+WITH IO_Interfaces;
+
+PACKAGE Modbus.InputRegisters IS
+
+  TYPE RegisterData IS MOD 2**16;
+
+  PACKAGE Interfaces IS NEW IO_Interfaces(RegisterData);
+
+  TYPE RegisterClass IS NEW Interfaces.InputInterface WITH PRIVATE;
+
+  TYPE Register IS ACCESS ALL RegisterClass'Class;
+
+  Destroyed : CONSTANT RegisterClass;
+
+  -- Input register constructor
+
+  FUNCTION Create
+   (cont  : Bus;
+    slave : Natural;
+    addr  : Natural) RETURN Register;
+
+  -- Input register initializer
+
+  PROCEDURE Initialize
+   (Self  : IN OUT RegisterClass;
+    cont  : Bus;
+    slave : Natural;
+    addr  : Natural);
+
+  -- Input register destructor
+
+  PROCEDURE Destroy(Self : IN OUT RegisterClass);
+
+  -- Input register methods
+
+  FUNCTION Get(Self : IN OUT RegisterClass) RETURN RegisterData;
+
+PRIVATE
+
+  TYPE RegisterClass IS NEW Interfaces.InputInterface WITH RECORD
+    ctx   : libModbus.Context := libModbus.Null_Context;
+    slave : Natural           := Natural'Last;
+    addr  : Natural           := Natural'Last;
+  END RECORD;
+
+  Destroyed : CONSTANT RegisterClass := RegisterClass'(libModbus.Null_Context,
+    Natural'Last, Natural'Last);
+
+END ModBus.InputRegisters;
