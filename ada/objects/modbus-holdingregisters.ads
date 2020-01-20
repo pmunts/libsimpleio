ac7f5a23c1046008650209512800904899c120f5
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-01-20 04:21:49 +0000

    Changed comment to include WRITE ONLY
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17901 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/objects/modbus-holdingregisters.ads
+++ ada/objects/modbus-holdingregisters.ads
@@ -18,7 +18,7 @@
 -- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
--- Modbus (output) holding registers are READ/WRITE
+-- Modbus (output) holding registers are READ/WRITE or possibly WRITE ONLY
 
 WITH IO_Interfaces;
 
