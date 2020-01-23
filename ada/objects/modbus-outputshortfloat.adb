0f201f8ad789ee7a78c830881e30657c99cb9b3e
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-01-23 17:51:50 +0000

    Added support for 32-bit floating point holding registers.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17913 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/objects/modbus-outputshortfloat.adb
@@ -0,0 +1,131 @@
+-- Copyright (C)2020, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+-- Modbus input registers are READ ONLY
+
+PACKAGE BODY Modbus.OutputShortFloat IS
+
+  -- Output holding register constructor
+
+  FUNCTION Create
+   (cont  : Bus;
+    slave : Natural;
+    addr  : Natural;
+    state : Quantity;
+    order : ShortFloatByteOrder := ABCD) RETURN Output IS
+
+    Self : OutputClass := Destroyed;
+
+  BEGIN
+    Self.Initialize(cont, slave, addr, state, order);
+    RETURN NEW OutputClass'(Self);
+  END Create;
+
+  -- Output holding register initializer
+
+  PROCEDURE Initialize
+   (Self  : IN OUT OutputClass;
+    cont  : Bus;
+    slave : Natural;
+    addr  : Natural;
+    state : Quantity;
+    order : ShortFloatByteOrder := ABCD) IS
+
+  BEGIN
+    Self.Destroy;
+    Self := OutputClass'(cont.ctx, slave, addr, order);
+    Self.Put(state);
+  EXCEPTION
+    WHEN Error =>
+      Self.Destroy;
+      RAISE;
+  END Initialize;
+
+  -- Output holding register destructor
+
+  PROCEDURE Destroy(Self : IN OUT OutputClass) IS
+
+  BEGIN
+    IF Self = Destroyed THEN
+      RETURN;
+    END IF;
+
+    Self := Destroyed;
+  END Destroy;
+
+  -- Output holding register methods
+
+  FUNCTION Get(Self : IN OUT OutputClass) RETURN Quantity IS
+
+    buf : libModbus.wordarray(0 .. 1);
+
+  BEGIN
+    SelectSlave(Self.ctx, Self.slave);
+
+    IF libModbus.modbus_read_registers(Self.ctx, Self.addr, 2, buf) /= 2 THEN
+      RAISE Error WITH "modbus_read_registers() failed, " &
+        libModbus.error_message;
+    END IF;
+
+    CASE Self.order IS
+      WHEN ABCD =>
+        RETURN Quantity(libModbus.modbus_get_float_badc(buf));
+
+      WHEN BADC =>
+        RETURN Quantity(libModbus.modbus_get_float_abcd(buf));
+
+      WHEN CDAB =>
+        RETURN Quantity(libModbus.modbus_get_float_dcba(buf));
+
+      WHEN DCBA =>
+        RETURN Quantity(libModbus.modbus_get_float_cdab(buf));
+    END CASE;
+  END Get;
+
+  PROCEDURE Put
+   (Self  : IN OUT OutputClass;
+    state : Quantity) IS
+
+    buf : libModbus.wordarray(0 .. 1);
+
+  BEGIN
+    SelectSlave(Self.ctx, Self.slave);
+
+    CASE Self.order IS
+      WHEN ABCD =>
+        libModbus.modbus_set_float_abcd(Short_Float(state), buf);
+
+      WHEN BADC =>
+        libModbus.modbus_set_float_badc(Short_Float(state), buf);
+
+      WHEN CDAB =>
+        libModbus.modbus_set_float_cdab(Short_Float(state), buf);
+
+      WHEN DCBA =>
+        libModbus.modbus_set_float_dcba(Short_Float(state), buf);
+    END CASE;
+
+    IF libModbus.modbus_write_registers(Self.ctx, Self.addr, 2, buf) /= 2 THEN
+      RAISE Error WITH "modbus_write_registers() failed, " &
+        libModbus.error_message;
+    END IF;
+  END Put;
+
+END ModBus.OutputShortFloat;
---
+++ ada/objects/modbus-outputshortfloat.ads
@@ -0,0 +1,77 @@
+-- Copyright (C)2020, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+-- Modbus (output) holding registers are READ/WRITE or possibly WRITE ONLY
+
+GENERIC
+
+  TYPE Quantity IS DIGITS <>;
+
+PACKAGE Modbus.OutputShortFloat IS
+
+  TYPE OutputClass IS TAGGED PRIVATE;
+  TYPE Output      IS ACCESS ALL OutputClass'Class;
+
+  Destroyed : CONSTANT OutputClass;
+
+  -- Output holding register constructor
+
+  FUNCTION Create
+   (cont  : Bus;
+    slave : Natural;
+    addr  : Natural;
+    state : Quantity;
+    order : ShortFloatByteOrder := ABCD) RETURN Output;
+
+  -- Output holding register initializer
+
+  PROCEDURE Initialize
+   (Self  : IN OUT OutputClass;
+    cont  : Bus;
+    slave : Natural;
+    addr  : Natural;
+    state : Quantity;
+    order : ShortFloatByteOrder := ABCD);
+
+  -- Output holding register destructor
+
+  PROCEDURE Destroy(Self : IN OUT OutputClass);
+
+  -- Output holding register methods
+
+  FUNCTION Get(Self : IN OUT OutputClass) RETURN Quantity;
+
+  PROCEDURE Put
+   (Self  : IN OUT OutputClass;
+    state : Quantity);
+
+PRIVATE
+
+  TYPE OutputClass IS TAGGED RECORD
+    ctx   : libModbus.Context   := libModbus.Null_Context;
+    slave : Natural             := Natural'Last;
+    addr  : Natural             := Natural'Last;
+    order : ShortFloatByteOrder := ShortFloatByteOrder'Last;
+  END RECORD;
+
+  Destroyed : CONSTANT OutputClass := OutputClass'(libModbus.Null_Context,
+    Natural'Last, Natural'Last, ShortFloatByteOrder'Last);
+
+END ModBus.OutputShortFloat;
