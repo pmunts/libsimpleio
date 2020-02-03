d0200307585690a63e583a5c2d81ae6f70839970
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-02-03 07:30:28 +0000

    Set proper permissions on Windows DLL files
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17968 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/include/remoteio.mk
+++ ada/include/remoteio.mk
@@ -41,9 +41,11 @@ WINARCH		?= win64
 ADA_LDFLAGS	+= -L$(LIBSIMPLEIO)/win/$(WINARCH)
 
 hidapi.dll:
-	cp $(LIBSIMPLEIO)/win/$(WINARCH)/hidapi.dll .
+	cp $(LIBSIMPLEIO)/win/$(WINARCH)/hidapi.dll $@
+	chmod 755 $@
 
 libusb-1.0.dll:
-	cp $(LIBSIMPLEIO)/win/$(WINARCH)/libusb-1.0.dll .
+	cp $(LIBSIMPLEIO)/win/$(WINARCH)/libusb-1.0.dll $@
+	chmod 755 $@
 endif
 endif
