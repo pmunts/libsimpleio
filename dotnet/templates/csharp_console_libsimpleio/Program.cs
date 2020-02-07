d85cca46f9e1af3b39f8e91c4c6288a4d617047d
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-02-07 00:48:16 +0000

    Added templates.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17974 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ dotnet/templates/csharp_console_libsimpleio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console.libsimpleio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
