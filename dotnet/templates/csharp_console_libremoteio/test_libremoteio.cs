0e57561f7a87af2a0575827c6c19809013edc28d
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-02-07 05:27:58 +0000

    Added useful Remote I/O code
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@17981 06ee2f96-5407-11de-8bab-f3d7589a4122

--- dotnet/templates/csharp_console_libremoteio/test_libremoteio.cs
+++ dotnet/templates/csharp_console_libremoteio/test_libremoteio.cs
@@ -6,7 +6,16 @@ namespace test_libremoteio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nConsole Application using the Remote I/O Protocol\n");
+
+            IO.Objects.USB.HID.Messenger m = new IO.Objects.USB.HID.Messenger();
+            IO.Remote.Device dev = new IO.Remote.Device(m);
+
+            // Display some device information
+
+            Console.WriteLine(m.Info);
+            Console.WriteLine(dev.Version);
+            Console.WriteLine(dev.Capabilities);
         }
     }
 }
