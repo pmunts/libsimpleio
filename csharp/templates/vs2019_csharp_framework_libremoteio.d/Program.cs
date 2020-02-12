6a63d5f47dbb329efa56b2d9778337a1b8f7428a
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-02-12 05:08:00 +0000

    Added templates for building C# .Net Framework applications using libremoteio and libsimpleio.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@18013 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ csharp/templates/vs2019_csharp_framework_libremoteio.d/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nConsole Application using the Remote I/O Protocol\n");
+
+            IO.Objects.USB.HID.Messenger m = new IO.Objects.USB.HID.Messenger();
+            IO.Remote.Device dev = new IO.Remote.Device(m);
+
+            // Display some device information
+
+            Console.WriteLine(m.Info);
+            Console.WriteLine(dev.Version);
+            Console.WriteLine(dev.Capabilities);
+        }
+    }
+}
