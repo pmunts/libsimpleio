ee5432d316373f9147859b32f5a314ce94484717
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-02-12 16:46:04 +0000

    Added Visual Studio templates for .Net Core applications.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@18020 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ csharp/templates/vs2019_csharp_coreapp_libsimpleio.d/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
