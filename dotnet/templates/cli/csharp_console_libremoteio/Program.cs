e41d13494ed62649347b3691842b0795d40cdbca
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-03-15 03:54:47 +0000

    Renamed variable dev to remdev.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@18189 06ee2f96-5407-11de-8bab-f3d7589a4122

--- dotnet/templates/cli/csharp_console_libremoteio/Program.cs
+++ dotnet/templates/cli/csharp_console_libremoteio/Program.cs
@@ -9,13 +9,13 @@ namespace test_libremoteio
             Console.WriteLine("\nConsole Application using the Remote I/O Protocol\n");
 
             IO.Objects.USB.HID.Messenger m = new IO.Objects.USB.HID.Messenger();
-            IO.Remote.Device dev = new IO.Remote.Device(m);
+            IO.Remote.Device remdev = new IO.Remote.Device(m);
 
             // Display some device information
 
             Console.WriteLine(m.Info);
-            Console.WriteLine(dev.Version);
-            Console.WriteLine(dev.Capabilities);
+            Console.WriteLine(remdev.Version);
+            Console.WriteLine(remdev.Capabilities);
         }
     }
 }
