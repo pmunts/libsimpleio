e9600efb75da5bd3936dd8274b98d4be0322124d
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-11-21 04:05:14 +0000

    Added GPIO output toggle test.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@18903 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ c++/programs/remoteio/test_gpio.cpp
@@ -0,0 +1,56 @@
+// Remote I/O GPIO Pin Toggle Test
+
+// Copyright (C)2020, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <cstdio>
+#include <cstring>
+
+#include <gpio-interface.h>
+#include <remoteio-client.h>
+#include <remoteio-gpio.h>
+
+int main(void)
+{
+  puts("\nRemote I/O GPIO Pin Toggle Test\n");
+
+  // Create a Remote I/O client object
+
+  RemoteIO::Client::Device_Class dev;
+
+  // Create a GPIO output pin object
+
+  printf("Enter GPIO output channel number: ");
+  fflush(stdout);
+
+  char buf[256];
+  memset(buf, 0, sizeof(buf));
+  fgets(buf, sizeof(buf), stdin);
+
+  unsigned chan = atoi(buf);
+
+  Interfaces::GPIO::Pin outp = new RemoteIO::GPIO::Pin_Class(&dev, chan,
+    Interfaces::GPIO::OUTPUT, false);
+
+  // Toggle the GPIO output
+
+  for (;;)
+    *outp = !*outp;
+}
