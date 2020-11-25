63faa831eac489acd231497d007ba5011d86cbed
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-11-25 03:52:56 +0000

    Updated the copyright year.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@18930 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/programs/mcp2221/test_gpio.adb
+++ ada/programs/mcp2221/test_gpio.adb
@@ -1,6 +1,6 @@
 -- MCP2221 GPIO Toggle Test
 
--- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+-- Copyright (C)2018-2020, Philip Munts, President, Munts AM Corp.
 --
 -- Redistribution and use in source and binary forms, with or without
 -- modification, are permitted provided that the following conditions are met:
--- ada/programs/mcp2221/test_led.adb
+++ ada/programs/mcp2221/test_led.adb
@@ -1,6 +1,6 @@
 -- MCP2221 LED toggle test
 
--- Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+-- Copyright (C)2018-2020, Philip Munts, President, Munts AM Corp.
 --
 -- Redistribution and use in source and binary forms, with or without
 -- modification, are permitted provided that the following conditions are met:
