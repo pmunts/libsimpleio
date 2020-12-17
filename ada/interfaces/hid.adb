dc32a3715eaf3736bd3f76ab6adf5ad3980b9519
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-12-17 19:50:59 +0000

    Added function ToString()
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@19065 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/interfaces/hid.adb
+++ ada/interfaces/hid.adb
@@ -20,22 +20,40 @@
 -- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
-PACKAGE HID IS
-
-  -- Define an exception for HID errors
-
-  HID_Error : EXCEPTION;
-
-  -- Type definitions
-
-  TYPE Vendor  IS NEW NATURAL RANGE 0 .. 65535;
-  TYPE Product IS NEW NATURAL RANGE 0 .. 65535;
-
-  TYPE Capitalization IS (Lower, Upper);
+PACKAGE BODY HID IS
 
   FUNCTION ToString
    (VID : Vendor;
     PID : Product;
-    cap : Capitalization := Upper) RETURN String;
+    cap : Capitalization := Upper) RETURN String IS
+
+    hexchars_lower : CONSTANT String := "0123456789abcdef";
+    hexchars_upper : CONSTANT String := "0123456789ABCDEF";
+
+  BEGIN
+    CASE cap IS
+      WHEN Lower =>
+	RETURN
+          hexchars_lower(Natural(VID / 4096 MOD 16) + 1) &
+	  hexchars_lower(Natural(VID / 256 MOD 16) + 1)  &
+	  hexchars_lower(Natural(VID / 16 MOD 16) + 1)   &
+	  hexchars_lower(Natural(VID MOD 16) + 1) & ":"  &
+          hexchars_lower(Natural(PID / 4096 MOD 16) + 1) &
+	  hexchars_lower(Natural(PID / 256 MOD 16) + 1)  &
+	  hexchars_lower(Natural(PID / 16 MOD 16) + 1)   &
+	  hexchars_lower(Natural(PID MOD 16) + 1);
+
+      WHEN Upper =>
+	RETURN
+          hexchars_upper(Natural(VID / 4096 MOD 16) + 1) &
+	  hexchars_upper(Natural(VID / 256 MOD 16) + 1)  &
+	  hexchars_upper(Natural(VID / 16 MOD 16) + 1)   &
+	  hexchars_upper(Natural(VID MOD 16) + 1) & ":"  &
+          hexchars_upper(Natural(PID / 4096 MOD 16) + 1) &
+	  hexchars_upper(Natural(PID / 256 MOD 16) + 1)  &
+	  hexchars_upper(Natural(PID / 16 MOD 16) + 1)   &
+	  hexchars_upper(Natural(PID MOD 16) + 1);
+    END CASE;
+  END ToString;
 
 END HID;
