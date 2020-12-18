e0a8b2a8226323cdcfcc9e2714d6d388c9d761af
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-12-18 01:03:10 +0000

    Changed __xpg_strerror_r to strerror_r.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@19069 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/bindings/posix.ads
+++ ada/bindings/posix.ads
@@ -83,7 +83,7 @@ PACKAGE Posix IS
   PRAGMA Import(C, C_write,      "write");
   PRAGMA Import(C, C_poll,       "poll");
   PRAGMA Import(C, C_errno,      "__get_errno");   -- From GNAT runtime library
-  PRAGMA Import(C, C_strerror_r, "__xpg_strerror_r");
+  PRAGMA Import(C, C_strerror_r, "strerror_r");
 
   -- Nicer Ada wrappers
 
