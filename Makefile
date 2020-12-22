890f3037e1af7ac8061efca4545534e6595581bd
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2020-12-22 03:32:14 +0000

    Imported USB raw HID helpers from hotplug/openbsd/.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@19093 06ee2f96-5407-11de-8bab-f3d7589a4122

--- Makefile
+++ Makefile
@@ -28,10 +28,11 @@ CFLAGS		= -Wall -fPIC -I. -I.. $(DEBUGFLAGS)
 
 BUILDNUM	?= 1
 
+DESTDIR		?= /usr/local
+ETCDIR		?= /etc
+
 ifeq ($(BOARDNAME),)
 # Definitions for compiling for native Linux
-
-DESTDIR		?= /usr/local
 
 OSNAME		?= unknown
 PKGNAME		:= munts-libsimpleio
@@ -52,8 +53,6 @@ PKGARCH		:= all
 PKGDIR		:= $(PKGNAME)-$(PKGVERSION)-$(OSNAME)-$(PKGARCH)
 DEBFILE		:= $(PKGDIR).deb
 endif
-
-UDEVRULESDIR	?= /etc/udev/rules.d
 
 include include/dpkg.mk
 
@@ -89,7 +88,7 @@ install: libsimpleio.a libsimpleio.so
 	mkdir -p				$(DESTDIR)/include
 	mkdir -p				$(DESTDIR)/lib
 	mkdir -p				$(DESTDIR)/share/libsimpleio/doc
-	mkdir -p				$(DESTDIR)/share/man/man2
+	ln -s ../share/libsimpleio/include	$(DESTDIR)/include/libsimpleio
 	install -cm 0644 *.a			$(DESTDIR)/lib
 	install -cm 0755 *.so			$(DESTDIR)/lib
 	cp -R -P -p ada				$(DESTDIR)/share/libsimpleio
@@ -103,21 +102,23 @@ install: libsimpleio.a libsimpleio.so
 	cp -R -P -p modula2			$(DESTDIR)/share/libsimpleio
 	cp -R -P -p pascal			$(DESTDIR)/share/libsimpleio
 ifeq ($(BOARDNAME),)
-	mkdir -p				$(DESTDIR)/share/libsimpleio/udev
-	cp -P -p hotplug/linux/*		$(DESTDIR)/share/libsimpleio/udev
+	mkdir -p				$(ETCDIR)/udev/rules.d
+	mkdir -p				$(DESTDIR)/libexec
+	mkdir -p				$(DESTDIR)/share/man/man2
+	install -cm 0644 hotplug/linux/*.conf	$(ETCDIR)
+	install -cm 0644 hotplug/linux/*.rules	$(ETCDIR)/udev/rules.d
+	install -cm 0755 hotplug/linux/*helper*	$(DESTDIR)/libexec
+	$(CC) -o$(DESTDIR)/libexec/usb-hid-hotplug-attach hotplug/linux/usb-hid-hotplug-attach.c
+	$(CC) -o$(DESTDIR)/libexec/usb-hid-hotplug-detach hotplug/linux/usb-hid-hotplug-detach.c
+	strip					$(DESTDIR)/libexec/usb-hid-hotplug*
 	cp -R -P -p java			$(DESTDIR)/share/libsimpleio
 	cp -R -P -p nuget			$(DESTDIR)/share/libsimpleio
+	install -cm 0644 doc/*.2		$(DESTDIR)/share/man/man2
 endif
 	install -cm 0644 COPYING		$(DESTDIR)/share/libsimpleio/doc
 	install -cm 0644 README.txt		$(DESTDIR)/share/libsimpleio/doc/README
 	install -cm 0644 doc/*.pdf		$(DESTDIR)/share/libsimpleio/doc
 	install -cm 0644 c/*.h			$(DESTDIR)/share/libsimpleio/include
-	install -cm 0644 doc/*.2		$(DESTDIR)/share/man/man2
-
-# Install symlinks for the udev rules
-
-install_udev_rules:
-	cd $(UDEVRULESDIR) && for R in /usr/local/share/libsimpleio/udev/*.rules ; do ln -s $$R ; done
 
 # Create Debian package file
 
@@ -129,17 +130,13 @@ $(PKGDIR):
 	sed -i s/@@VERSION@@/$(PKGVERSION)/g	$(PKGDIR)/DEBIAN/control
 ifeq ($(BOARDNAME),)
 # Native package for Debian Linux et al
-	echo "Depends: libhidapi-dev" >>	$(PKGDIR)/DEBIAN/control
+	echo "Depends: libhidapi-dev, libusb-1.0-0-dev" >> $(PKGDIR)/DEBIAN/control
 	install -cm 0755 postinst.native	$(PKGDIR)/DEBIAN/postinst
 	install -cm 0755 postrm.native		$(PKGDIR)/DEBIAN/postrm
-	$(MAKE) install DESTDIR=$(PKGDIR)/usr/local
+	$(MAKE) install DESTDIR=$(PKGDIR)/usr/local ETCDIR=$(PKGDIR)/etc
 else
 # Cross-compiled package for MuntsOS embedded Linux
 	$(MAKE) install DESTDIR=$(PKGDIR)$(GCCSYSROOT)/usr
-	install -cm 0755 postinst.muntsos	$(PKGDIR)/DEBIAN/postinst
-	install -cm 0755 postrm.muntsos		$(PKGDIR)/DEBIAN/postrm
-	sed -i 's#@@GCCSYSROOT@@#$(GCCSYSROOT)#g' $(PKGDIR)/DEBIAN/postinst
-	sed -i 's#@@GCCSYSROOT@@#$(GCCSYSROOT)#g' $(PKGDIR)/DEBIAN/postrm
 endif
 
 package.deb: $(DEBFILE)
---
+++ hotplug/linux/usb-hid-hotplug-attach.c
@@ -0,0 +1,168 @@
+// USB Raw HID Hotplug Helper
+
+// Copyright (C)2020, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <errno.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <linux/types.h>
+#include <linux/input.h>
+#include <linux/hidraw.h>
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+
+#define PROGNAME	"usb-hid-hotplug-attach"
+#define DEVNAME		argv[1]
+
+// Search the config file for a matching device ID
+
+void SearchConfig(uint16_t VID, uint16_t PID)
+{
+  FILE *cfg;
+  char devid[32];
+  char inbuf[256];
+
+  // Open the configuration file
+
+  cfg = fopen("/etc/hidraw.conf", "rt");
+
+  if (cfg == NULL)
+  {
+    syslog(LOG_ERR, "fopen() failed, %s", strerror(errno));
+    exit(0);
+  }
+
+  // Search for a matching entry in the configuration file
+
+  snprintf(devid, sizeof(devid)-1, "%04X:%04X", VID, PID);
+
+  while (fgets(inbuf, sizeof(inbuf)-1, cfg) != NULL)
+    if (!strncasecmp(devid, inbuf, 9))
+    {
+      fclose(cfg);
+      return;
+    }
+
+  // Nothing found
+
+  exit(0);
+}
+
+int main(int argc, char **argv)
+{
+  char devname[MAXPATHLEN];
+  int fd;
+  struct hidraw_devinfo devinfo;
+  char linkname[MAXPATHLEN];
+
+  openlog(PROGNAME, LOG_PERROR|LOG_PID, LOG_LOCAL0);
+
+  // Validate command line arguments
+
+  if (argc != 2) exit(0);
+  if (strncmp(DEVNAME, "hidraw", 6)) exit(0);
+
+  snprintf(devname, sizeof(devname) - 1, "/dev/%s", DEVNAME);
+
+  // Open the candidate raw HID device
+
+  fd = open(devname, O_RDONLY);
+
+  if (fd < 0)
+  {
+    syslog(LOG_ERR, "open() failed, %s", strerror(errno));
+    exit(0);
+  }
+
+  // Fetch the USB device information
+
+  if (ioctl(fd, HIDIOCGRAWINFO, &devinfo) < 0)
+  {
+    syslog(LOG_ERR, "ioctl() failed, %s", strerror(errno));
+    exit(0);
+  }
+
+  close(fd);
+
+  // Search the configuration file for a matching device entry
+
+  SearchConfig(devinfo.vendor, devinfo.product);
+
+  // Change group ownership of the raw HID device
+
+  struct group *groupinfo = getgrnam("plugdev");
+
+  if (groupinfo == NULL)
+  {
+    syslog(LOG_ERR, "getgrnam() failed, %s", strerror(errno));
+    exit(0);
+  }
+
+  if (chown(devname, 0, groupinfo->gr_gid))
+  {
+    syslog(LOG_ERR, "chown() failed, %s", strerror(errno));
+    exit(0);
+  }
+
+  // Change permissions of the raw HID device
+
+  if (chmod(devname, 0660) < 0)
+  {
+    syslog(LOG_ERR, "chmod() failed, %s", strerror(errno));
+    exit(0);
+  }
+
+  // Create some useful symbolic links to the raw HID device
+
+  snprintf(linkname, sizeof(linkname) - 1, "/dev/hidraw-%04x:%04x",
+    devinfo.vendor, devinfo.product);
+
+  unlink(linkname);
+
+  if (symlink(DEVNAME, linkname) < 0)
+  {
+    syslog(LOG_ERR, "symlink() failed, %s", strerror(errno));
+    exit(0);
+  }
+
+#if 0
+  if (strlen(devinfo.udi_serial) == 0) exit(0);
+
+  snprintf(linkname, sizeof(linkname) - 1, "/dev/hidraw-%04x:%04x-%s",
+    devinfo.vendor, devinfo.product, devinfo.udi_serial);
+
+  unlink(linkname);
+
+  if (symlink(DEVNAME, linkname) < 0)
+  {
+    syslog(LOG_ERR, "symlink() failed, %s", strerror(errno));
+    exit(0);
+  }
+#endif
+}
