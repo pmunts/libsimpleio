e9cb511944e93350d18582e55ed44261a2f02c57
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2021-01-20 18:40:51 +0000

    Added Message64.Stream and friends.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@19195 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/objects/messaging-fixed-ada_streams.adb
@@ -0,0 +1,138 @@
+-- Fixed length message services using the Stream Framing Protocol.
+-- Must be instantiated for each message size.
+
+-- Copyright (C)2021, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Stream_Framing_Protocol;
+
+PACKAGE BODY Messaging.Fixed.Ada_Streams IS
+
+  PACKAGE SFP IS NEW Stream_Framing_Protocol(MessageSize);
+
+  DLE : CONSTANT Messaging.Byte := 16#10#;
+  STX : CONSTANT Messaging.Byte := 16#02#;
+  ETX : CONSTANT Messaging.Byte := 16#03#;
+
+  -- Constructor
+
+  FUNCTION Create
+   (stream    : Ada.Streams.Stream_IO.Stream_Access;
+    trimzeros : Boolean := True) RETURN Messenger IS
+
+  BEGIN
+    RETURN NEW MessengerSubclass'(stream, trimzeros);
+  END Create;
+
+  -- Send a message
+
+  PROCEDURE Send(Self : MessengerSubclass; msg : Message) IS
+
+    msglen   : Natural;
+    frame    : SFP.FrameBuffer;
+    framelen : SFP.FrameSize;
+
+  BEGIN
+    msglen := msg'Length;
+
+    -- Optimize bandwidth by not sending trailing zeros.  For short
+    -- messages, this will greatly reduce channel bandwidth
+
+    IF Self.trim THEN
+      WHILE (msglen > 0) AND (msg(msglen - 1) = 0) LOOP
+        msglen := msglen - 1;
+      END LOOP;
+    END IF;
+
+    -- Encode the frame
+
+    SFP.Encode(msg, msglen, frame, framelen);
+
+    -- Send the frame
+
+    Messaging.Buffer'Write(Self.stream, frame(1 .. framelen));
+  END Send;
+
+  -- Receive a message
+
+  PROCEDURE Receive(Self : MessengerSubclass; msg : OUT Message) IS
+
+    b          : Messaging.Byte;
+    DLE_count  : Natural := 0;
+    frame      : SFP.FrameBuffer;
+    framelen   : Natural := 0;
+    payload    : SFP.PayloadBuffer;
+    payloadlen : SFP.PayloadSize;
+
+  BEGIN
+    LOOP
+      Messaging.Byte'Read(Self.stream, b);
+
+      CASE framelen IS
+        WHEN 0 =>
+          IF b = DLE THEN
+            frame(1)  := DLE;
+            framelen  := 1;
+            DLE_Count := 1;
+          ELSE
+            framelen  := 0;
+            DLE_Count := 0;
+          END IF;
+
+        WHEN 1 =>
+          IF b = STX THEN
+            frame(2)  := STX;
+            framelen  := 2;
+          ELSE
+            framelen  := 0;
+            DLE_Count := 0;
+          END IF;
+
+        WHEN OTHERS =>
+          IF b = DLE THEN
+            DLE_Count := DLE_Count + 1;
+          END IF;
+
+          frame(framelen+1) := b;
+          framelen := framelen + 1;
+      END CASE;
+
+      -- Check for complete frame
+
+      EXIT WHEN framelen >= 6 AND THEN (DLE_count MOD 2 = 0 AND
+        frame(1) = DLE AND
+        frame(2) = STX AND
+        frame(framelen-1) = DLE AND
+        frame(framelen) = ETX);
+
+      -- Check for buffer overflow
+
+      IF framelen = SFP.FrameSize'Last THEN
+        framelen  := 0;
+        DLE_Count := 0;
+      END IF;
+    END LOOP;
+
+    SFP.Decode(frame, framelen, payload, payloadlen);
+
+    msg := payload;
+  END Receive;
+
+END Messaging.Fixed.Ada_Streams;
---
+++ ada/programs/remoteio/test_query_serial.adb
@@ -0,0 +1,191 @@
+-- Remote I/O Device Information Query
+
+-- Copyright (C)2021, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Command_Line;
+WITH Ada.Streams.Stream_IO;
+WITH Ada.Strings.Fixed;
+WITH Ada.Text_IO; USE Ada.Text_IO;
+WITH GNAT.Serial_Communications; USE GNAT.Serial_Communications;
+
+WITH Message64.Stream;
+WITH RemoteIO.Client;
+
+PROCEDURE test_query_serial IS
+
+  port     : ALIASED Serial_Port;
+  msg      : Message64.Messenger;
+  remdev   : RemoteIO.Client.Device;
+  channels : RemoteIO.ChannelSets.Set;
+
+BEGIN
+  New_Line;
+  Put_Line("Remote I/O Device Information Query");
+  New_Line;
+
+  -- Check command line parameters
+
+  IF Ada.Command_Line.Argument_Count /= 1 THEN
+    Put_Line("Usage: test_query_serial <device name>");
+    New_Line;
+    RETURN;
+  END IF;
+
+  -- Open serial device
+
+  Open(port, Port_Name(Ada.Command_Line.Argument(1)));
+  Set(port, B115200);
+
+  -- Create the messenger object
+
+  msg := Message64.Stream.Create(port'Unchecked_Access);
+
+  -- Create the remote I/O device
+
+  remdev := RemoteIO.Client.Create(msg);
+
+  -- Query the firmware version
+
+  Put_Line("Information:        " & remdev.GetVersion);
+
+  -- Query the capability string
+
+  Put_Line("Capabilities:       " & remdev.GetCapability);
+
+  IF Ada.Strings.Fixed.Index(remdev.GetCapability, "ADC") /= 0 THEN
+
+    -- Query the available ADC input pins
+
+    channels := remdev.GetAvailableChannels(RemoteIO.Channel_ADC);
+
+    IF NOT channels.Is_Empty THEN
+      Put("Found ADC inputs:  ");
+
+      FOR pin OF channels LOOP
+        Put(Integer'Image(pin));
+      END LOOP;
+
+      New_Line;
+    END IF;
+  END IF;
+
+  IF Ada.Strings.Fixed.Index(remdev.GetCapability, "DAC") /= 0 THEN
+
+    -- Query the available DAC input pins
+
+    channels := remdev.GetAvailableChannels(RemoteIO.Channel_DAC);
+
+    IF NOT channels.Is_Empty THEN
+      Put("Found DAC outputs: ");
+
+      FOR pin OF channels LOOP
+        Put(Integer'Image(pin));
+      END LOOP;
+
+      New_Line;
+    END IF;
+  END IF;
+
+  IF Ada.Strings.Fixed.Index(remdev.GetCapability, "GPIO") /= 0 THEN
+
+    -- Query the available GPIO pins
+
+    channels := remdev.GetAvailableChannels(RemoteIO.Channel_GPIO);
+
+    IF NOT channels.Is_Empty THEN
+      Put("Found GPIO pins:   ");
+
+      FOR pin OF channels LOOP
+        Put(Integer'Image(pin));
+      END LOOP;
+
+      New_Line;
+    END IF;
+  END IF;
+
+  IF Ada.Strings.Fixed.Index(remdev.GetCapability, "I2C") /= 0 THEN
+
+    -- Query the available I2C buses
+
+    channels := remdev.GetAvailableChannels(RemoteIO.Channel_I2C);
+
+    IF NOT channels.Is_Empty THEN
+      Put("Found I2C buses:   ");
+
+      FOR bus OF channels LOOP
+        Put(Integer'Image(bus));
+      END LOOP;
+
+      NEW_Line;
+    END IF;
+  END IF;
+
+  IF Ada.Strings.Fixed.Index(remdev.GetCapability, "PWM") /= 0 THEN
+
+    -- Query the available PWM outputs
+
+    channels := remdev.GetAvailableChannels(RemoteIO.Channel_PWM);
+
+    IF NOT channels.Is_Empty THEN
+      Put("Found PWM outputs: ");
+
+      FOR dev OF channels LOOP
+        Put(Integer'Image(dev));
+      END LOOP;
+
+      New_Line;
+    END IF;
+  END IF;
+
+  IF Ada.Strings.Fixed.Index(remdev.GetCapability, "SPI") /= 0 THEN
+
+    -- Query the available SPI devices
+
+    channels := remdev.GetAvailableChannels(RemoteIO.Channel_SPI);
+
+    IF NOT channels.Is_Empty THEN
+      Put("Found SPI devices: ");
+
+      FOR dev OF channels LOOP
+        Put(Integer'Image(dev));
+      END LOOP;
+
+      New_Line;
+    END IF;
+  END IF;
+
+  IF Ada.Strings.Fixed.Index(remdev.GetCapability, "DEVICE") /= 0 THEN
+
+    -- Query the available abstract devices
+
+    channels := remdev.GetAvailableChannels(RemoteIO.Channel_Device);
+
+    IF NOT channels.Is_Empty THEN
+      Put("Found devices:     ");
+
+      FOR dev OF channels LOOP
+        Put(Integer'Image(dev));
+      END LOOP;
+
+      New_Line;
+    END IF;
+  END IF;
+END test_query_serial;
