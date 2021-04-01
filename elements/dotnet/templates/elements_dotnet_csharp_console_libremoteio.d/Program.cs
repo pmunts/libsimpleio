9aae1805a81b23b7035eb5be0e6084218592d000
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2021-04-01 20:24:23 +0000

    Change tabs to spaces.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@19503 06ee2f96-5407-11de-8bab-f3d7589a4122

--- elements/dotnet/templates/elements_dotnet_csharp_console_libremoteio.d/Program.cs
+++ elements/dotnet/templates/elements_dotnet_csharp_console_libremoteio.d/Program.cs
@@ -2,19 +2,19 @@ using System;
 
 namespace $safeprojectname$
 {
-	static class Program
-	{
-		public static Int32 Main(string[] args)
-		{
-			Console.WriteLine("\nRemote I/O Protocol Client\n");
+  static class Program
+  {
+    public static Int32 Main(string[] args)
+    {
+      Console.WriteLine("\nRemote I/O Protocol Client\n");
 
-			IO.Remote.Device dev =
-				new IO.Remote.Device(new IO.Objects.USB.HID.Messenger());
+      IO.Remote.Device dev =
+        new IO.Remote.Device(new IO.Objects.USB.HID.Messenger());
 
-			Console.WriteLine(dev.Version);
-			Console.WriteLine(dev.Capabilities);
+      Console.WriteLine(dev.Version);
+      Console.WriteLine(dev.Capabilities);
 
-			return 0;
-		}
-	}
+      return 0;
+    }
+  }
 }
--- elements/dotnet/templates/elements_dotnet_csharp_console_libsimpleio.d/Program.cs
+++ elements/dotnet/templates/elements_dotnet_csharp_console_libsimpleio.d/Program.cs
@@ -2,12 +2,12 @@ using System;
 
 namespace $safeprojectname$
 {
-	static class Program
-	{
-		public static Int32 Main(string[] args)
-		{
-			Console.WriteLine("Hello, world!");
-			return 0;
-		}
-	}
+  static class Program
+  {
+    public static Int32 Main(string[] args)
+    {
+      Console.WriteLine("Hello, world!");
+      return 0;
+    }
+  }
 }
