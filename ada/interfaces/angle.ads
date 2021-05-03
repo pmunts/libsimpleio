c440720f3d67c0013c75b2045e8a848af0a7eeaf
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2021-05-03 04:30:33 +0000

    Changed package Angle from generic package back to normal package.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@19619 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/interfaces/angle.ads
+++ ada/interfaces/angle.ads
@@ -1,4 +1,4 @@
--- Instantiate Angle_Template with Standard.Float
+-- Abstract angle sensor and actuator interface definitions
 
 -- Copyright (C)2021, Philip Munts, President, Munts AM Corp.
 --
@@ -20,6 +20,103 @@
 -- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
-WITH Angle_Template;
+WITH Ada.Text_IO;
+WITH Ada.Numerics;
+WITH IO_Interfaces;
 
-PACKAGE Angle IS NEW Angle_Template(Standard.Float);
+PRIVATE WITH Ada.Numerics.Generic_Elementary_Functions;
+
+PACKAGE Angle IS
+
+  Error : EXCEPTION;
+
+  -- Define a type for full circle angle values
+
+  TYPE Degrees  IS NEW Float RANGE -360.0 .. +360.0;
+  TYPE Radians  IS NEW Float RANGE -2.0*Ada.Numerics.Pi .. 2.0*Ada.Numerics.Pi;
+
+  -- Define a type for angular velocity (radians per second)
+
+  TYPE Velocity IS NEW Float RANGE 0.0 .. Float'Last;
+
+  -- Instantiate text I/O packages
+
+  PACKAGE Radians_IO IS NEW Ada.Text_IO.Float_IO(Radians);
+
+  PACKAGE Degrees_IO IS NEW Ada.Text_IO.Float_IO(Degrees);
+
+  -- Instantiate abstract interfaces package
+
+  PACKAGE Interfaces IS NEW IO_Interfaces(Radians);
+
+  -- Interfaces
+
+  TYPE InputInterface IS INTERFACE AND Interfaces.InputInterface;
+
+  TYPE OutputInterface IS INTERFACE AND Interfaces.OutputInterface;
+
+  TYPE InputOutputInterface IS INTERFACE AND Interfaces.InputOutputInterface;
+
+  -- Access types
+
+  TYPE Input IS ACCESS ALL InputInterface'Class;
+
+  TYPE Output IS ACCESS ALL OutputInterface'Class;
+
+  TYPE InputOutput IS ACCESS ALL InputOutputInterface'Class;
+
+  -- Additional methods aka primitive operations
+
+  PROCEDURE Put
+   (Self  : IN OUT OutputInterface;
+    theta : Radians;
+    slew  : Velocity) IS ABSTRACT;
+
+  -- Conversion functions
+
+  FUNCTION To_Degrees(theta : Radians) RETURN Degrees;
+
+  FUNCTION To_Radians(theta : Degrees) RETURN Radians;
+
+  -- Trigonometric functions
+
+  FUNCTION Sin(theta : Radians) RETURN Float;
+
+  FUNCTION Cos(theta : Radians) RETURN Float;
+
+  FUNCTION Tan(theta : Radians) RETURN Float;
+
+  FUNCTION Cot(theta : Radians) RETURN Float;
+
+PRIVATE
+
+  Degrees_Per_Radian : CONSTANT := 180.0*Ada.Numerics.Pi;
+  Radians_Per_Degree : CONSTANT := Ada.Numerics.Pi/180.0;
+
+  -- Conversion functions
+
+  FUNCTION To_Degrees(theta : Radians) RETURN Degrees IS
+   (Degrees(Float(theta)*Degrees_Per_Radian));
+
+  FUNCTION To_Radians(theta : Degrees) RETURN Radians IS
+   (Radians(Float(theta)*Radians_Per_Degree));
+
+  -- Instantiate elementary functions
+
+  PACKAGE Trig IS NEW Ada.Numerics.Generic_Elementary_Functions(Float);
+
+  -- Trigonometric functions
+
+  FUNCTION Sin(theta : Radians) RETURN Float IS
+   (Trig.Sin(Float(theta)));
+
+  FUNCTION Cos(theta : Radians) RETURN Float IS
+   (Trig.Cos(Float(theta)));
+
+  FUNCTION Tan(theta : Radians) RETURN Float IS
+   (Trig.Tan(Float(theta)));
+
+  FUNCTION Cot(theta : Radians) RETURN Float IS
+   (Trig.Cot(Float(theta)));
+
+END Angle;
