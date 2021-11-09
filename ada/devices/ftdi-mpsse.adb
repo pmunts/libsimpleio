9f7202f51d86f9a9c02c8671cb281d0e4f697753
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2021-11-09 17:34:11 +0000

    Removed some unnecessary WITH clauses
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20025 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/devices/ftdi-mpsse.adb
+++ ada/devices/ftdi-mpsse.adb
@@ -18,8 +18,7 @@
 -- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
-WITH System;
-WITH Interfaces.C.Strings;
+WITH Interfaces.C;
 
 USE TYPE Interfaces.C.int;
 USE TYPE Interfaces.C.unsigned;
