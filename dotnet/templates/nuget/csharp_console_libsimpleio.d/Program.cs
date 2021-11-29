eed5c8f1c68f316e1e8f1bffda92aad014b803c2
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2021-11-29 04:04:14 +0000

    Added code to write to syslog.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20112 06ee2f96-5407-11de-8bab-f3d7589a4122

--- dotnet/templates/nuget/csharp_console_libsimpleio.d/Program.cs
+++ dotnet/templates/nuget/csharp_console_libsimpleio.d/Program.cs
@@ -1,3 +1,9 @@
 using static System.Console;
 
-WriteLine("Hello World!");
+WriteLine("\nHello, World!\n");
+
+IO.Interfaces.Log.Logger syslog =
+    new IO.Objects.libsimpleio.syslog.Logger("test_syslog",
+        IO.Objects.libsimpleio.syslog.Logger.LOG_LOCAL0);
+
+syslog.Note("Hello, World!");
