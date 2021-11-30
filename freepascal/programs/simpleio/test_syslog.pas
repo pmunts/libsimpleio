fe49aeae5f47473158aea2e10e15365d6be9f8f3
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2021-11-30 18:09:40 +0000

    Added test_syslog
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20130 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ freepascal/programs/simpleio/test_syslog.pas
@@ -0,0 +1,39 @@
+{ Linux syslog Test }
+
+{ Copyright (C)2021, Philip Munts, President, Munts AM Corp.                  }
+{                                                                             }
+{ Redistribution and use in source and binary forms, with or without          }
+{ modification, are permitted provided that the following conditions are met: }
+{                                                                             }
+{ * Redistributions of source code must retain the above copyright notice,    }
+{   this list of conditions and the following disclaimer.                     }
+{                                                                             }
+{ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" }
+{ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   }
+{ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  }
+{ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   }
+{ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         }
+{ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        }
+{ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    }
+{ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     }
+{ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     }
+{ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
+{ POSSIBILITY OF SUCH DAMAGE.                                                 }
+
+PROGRAM test_syslog;
+
+USES
+  libLinux;
+
+VAR
+  error  : Integer;
+
+BEGIN
+  Writeln;
+  Writeln('Linux syslog Test');
+  Writeln;
+
+  liblinux.OpenLog(LOG_PROGNAME, LOG_PID + LOG_PERROR, LOG_LOCAL0, error);
+  liblinux.WriteLog(LOG_NOTICE, 'This is a test.', error);
+  liblinux.CloseLog(error);
+END.
