de5820e135b7691988f9ebec2c1349b84b0dc8e9
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2021-12-10 06:49:38 +0000

    Removed the word Legacy
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20187 06ee2f96-5407-11de-8bab-f3d7589a4122

--- csharp/objects/simpleio/raspberrypi.cs
+++ csharp/objects/simpleio/raspberrypi.cs
@@ -42,61 +42,61 @@ namespace IO.Objects.libsimpleio.Platforms
 
         // The following GPIO pins are available on all Raspberry Pi Models
 
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO2 = new Designator(0, 2);    // I2C1 SDA
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO3 = new Designator(0, 3);    // I2C1 SCL
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO4 = new Designator(0, 4);
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO7 = new Designator(0, 7);    // SPI0 SS1
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO8 = new Designator(0, 8);    // SPI0 SS0
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO9 = new Designator(0, 9);    // SPI0 MISO
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO10 = new Designator(0, 10);  // SPI0 MOSI
-         /// <summary>Legacy GPIO pin designator</summary>
+         /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO11 = new Designator(0, 11);  // SPI0 SCLK
-         /// <summary>Legacy GPIO pin designator</summary>
+         /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO14 = new Designator(0, 14);  // UART0 TXD
-         /// <summary>Legacy GPIO pin designator</summary>
+         /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO15 = new Designator(0, 15);  // UART0 RXD
-         /// <summary>Legacy GPIO pin designator</summary>
+         /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO17 = new Designator(0, 17);
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO18 = new Designator(0, 18);  // PWM0
-         /// <summary>Legacy GPIO pin designator</summary>
+         /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO22 = new Designator(0, 22);
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO23 = new Designator(0, 23);
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO24 = new Designator(0, 24);
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO25 = new Designator(0, 25);
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO27 = new Designator(0, 27);
 
         // The following GPIO pins are only available on Raspberry Pi Model
         // B+ and later, with 40-pin expansion header
 
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO5 = new Designator(0, 5);
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO6 = new Designator(0, 6);
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO12 = new Designator(0, 12);
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO13 = new Designator(0, 13);
-        /// <summary>Legacy GPIO pin designator</summary>
+        /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO16 = new Designator(0, 16);  // SPI1 SS0
-         /// <summary>Legacy GPIO pin designator</summary>
+         /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO19 = new Designator(0, 19);  // SPI1 MISO, PWM1
-         /// <summary>Legacy GPIO pin designator</summary>
+         /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO20 = new Designator(0, 20);  // SPI1 MOSI
-         /// <summary>Legacy GPIO pin designator</summary>
+         /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO21 = new Designator(0, 21);  // SPI1 SCLK
-         /// <summary>Legacy GPIO pin designator</summary>
+         /// <summary> GPIO pin designator</summary>
         public static readonly Designator GPIO26 = new Designator(0, 26);
 
         // The following I2C buses are available on all Raspberry Pi Models
