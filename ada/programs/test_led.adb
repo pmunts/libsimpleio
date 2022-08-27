34022fd83eb5487eff34780a67784d90276d4745
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2022-08-27 21:44:20 +0000

    Added LED test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20441 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/programs/test_led.adb
@@ -0,0 +1,46 @@
+-- LED Test using libsimpleio
+
+-- Copyright (C)2022, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+WITH GPIO.libsimpleio;
+
+PROCEDURE test_led IS
+
+  LED : GPIO.Pin;
+
+BEGIN
+  New_Line;
+  Put_Line("LED Test using libsimpleio");
+  New_Line;
+
+  -- Configure GPIO output to drive an LED
+
+  LED := GPIO.libsimpleio.Create((0, 26), GPIO.Output);
+
+  -- Flash the LED
+
+  LOOP
+    LED.Put(NOT LED.Get);
+    DELAY 0.5;
+  END LOOP;
+END test_led;
