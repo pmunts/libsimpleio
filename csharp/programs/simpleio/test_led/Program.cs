0f3ab256cc7331d65f0e361f463b1179a326bee7
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2022-10-22 04:51:26 +0000

    Added LED (on GPIO 26) toggle test.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20511 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ csharp/programs/simpleio/test_led/Program.cs
@@ -0,0 +1,51 @@
+// LED Toggle Test Using GPIO Output
+
+// Copyright (C)2022, Philip Munts, President, Munts AM Corp.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+
+namespace test_led
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nLED Toggle Test Using GPIO Output\n");
+
+            // Create GPIO output object
+
+            IO.Objects.libsimpleio.Device.Designator desg_LED =
+                new IO.Objects.libsimpleio.Device.Designator(0, 26);
+
+            IO.Interfaces.GPIO.Pin LED =
+                new IO.Objects.libsimpleio.GPIO.Pin(desg_LED,
+                    IO.Interfaces.GPIO.Direction.Output, false);
+
+            Console.WriteLine("\nPress CONTROL-C to exit");
+
+            for (;;)
+            {
+                LED.state = !LED.state;
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+    }
+}
