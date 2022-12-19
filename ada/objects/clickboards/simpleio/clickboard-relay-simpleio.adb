9a346a30d68ed79538030fd7c776eba64c5985f4
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2022-12-19 07:21:42 +0000

    Added support for the Mikroelektronika Relay Click (MIKROE-1370).
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20553 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/objects/clickboards/simpleio/clickboard-relay-simpleio.adb
@@ -0,0 +1,56 @@
+-- Services for the Mikroelektronika Relay Click (MIKROE-1370)
+
+-- Copyright (C)2022, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+-- NOTES: RELAY1 is driven by PWM and RELAY2 is driven by CS.
+
+WITH ClickBoard.SimpleIO;
+WITH GPIO.libsimpleio;
+
+PACKAGE BODY ClickBoard.Relay.SimpleIO IS
+
+  -- Create relay object from socket
+
+  FUNCTION Create
+   (socket   : NOT NULL ClickBoard.SimpleIO.Socket;
+    relaynum : RelayNumber) RETURN GPIO.Pin IS
+
+  BEGIN
+    CASE relaynum IS
+      WHEN 1 => RETURN GPIO.libsimpleio.Create(socket.GPIO(PWM), GPIO.Output, False);
+      WHEN 2 => RETURN GPIO.libsimpleio.Create(socket.GPIO(CS),  GPIO.Output, False);
+    END CASE;
+  END Create;
+
+  -- Create relay object from socket number
+
+  FUNCTION Create
+   (socknum  : Positive;
+    relaynum : RelayNumber) RETURN GPIO.Pin IS
+
+    socket : NOT NULL ClickBoard.SimpleIO.Socket :=
+      ClickBoard.SimpleIO.Create(socknum);
+
+  BEGIN
+    RETURN Create(socket, relaynum);
+  END Create;
+
+END ClickBoard.Relay.SimpleIO;
---
+++ ada/programs/test_clickboard_relay.adb
@@ -0,0 +1,42 @@
+-- Mikroelektronika Relay Click (MIKROE-1370) Toggle Test
+
+-- Copyright (C)2022, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.test_clickboard_relay
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+WITH ClickBoard.Relay.SimpleIO;
+WITH GPIO;
+
+PROCEDURE test_clickboard_relay IS
+
+  Relay1 : CONSTANT GPIO.Pin := ClickBoard.Relay.SimpleIO.Create(1, 1);
+  Relay2 : CONSTANT GPIO.Pin := ClickBoard.Relay.SimpleIO.Create(1, 2);
+
+BEGIN
+  LOOP
+    Relay1.Put(True);
+    Relay2.Put(False);
+    DELAY 1.0;
+    Relay1.Put(False);
+    Relay2.Put(True);
+    DELAY 1.0;
+  END LOOP;
+END test_clickboard_relay;
