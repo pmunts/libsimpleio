7d7aab87834c1a0986d832412fca2270775e3ece
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2022-12-28 00:35:29 +0000

    Added support for the Digilent Pmod Hygro.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20567 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/objects/clickboards/simpleio/clickboard-pmodhygro-simpleio.adb
@@ -0,0 +1,57 @@
+-- Services for the Digilent Pmod HYGRO, using libsimpleio
+
+-- Copyright (C)2022, Philip Munts, President, Munts AM Corp.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH I2C.libsimpleio;
+
+PACKAGE BODY ClickBoard.PmodHygro.SimpleIO IS
+
+  -- Create HDC1080 sensor object from socket instance
+
+  FUNCTION Create(socket : ClickBoard.SimpleIO.SocketSubclass) RETURN HDC1080.Device IS
+
+    bus : I2C.Bus;
+
+  BEGIN
+    bus := I2C.libsimpleio.Create(socket.I2C);
+    RETURN ClickBoard.PmodHygro.Create(bus);
+  END Create;
+
+  -- Create HDC1080 sensor object from socket object
+
+  FUNCTION Create(socket : NOT NULL ClickBoard.SimpleIO.Socket) RETURN HDC1080.Device IS
+
+  BEGIN
+    RETURN Create(socket.ALL);
+  END Create;
+
+  -- Create HDC1080 sensor object from socket number
+
+  FUNCTION Create(socknum : Positive) RETURN HDC1080.Device IS
+
+    socket : ClickBoard.SimpleIO.SocketSubclass;
+
+  BEGIN
+    socket.Initialize(socknum);
+    RETURN Create(socket);
+  END Create;
+
+END ClickBoard.PmodHygro.SimpleIO;
