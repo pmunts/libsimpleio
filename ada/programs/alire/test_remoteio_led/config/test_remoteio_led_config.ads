ca4eccab1c8ac0596f2bdd9f8f7adbf72e8d76e3
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-01-16 21:13:34 +0000

    Minor casing fixup
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20654 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/programs/alire/test_remoteio_led/config/test_remoteio_led_config.ads
+++ ada/programs/alire/test_remoteio_led/config/test_remoteio_led_config.ads
@@ -2,7 +2,7 @@
 pragma Restrictions (No_Elaboration_Code);
 pragma Style_Checks (Off);
 
-package Test_RemoteIO_LED_Config is
+package Test_Remoteio_Led_Config is
    pragma Pure;
 
    Crate_Version : constant String := "0.1.0-dev";
@@ -17,4 +17,4 @@ package Test_RemoteIO_LED_Config is
    type Build_Profile_Kind is (release, validation, development);
    Build_Profile : constant Build_Profile_Kind := development;
 
-end Test_RemoteIO_LED_Config;
+end Test_Remoteio_Led_Config;
