05f2d722f61246242c0bde0886a355a0b6a5fa97
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-02-27 01:39:18 +0000

    Use implied typing for variable remdev
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20786 06ee2f96-5407-11de-8bab-f3d7589a4122

--- elements/dotnet/templates/elements_dotnet_swift_console_libremoteio.d/Program.swift
+++ elements/dotnet/templates/elements_dotnet_swift_console_libremoteio.d/Program.swift
@@ -1,7 +1,6 @@
 print("Remote I/O Protocol Client")
 
-var remdev : IO.Objects.RemoteIO.Device =
-  IO.Objects.RemoteIO.Device(IO.Objects.USB.HID.Messenger())
+var remdev = IO.Objects.RemoteIO.Device(IO.Objects.USB.HID.Messenger())
 
 print(remdev.Version)
 print(remdev.Capabilities)
