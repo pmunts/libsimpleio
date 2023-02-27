f0ab0f29e5030ee0372b8baf82577fc4cf2a009b
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-02-27 02:56:20 +0000

    Added project templates for Go using libremoteio and libsimpleio.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20788 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ elements/dotnet/templates/elements_dotnet_go_console_libremoteio.d/Program.go
@@ -0,0 +1,16 @@
+package elements_dotnet_go_console_libremoteio1
+
+import "fmt"
+
+func main() {
+  fmt.Println("\nRemote I/O Protocol Client\n")
+
+  // Create Remote I/O Protocol server object instance
+
+  var remdev = IO.Objects.RemoteIO.Device { IO.Objects.USB.HID.Messenger {} }
+
+  // Query the Remote I/O Protocol server
+
+  fmt.Println(remdev.Version)
+  fmt.Println(remdev.Capabilities)
+}
---
+++ elements/dotnet/templates/elements_dotnet_go_console_libsimpleio.d/Program.go
@@ -0,0 +1,6 @@
+package $safeprojectname$
+
+func main() {
+  var log = IO.Objects.SimpleIO.syslog.Logger {}
+  log.Note("Hello, World!")
+}
