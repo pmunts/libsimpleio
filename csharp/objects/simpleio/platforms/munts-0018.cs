9e0807a3a567732fbb722a0bc855b59d0e0882a3
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-03-01 07:21:57 +0000

    Added I/O device object factory functions.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20824 06ee2f96-5407-11de-8bab-f3d7589a4122

--- csharp/objects/simpleio/platforms/munts-0018.cs
+++ csharp/objects/simpleio/platforms/munts-0018.cs
@@ -21,6 +21,7 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 using IO.Objects.SimpleIO.Device;
+using static IO.Objects.SimpleIO.GPIO.Pin;
 using static IO.Objects.SimpleIO.Platforms.RaspberryPi;
 
 namespace IO.Objects.SimpleIO.Platforms
@@ -202,5 +203,103 @@ namespace IO.Objects.SimpleIO.Platforms
         /// <c>J11</c> pin <c>A1</c> (MCP3204 input <c>CH1</c>.
         /// </summary>
         public static readonly Designator J11A1 = new Designator(0, 1);
+
+        /// <summary>
+        /// Analog input factory for the on-board analog inputs at <c>J10</c>
+        /// and <c>J11</c>.
+        /// </summary>
+        /// <param name="desg">Device designator for one of the on-board
+        /// analog inputs (<c>J10A0</c>, <c>J10A1</c>, <c>J11A0</c>, or
+        /// <c>J11A1</c>).</param>
+        /// <returns>Analog input object.</returns>
+        public static IO.Interfaces.ADC.Voltage AnalogInputFactory(Designator desg)
+        {
+            return new IO.Interfaces.ADC.Input(new IO.Objects.SimpleIO.ADC.Sample(desg, 12), 3.3);
+        }
+
+        /// <summary>
+        /// GPIO pin object factory for the on-board button switch at <c>SW1</c>.
+        /// </summary>
+        /// <param name="edge">Interrupt edge setting.</param>
+        /// <returns>GPIO input pin object.</returns>
+        public static IO.Interfaces.GPIO.Pin ButtonInputFactory(IO.Objects.SimpleIO.GPIO.Pin.Edge edge = IO.Objects.SimpleIO.GPIO.Pin.Edge.None)
+        {
+            return new IO.Objects.SimpleIO.GPIO.Pin(SW1, IO.Interfaces.GPIO.Direction.Input, false, IO.Objects.SimpleIO.GPIO.Pin.Driver.PushPull, edge);
+        }
+
+        /// <summary>
+        /// GPIO pin factory for the on-board LED at <c>D1</c>.
+        /// </summary>
+        /// <param name="state">Initial GPIO output state.</param>
+        /// <returns>GPIO output pin object.</returns>
+        public static IO.Interfaces.GPIO.Pin LEDOutputFactory(bool state = false)
+        {
+            return new IO.Objects.SimpleIO.GPIO.Pin(D1, IO.Interfaces.GPIO.Direction.Output, state);
+        }
+
+        /// <summary>
+        /// GPIO pin factory for GPIO pins at <c>J4</c> to <c>J6</c>.
+        /// </summary>
+        /// <param name="desg">Device designator for one of the on-board GPIO
+        /// pins (<c>J4D0</c> to <c>J7D1</c>.</param>
+        /// <param name="dir">Data direction.</param>
+        /// <param name="state">Initial GPIO output state.</param>
+        /// <param name="driver">Output driver setting.</param>
+        /// <param name="edge">Interrupt edge setting.</param>
+        /// <param name="polarity">Polarity setting.</param>
+        /// <returns>GPIO pin object.</returns>
+        public static IO.Interfaces.GPIO.Pin GPIOPinFactory(Designator desg,
+            IO.Interfaces.GPIO.Direction dir, bool state = false,
+            Driver driver = Driver.PushPull, Edge edge = Edge.None,
+            Polarity polarity = Polarity.ActiveHigh)
+        {
+            return new IO.Objects.SimpleIO.GPIO.Pin(desg, dir, state, driver, edge, polarity);
+        }
+
+        /// <summary>
+        /// I<sup>2</sup>C bus object factory for the on-board I<sup>2</sup>C
+        /// buses at <c>J5</c> and <c>J9</c>.
+        /// </summary>
+        /// <param name="desg">Device designator for one of the on-board
+        /// I<sup>2</sup>C buses (<c>J5I2C</c> or <c>J9I2C</c>).</param>
+        /// <returns>I<sup>2</sup>C bus object.</returns>
+        public static IO.Interfaces.I2C.Bus I2CBusFactory(Designator desg)
+        {
+            return new IO.Objects.SimpleIO.I2C.Bus(desg);
+        }
+
+        /// <summary>
+        /// Motor output object factory for the on-board motor outputs
+        /// at <c>J6</c> and <c>J7</c>.
+        /// </summary>
+        /// <param name="speed">Device designator for the motor speed PWM
+        /// output (<c>J6PWM</c> or <c>J7PWM</c>).</param>
+        /// <param name="direction">Device designator for the motor direction
+        /// GPIO output (<c>J6DIR</c> or <c>J7DIR</c>).</param>
+        /// <param name="frequency">PWM pulse frequency.</param>
+        /// <param name="velocity">Initial motor velocity.</param>
+        /// <returns>Motor output object.</returns>
+        public static IO.Interfaces.Motor.Output MotorOutputFactory(Designator speed,
+            Designator direction, int frequency, double velocity = 0.0)
+        {
+            IO.Interfaces.PWM.Output S = new IO.Objects.SimpleIO.PWM.Output(speed, frequency);
+            IO.Interfaces.GPIO.Pin D = new IO.Objects.SimpleIO.GPIO.Pin(direction, IO.Interfaces.GPIO.Direction.Output);
+
+            return new IO.Objects.Motor.PWM.Output(D, S, velocity);
+        }
+
+        /// <summary>
+        /// Servo output object factory for the on-board servo outputs
+        /// at <c>J2</c> and <c>J3</c>.
+        /// </summary>
+        /// <param name="desg">Device designator for one of the on-board
+        /// PWM outputs (<c>J2PWM</c> or <c>J3PWM</c>).</param>
+        /// <param name="frequency">PWM pulse frequency.  Ordinary analog
+        /// servos operate best at 50 Hz.</param>
+        /// <returns>Servo output object.</returns>
+        public static IO.Interfaces.Servo.Output ServoOutputFactory(Designator desg, int frequency = 50)
+        {
+            return new IO.Objects.SimpleIO.Servo.Output(desg, frequency);
+        }
     }
 }
