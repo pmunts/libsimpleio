2fcbff02b0757a10cf5ef2fbab79664df232486a
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-03-02 23:04:26 +0000

    Added more cross-references, more documentation.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@20843 06ee2f96-5407-11de-8bab-f3d7589a4122

--- csharp/objects/simpleio/platforms/pocketbeagle.cs
+++ csharp/objects/simpleio/platforms/pocketbeagle.cs
@@ -30,133 +30,186 @@ namespace IO.Objects.SimpleIO.Platforms
     /// </summary>
     public static class PocketBeagle
     {
-        /// <summary>ADC input designator for P1.19 (1.8V)</summary>
+        /// <summary>ADC input designator (0,0) for P1.19 (1.8V)</summary>
         /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
-        public static readonly Designator AIN0 = new Designator(0, 0);      // P1.19 1.8V
-        /// <summary>ADC input designator for P1.21 (1.8V)</summary>
-        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
-        public static readonly Designator AIN1 = new Designator(0, 1);      // P1.21 1.8V
-        /// <summary>ADC input designator for P1.23 (1.8V)</summary>
-        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
-        public static readonly Designator AIN2 = new Designator(0, 2);      // P1.23 1.8V
-        /// <summary>ADC input designator for P1.25 (1.8V)</summary>
-        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
-        public static readonly Designator AIN3 = new Designator(0, 3);      // P1.25 1.8V
-        /// <summary>ADC input designator for P1.27 (1.8V)</summary>
-        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
-        public static readonly Designator AIN4 = new Designator(0, 4);      // P1.27 1.8V
-        /// <summary>ADC input designator for P2.35 (3.6V)</summary>
-        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
-        public static readonly Designator AIN5 = new Designator(0, 5);      // P2.35 3.6V
-        /// <summary>ADC input designator for P1.2 (3.6V)</summary>
-        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
-        public static readonly Designator AIN6 = new Designator(0, 6);      // P1.2 3.6
-        /// <summary>ADC input designator for P2.36 (1.8V)</summary>
-        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
-        public static readonly Designator AIN7 = new Designator(0, 7);      // P2.36 1.8V
+        public static readonly Designator AIN0 = new Designator(0, 0);
 
-        /// <summary>Legacy GPIO pin designator for P1.8</summary>
-        public static readonly Designator GPIO2 = new Designator(0, 2);     // P1.8   SPI0 SCLK
-        /// <summary>Legacy GPIO pin designator for P1.10</summary>
-        public static readonly Designator GPIO3 = new Designator(0, 3);     // P1.10  SPI0 MISO
-        /// <summary>Legacy GPIO pin designator for P1.12</summary>
-        public static readonly Designator GPIO4 = new Designator(0, 4);     // P1.12  SPI0 MOSI
-        /// <summary>Legacy GPIO pin designator for P1.6</summary>
-        public static readonly Designator GPIO5 = new Designator(0, 5);     // P1.6   SPI0 CS
-        /// <summary>Legacy GPIO pin designator for P2.29</summary>
-        public static readonly Designator GPIO7 = new Designator(0, 7);     // P2.29  SPI1 SCLK
-        /// <summary>Legacy GPIO pin designator for P1.26</summary>
-        public static readonly Designator GPIO12 = new Designator(0, 12);   // P1.26  I2C2 SDA
-        /// <summary>Legacy GPIO pin designator for P1.28</summary>
-        public static readonly Designator GPIO13 = new Designator(0, 13);   // P1.28  I2C2 SCL
-        /// <summary>Legacy GPIO pin designator for P2.11</summary>
-        public static readonly Designator GPIO14 = new Designator(0, 14);   // P2.11  I2C1 SDA
-        /// <summary>Legacy GPIO pin designator for P2.9</summary>
-        public static readonly Designator GPIO15 = new Designator(0, 15);   // P2.9   I2C1 SCL
-        /// <summary>Legacy GPIO pin designator for P2.31</summary>
-        public static readonly Designator GPIO19 = new Designator(0, 19);   // P2.31  SPI1 CS
-        /// <summary>Legacy GPIO pin designator for P1.20</summary>
-        public static readonly Designator GPIO20 = new Designator(0, 20);   // P1.20
-        /// <summary>Legacy GPIO pin designator for P2.3</summary>
-        public static readonly Designator GPIO23 = new Designator(0, 23);   // P2.3
-        /// <summary>Legacy GPIO pin designator for P1.34</summary>
-        public static readonly Designator GPIO26 = new Designator(0, 26);   // P1.34
-        /// <summary>Legacy GPIO pin designator for P2.19</summary>
-        public static readonly Designator GPIO27 = new Designator(0, 27);   // P2.19
-        /// <summary>Legacy GPIO pin designator for P2.5</summary>
-        public static readonly Designator GPIO30 = new Designator(0, 30);   // P2.5   RXD4
-        /// <summary>Legacy GPIO pin designator for P2.7</summary>
-        public static readonly Designator GPIO31 = new Designator(0, 31);   // P2.7   TXD4
-        /// <summary>Legacy GPIO pin designator for P2.27</summary>
-        public static readonly Designator GPIO40 = new Designator(1, 8);    // P2.27  SPI1 MISO
-        /// <summary>Legacy GPIO pin designator for P2.25</summary>
-        public static readonly Designator GPIO41 = new Designator(1, 9);    // P2.25  SPI1 MOSI
-        /// <summary>Legacy GPIO pin designator for P1.32</summary>
-        public static readonly Designator GPIO42 = new Designator(1, 10);   // P1.32  RXD0
-        /// <summary>Legacy GPIO pin designator for P1.30</summary>
-        public static readonly Designator GPIO43 = new Designator(1, 11);   // P1.30  TXD0
-        /// <summary>Legacy GPIO pin designator for P2.24</summary>
-        public static readonly Designator GPIO44 = new Designator(1, 12);   // P2.24
-        /// <summary>Legacy GPIO pin designator for P2.33</summary>
-        public static readonly Designator GPIO45 = new Designator(1, 13);   // P2.33
-        /// <summary>Legacy GPIO pin designator for P2.22</summary>
-        public static readonly Designator GPIO46 = new Designator(1, 14);   // P2.22
-        /// <summary>Legacy GPIO pin designator for P2.18</summary>
-        public static readonly Designator GPIO47 = new Designator(1, 15);   // P2.18
-        /// <summary>Legacy GPIO pin designator for P2.1</summary>
-        public static readonly Designator GPIO50 = new Designator(1, 18);   // P2.1
-        /// <summary>Legacy GPIO pin designator for P2.10</summary>
-        public static readonly Designator GPIO52 = new Designator(1, 20);   // P2.10
-        /// <summary>Legacy GPIO pin designator for P2.6</summary>
-        public static readonly Designator GPIO57 = new Designator(1, 25);   // P2.6
-        /// <summary>Legacy GPIO pin designator for P2.4</summary>
-        public static readonly Designator GPIO58 = new Designator(1, 26);   // P2.4
-        /// <summary>Legacy GPIO pin designator for P2.2</summary>
-        public static readonly Designator GPIO59 = new Designator(1, 27);   // P2.2
-        /// <summary>Legacy GPIO pin designator for P2.8</summary>
-        public static readonly Designator GPIO60 = new Designator(1, 28);   // P2.8
-        /// <summary>Legacy GPIO pin designator for P2.20</summary>
-        public static readonly Designator GPIO64 = new Designator(2, 0);    // P2.20
-        /// <summary>Legacy GPIO pin designator for P2.17</summary>
-        public static readonly Designator GPIO65 = new Designator(2, 1);    // P2.17
-        /// <summary>Legacy GPIO pin designator for P2.35</summary>
-        public static readonly Designator GPIO86 = new Designator(2, 22);   // P2.35  AIN5 3.3V
-        /// <summary>Legacy GPIO pin designator for P1.2</summary>
-        public static readonly Designator GPIO87 = new Designator(2, 23);   // P1.2   AIN6 3.3V
-        /// <summary>Legacy GPIO pin designator for P1.35</summary>
-        public static readonly Designator GPIO88 = new Designator(2, 24);   // P1.35
-        /// <summary>Legacy GPIO pin designator for P1.4</summary>
-        public static readonly Designator GPIO89 = new Designator(2, 25);   // P1.4
-        /// <summary>Legacy GPIO pin designator for P1.36</summary>
-        public static readonly Designator GPIO110 = new Designator(3, 14);  // P1.36
-        /// <summary>Legacy GPIO pin designator for P1.33</summary>
-        public static readonly Designator GPIO111 = new Designator(3, 15);  // P1.33
-        /// <summary>Legacy GPIO pin designator for P2.32</summary>
-        public static readonly Designator GPIO112 = new Designator(3, 16);  // P2.32
-        /// <summary>Legacy GPIO pin designator for P2.30</summary>
-        public static readonly Designator GPIO113 = new Designator(3, 17);  // P2.30
-        /// <summary>Legacy GPIO pin designator for P1.31</summary>
-        public static readonly Designator GPIO114 = new Designator(3, 18);  // P1.31
-        /// <summary>Legacy GPIO pin designator for P2.34</summary>
-        public static readonly Designator GPIO115 = new Designator(3, 19);  // P2.34
-        /// <summary>Legacy GPIO pin designator for P2.28</summary>
-        public static readonly Designator GPIO116 = new Designator(3, 20);  // P2.28
-        /// <summary>Legacy GPIO pin designator for P1.29</summary>
-        public static readonly Designator GPIO117 = new Designator(3, 21);  // P1.29
+        /// <summary>ADC input designator (0,1) for P1.21 (1.8V)</summary>
+        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
+        public static readonly Designator AIN1 = new Designator(0, 1);
 
-        /// <summary>I2C bus designator for P1.26 and P1.28</summary>
-        public static readonly Designator I2C1 = new Designator(0, 1);      // P1.26 and P1.28
-        /// <summary>I2C bus designator for P2.9 and P2.11</summary>
-        public static readonly Designator I2C2 = new Designator(0, 2);      // P2.9 and P2.11
+        /// <summary>ADC input designator (0,2) for P1.23 (1.8V)</summary>
+        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
+        public static readonly Designator AIN2 = new Designator(0, 2);
 
-        /// <summary>PWM output designator for P1.36</summary>
-        public static readonly Designator PWM0_0 = new Designator(0, 0);    // P1.36
-        /// <summary>PWM output designator for P2.1</summary>
-        public static readonly Designator PWM2_0 = new Designator(2, 0);    // P2.1
+        /// <summary>ADC input designator (0,3) for P1.25 (1.8V)</summary>
+        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
+        public static readonly Designator AIN3 = new Designator(0, 3);
 
-        /// <summary>SPI slave select designator for P1.6</summary>
-        public static readonly Designator SPI0_0 = new Designator(0, 0);    // P1.6
-        /// <summary>SPI slave select designator for P2.31</summary>
-        public static readonly Designator SPI1_1 = new Designator(1, 1);    // P2.31
+        /// <summary>ADC input designator (0,4) for P1.27 (1.8V)</summary>
+        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
+        public static readonly Designator AIN4 = new Designator(0, 4);
+
+        /// <summary>ADC input designator (0,5) for P2.35 (3.6V)</summary>
+        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
+        public static readonly Designator AIN5 = new Designator(0, 5);
+
+        /// <summary>ADC input designator (0,6) for P1.2 (3.6V)</summary>
+        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
+        public static readonly Designator AIN6 = new Designator(0, 6);
+
+        /// <summary>ADC input designator (0,7) for P2.36 (1.8V)</summary>
+        /// <remarks>Requires the <c>PB-ADC</c> device tree overlay.</remarks>
+        public static readonly Designator AIN7 = new Designator(0, 7);
+
+        /// <summary>GPIO pin designator (0,2) for P1.8 aka <c>SPI0 SCLK</c></summary>
+        public static readonly Designator GPIO2 = new Designator(0, 2);
+
+        /// <summary>GPIO pin designator (0,3) for P1.10 aka <c>SPI0 MISO</c></summary>
+        public static readonly Designator GPIO3 = new Designator(0, 3);
+
+        /// <summary>GPIO pin designator (0,4) for P1.12 aka <c>SPI0 MOSI</c></summary>
+        public static readonly Designator GPIO4 = new Designator(0, 4);
+
+        /// <summary>GPIO pin designator (0,5) for P1.6 aka <c>SPI0 CS0</c></summary>
+        public static readonly Designator GPIO5 = new Designator(0, 5);
+
+        /// <summary>GPIO pin designator (0,7) for P2.29 aka <c>SPI1 SCLK</c></summary>
+        public static readonly Designator GPIO7 = new Designator(0, 7);
+
+        /// <summary>GPIO pin designator (0,12) for P1.26 aka <c>I2C2 SDA</c></summary>
+        public static readonly Designator GPIO12 = new Designator(0, 12);
+
+        /// <summary>GPIO pin designator (0,13) for P1.28 aka <c>I2C2 SCL</c></summary>
+        public static readonly Designator GPIO13 = new Designator(0, 13);
+
+        /// <summary>GPIO pin designator (0,14) for P2.11 aka <c>I2C1 SDA</c></summary>
+        public static readonly Designator GPIO14 = new Designator(0, 14);
+
+        /// <summary>GPIO pin designator (0,15) for P2.9 aka <c>I2C1 SCL</c></summary>
+        public static readonly Designator GPIO15 = new Designator(0, 15);
+
+        /// <summary>GPIO pin designator (0,19) for P2.31 aka <c>SPI1 CS1</c></summary>
+        public static readonly Designator GPIO19 = new Designator(0, 19);
+
+        /// <summary>GPIO pin designator (0,20) for P1.20</summary>
+        public static readonly Designator GPIO20 = new Designator(0, 20);
+
+        /// <summary>GPIO pin designator (0,23) for P2.3</summary>
+        public static readonly Designator GPIO23 = new Designator(0, 23);
+
+        /// <summary>GPIO pin designator (0,26) for P1.34</summary>
+        public static readonly Designator GPIO26 = new Designator(0, 26);
+
+        /// <summary>GPIO pin designator (0,27) for P2.19</summary>
+        public static readonly Designator GPIO27 = new Designator(0, 27);
+
+        /// <summary>GPIO pin designator (0,30) for P2.5 aka <c>RXD4</c></summary>
+        public static readonly Designator GPIO30 = new Designator(0, 30);
+
+        /// <summary>GPIO pin designator (0,31) for P2.7 aka <c>TXD4</c></summary>
+        public static readonly Designator GPIO31 = new Designator(0, 31);
+
+        /// <summary>GPIO pin designator (1,8) for P2.27 aka <c>SPI1 MISO</c></summary>
+        public static readonly Designator GPIO40 = new Designator(1, 8);
+
+        /// <summary>GPIO pin designator (1,9) for P2.25 aka <c>SPI1 MOSI</c></summary>
+        public static readonly Designator GPIO41 = new Designator(1, 9);
+
+        /// <summary>GPIO pin designator (1,10) for P1.32 aka <c>RXD0</c></summary>
+        public static readonly Designator GPIO42 = new Designator(1, 10);
+
+        /// <summary>GPIO pin designator (1,11) for P1.30 aka <c>TXD0</c></summary>
+        public static readonly Designator GPIO43 = new Designator(1, 11);
+
+        /// <summary>GPIO pin designator (1,12) for P2.24</summary>
+        public static readonly Designator GPIO44 = new Designator(1, 12);
+
+        /// <summary>GPIO pin designator (1,13) for P2.33</summary>
+        public static readonly Designator GPIO45 = new Designator(1, 13);
+
+        /// <summary>GPIO pin designator (1,14) for P2.22</summary>
+        public static readonly Designator GPIO46 = new Designator(1, 14);
+
+        /// <summary>GPIO pin designator (1,15) for P2.18</summary>
+        public static readonly Designator GPIO47 = new Designator(1, 15);
+
+        /// <summary>GPIO pin designator (1,18) for P2.1</summary>
+        public static readonly Designator GPIO50 = new Designator(1, 18);
+
+        /// <summary>GPIO pin designator (1,20) for P2.10</summary>
+        public static readonly Designator GPIO52 = new Designator(1, 20);
+
+        /// <summary>GPIO pin designator (1,25) for P2.6</summary>
+        public static readonly Designator GPIO57 = new Designator(1, 25);
+
+        /// <summary>GPIO pin designator (1,26) for P2.4</summary>
+        public static readonly Designator GPIO58 = new Designator(1, 26);
+
+        /// <summary>GPIO pin designator (1,27) for P2.2</summary>
+        public static readonly Designator GPIO59 = new Designator(1, 27);
+
+        /// <summary>GPIO pin designator (1,28) for P2.8</summary>
+        public static readonly Designator GPIO60 = new Designator(1, 28);
+
+        /// <summary>GPIO pin designator (2,0) for P2.20</summary>
+        public static readonly Designator GPIO64 = new Designator(2, 0);
+
+        /// <summary>GPIO pin designator (2,1) for P2.17</summary>
+        public static readonly Designator GPIO65 = new Designator(2, 1);
+
+        /// <summary>GPIO pin designator (2,22) for P2.35 aka <c>AIN5</c></summary>
+        public static readonly Designator GPIO86 = new Designator(2, 22);
+
+        /// <summary>GPIO pin designator (2,23) for P1.2 aka <c>AIN6</c></summary>
+        public static readonly Designator GPIO87 = new Designator(2, 23);
+
+        /// <summary>GPIO pin designator (2,24) for P1.35</summary>
+        public static readonly Designator GPIO88 = new Designator(2, 24);
+
+        /// <summary>GPIO pin designator (2,25) for P1.4</summary>
+        public static readonly Designator GPIO89 = new Designator(2, 25);
+
+        /// <summary>GPIO pin designator (3,14) for P1.36</summary>
+        public static readonly Designator GPIO110 = new Designator(3, 14);
+
+        /// <summary>GPIO pin designator (3,15) for P1.33</summary>
+        public static readonly Designator GPIO111 = new Designator(3, 15);
+
+        /// <summary>GPIO pin designator (3,16) for P2.32</summary>
+        public static readonly Designator GPIO112 = new Designator(3, 16);
+
+        /// <summary>GPIO pin designator (3,17) for P2.30</summary>
+        public static readonly Designator GPIO113 = new Designator(3, 17);
+
+        /// <summary>GPIO pin designator (3,18) for P1.31</summary>
+        public static readonly Designator GPIO114 = new Designator(3, 18);
+
+        /// <summary>GPIO pin designator (3,19) for P2.34</summary>
+        public static readonly Designator GPIO115 = new Designator(3, 19);
+
+        /// <summary>GPIO pin designator (3,20) for P2.28</summary>
+        public static readonly Designator GPIO116 = new Designator(3, 20);
+
+        /// <summary>GPIO pin designator (3,21) for P1.29</summary>
+        public static readonly Designator GPIO117 = new Designator(3, 21);
+
+        /// <summary>I2C bus designator (0,1) for P2.9 and P2.11 aka <c>GPIO15</c> and <c>GPIO14</c></summary>
+        public static readonly Designator I2C1 = new Designator(0, 1);
+
+        /// <summary>I2C bus designator (0,2) for P1.26 and P1.28 aka <c>GPIO12</c> and <c>GPIO13</c></summary>
+        public static readonly Designator I2C2 = new Designator(0, 2);
+
+        /// <summary>PWM output designator (0,0) for P1.36 aka <c>GPIO110</c></summary>
+        public static readonly Designator PWM0_0 = new Designator(0, 0);
+
+        /// <summary>PWM output designator (2,0) for P2.1 aka <c>GPIO50</c></summary>
+        public static readonly Designator PWM2_0 = new Designator(2, 0);
+
+        /// <summary>SPI slave designator (0,0) for P1.6 aka <c>GPIO5</c></summary>
+        public static readonly Designator SPI0_0 = new Designator(0, 0);
+
+        /// <summary>SPI slave designator (1,1) for P2.31 aka <c>GPIO19</c></summary>
+        public static readonly Designator SPI1_1 = new Designator(1, 1);
     }
 }
