373004c847b0db916fd81a1b573c387b1ffc5cd1
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-04-08 18:12:16 +0000

    Added Linux syslog test.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@21077 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ elements/dotnet/programs/simpleio/test_syslog/Program.pas
@@ -0,0 +1,70 @@
+{ Linux syslog Test }
+
+{ Copyright (C)2023, Philip Munts.                                            }
+{                                                                             }
+{ Redistribution and use in source and binary forms, with or without          }
+{ modification, are permitted provided that the following conditions are met: }
+{                                                                             }
+{ * Redistributions of source code must retain the above copyright notice,    }
+{   this list of conditions and the following disclaimer.                     }
+{                                                                             }
+{ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' }
+{ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   }
+{ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  }
+{ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   }
+{ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         }
+{ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        }
+{ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    }
+{ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     }
+{ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     }
+{ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
+{ POSSIBILITY OF SUCH DAMAGE.                                                 }
+
+namespace test_syslog;
+
+  procedure Main(args: array of String);
+
+  begin
+    writeLn;
+    writeLn('Linux syslog Test');
+    writeLn;
+
+    // Create a Logger instance
+
+    var log := new IO.Objects.SimpleIO.syslog.Logger;
+
+    // Test Logger methods
+
+    log.Note('This is a test NOTIFICATION message');
+    log.Warning('This is a test WARNING message');
+    log.Error('This is a test ERROR message');
+    log.Error('This is a test ERROR message', errno.EINVAL);
+    log.Error('This is a test ERROR message', 999);
+
+    // Register trace listener
+
+    System.Diagnostics.Trace.Listeners.Clear();
+    System.Diagnostics.Trace.Listeners.Add(log);
+
+    // Test Trace methods
+
+    System.Diagnostics.Trace.TraceInformation('This is TraceInformation()');
+    System.Diagnostics.Trace.TraceWarning('This is TraceWarning()');
+    System.Diagnostics.Trace.TraceError('This is TraceError()');
+
+    // Test Write()
+
+    System.Diagnostics.Trace.Write('This is Write()');
+    System.Diagnostics.Trace.Write(123456789);
+    System.Diagnostics.Trace.Write('This is Write()', 'ALERT');
+    System.Diagnostics.Trace.Write(123456789, 'INFO');
+
+    // Test WriteIf()
+
+    System.Diagnostics.Trace.WriteIf(true, 'This is WriteIf()');
+    System.Diagnostics.Trace.WriteIf(true, 123456789);
+    System.Diagnostics.Trace.WriteIf(true, 'This is WriteIf()', 'ALERT');
+    System.Diagnostics.Trace.WriteIf(true, 123456789, 'INFO');
+  end;
+
+end.
