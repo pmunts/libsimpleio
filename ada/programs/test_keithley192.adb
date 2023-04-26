3b17d648eea705244988be764f1450b2d8c5555e
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-04-26 04:14:42 +0000

    Added Keithley 192 Programmable DMM Test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@21121 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/programs/test_keithley192.adb
@@ -0,0 +1,53 @@
+-- Keithley 192 Programmable DMM Test
+
+-- Copyright (C)2023, Philip Munts.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+WITH Keithley192;
+WITH Prologix_GPIB_USB;
+
+PROCEDURE test_keithley192 IS
+
+  dev : Keithley192.Device;
+
+BEGIN
+  New_Line;
+  Put_Line("Keithley 192 Programmable DMM Test");
+  New_Line;
+
+  dev.Initialize(Prologix_GPIB_USB.Create("/dev/ttyUSB0"),
+    Keithley192.DefaultAddress);
+
+  dev.Clear;
+  DELAY 1.0;
+
+  dev.Configure(Keithley192.ConfigDCVolts);
+
+  LOOP
+    BEGIN
+      DELAY 1.0;
+      Put_Line(Float'Image(dev.Get));
+    EXCEPTION
+      WHEN Keithley192.OutOfRange => Put_Line("Input is out of range");
+    END;
+  END LOOP;
+END test_keithley192;
