9c19d66c890ea0dd236779525ed53598ff182c42
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-10-26 17:48:06 +0000

    Moved Raspberry Pi compatible device designator definitions to child package OrangePiZero2W.RaspberryPi.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@21345 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/objects/simpleio/orangepizero2w-raspberrypi.ads
@@ -0,0 +1,66 @@
+-- Orange Pi Zero 2W Device Designators for Raspberry Pi Compatibility
+
+-- Copyright (C)2023, Philip Munts dba Munts Technologies.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+-- The Orange Pi Zero 2W 40-pin expansion header has excellent compatibility
+-- with Raspbery Pi boards, with the exception that PWM outputs are not
+-- routed to GPIO18 or GPIO19.
+
+WITH Device;
+
+PACKAGE OrangePiZero2W.RaspberryPi IS
+
+  GPIO2  : Device.Designator RENAMES OrangePiZero2W.GPIO264; -- Pin 3
+  GPIO3  : Device.Designator RENAMES OrangePiZero2W.GPIO263; -- Pin 5
+  GPIO4  : Device.Designator RENAMES OrangePiZero2W.GPIO260; -- Pin 7
+  GPIO5  : Device.Designator RENAMES OrangePiZero2W.GPIO256; -- Pin 29
+  GPIO6  : Device.Designator RENAMES OrangePiZero2W.GPIO268; -- Pin 31
+  GPIO7  : Device.Designator RENAMES OrangePiZero2W.GPIO233; -- Pin 26
+  GPIO8  : Device.Designator RENAMES OrangePiZero2W.GPIO229; -- Pin 24
+  GPIO9  : Device.Designator RENAMES OrangePiZero2W.GPIO232; -- Pin 21
+  GPIO10 : Device.Designator RENAMES OrangePiZero2W.GPIO231; -- Pin 19
+  GPIO11 : Device.Designator RENAMES OrangePiZero2W.GPIO230; -- Pin 23
+  GPIO12 : Device.Designator RENAMES OrangePiZero2W.GPIO267; -- Pin 32
+  GPIO13 : Device.Designator RENAMES OrangePiZero2W.GPIO268; -- Pin 33
+  GPIO14 : Device.Designator RENAMES OrangePiZero2W.GPIO224; -- Pin 8
+  GPIO15 : Device.Designator RENAMES OrangePiZero2W.GPIO225; -- Pin 10
+  GPIO16 : Device.Designator RENAMES OrangePiZero2W.GPIO76;  -- Pin 36
+  GPIO17 : Device.Designator RENAMES OrangePiZero2W.GPIO226; -- Pin 11
+  GPIO18 : Device.Designator RENAMES OrangePiZero2W.GPIO257; -- Pin 12
+  GPIO19 : Device.Designator RENAMES OrangePiZero2W.GPIO258; -- Pin 35
+  GPIO20 : Device.Designator RENAMES OrangePiZero2W.GPIO260; -- Pin 38
+  GPIO21 : Device.Designator RENAMES OrangePiZero2W.GPIO259; -- Pin 40
+  GPIO22 : Device.Designator RENAMES OrangePiZero2W.GPIO261; -- Pin 15
+  GPIO23 : Device.Designator RENAMES OrangePiZero2W.GPIO270; -- Pin 16
+  GPIO24 : Device.Designator RENAMES OrangePiZero2W.GPIO228; -- Pin 18
+  GPIO25 : Device.Designator RENAMES OrangePiZero2W.GPIO262; -- Pin 22
+  GPIO26 : Device.Designator RENAMES OrangePiZero2W.GPIO272; -- Pin 37
+  GPIO27 : Device.Designator RENAMES OrangePiZero2W.GPIO227; -- Pin 13
+
+  I2C1   : Device.Designator RENAMES OrangePiZero2W.I2C1;    -- Pins 3 and 5
+
+  PWM0   : Device.Designator RENAMES OrangePiZero2W.PWM1;    -- Pin 32;
+  PWM1   : Device.Designator RENAMES OrangePiZero2W.PWM2;    -- Pin 33;
+
+  SPI0_0 : Device.Designator RENAMES OrangePiZero2W.SPI1_0;  -- Pin 24;
+  SPI0_1 : Device.Designator RENAMES OrangePiZero2W.SPI1_1;  -- Pin 26;
+
+END OrangePiZero2W.RaspberryPi;
--- ada/objects/simpleio/orangepizero2w.ads
+++ ada/objects/simpleio/orangepizero2w.ads
@@ -1,4 +1,4 @@
--- Orange Pi Zero 2W Device Definitions
+-- Orange Pi Zero 2W Device Designators
 
 -- Copyright (C)2023, Philip Munts dba Munts Technologies.
 --
@@ -24,27 +24,25 @@ WITH Device;
 
 PACKAGE OrangePiZero2W IS
 
-  -- Define GPIO pins on the 40 pin expansion header
-
   GPIO264 : CONSTANT Device.Designator := (0, 264); -- Pin 3  aka I2C1 SDA
   GPIO263 : CONSTANT Device.Designator := (0, 263); -- Pin 5  aka I2C1 SCL
-  GPIO269 : CONSTANT Device.Designator := (0, 269); -- Pin 7  aka PWM3
+  GPIO269 : CONSTANT Device.Designator := (0, 269); -- Pin 7  aka PWM3, UART4 TXD
   GPIO224 : CONSTANT Device.Designator := (0, 224); -- Pin 8  aka UART0 TXD
   GPIO225 : CONSTANT Device.Designator := (0, 225); -- Pin 10 aka UART0 RXD
   GPIO226 : CONSTANT Device.Designator := (0, 226); -- Pin 11 aka UART5 RXD
   GPIO257 : CONSTANT Device.Designator := (0, 257); -- Pin 12
   GPIO227 : CONSTANT Device.Designator := (0, 227); -- Pin 13 aka UART5 RXD
-  GPIO261 : CONSTANT Device.Designator := (0, 261); -- Pin 15 aka UART2 TXD
-  GPIO270 : CONSTANT Device.Designator := (0, 270); -- Pin 16 aka PWM4
+  GPIO261 : CONSTANT Device.Designator := (0, 261); -- Pin 15 aka I2C0 SCL, UART2 TXD
+  GPIO270 : CONSTANT Device.Designator := (0, 270); -- Pin 16 aka PWM4, UART4 RXD
   GPIO228 : CONSTANT Device.Designator := (0, 228); -- Pin 18
   GPIO231 : CONSTANT Device.Designator := (0, 231); -- Pin 19 aka SPI1 MOSI
   GPIO232 : CONSTANT Device.Designator := (0, 232); -- Pin 21 aka SPI1 MISO
-  GPIO262 : CONSTANT Device.Designator := (0, 262); -- Pin 22 aka UART2 TXD
+  GPIO262 : CONSTANT Device.Designator := (0, 262); -- Pin 22 aka I2C0 SDA, UART2 TXD
   GPIO230 : CONSTANT Device.Designator := (0, 230); -- Pin 23 aka SPI1 CLK
   GPIO229 : CONSTANT Device.Designator := (0, 229); -- Pin 24 aka SPI1 SS0
   GPIO233 : CONSTANT Device.Designator := (0, 233); -- Pin 26 aka SPI1 SS1
-  GPIO266 : CONSTANT Device.Designator := (0, 266); -- Pin 27 aka I2C2 SDA
-  GPIO265 : CONSTANT Device.Designator := (0, 265); -- Pin 28 aka I2C2 SCL
+  GPIO266 : CONSTANT Device.Designator := (0, 266); -- Pin 27 aka I2C2 SDA, UART3 RXD
+  GPIO265 : CONSTANT Device.Designator := (0, 265); -- Pin 28 aka I2C2 SCL, UART3 TXD
   GPIO256 : CONSTANT Device.Designator := (0, 256); -- Pin 29
   GPIO271 : CONSTANT Device.Designator := (0, 271); -- Pin 31
   GPIO267 : CONSTANT Device.Designator := (0, 267); -- Pin 32 aka PWM1
@@ -68,34 +66,5 @@ PACKAGE OrangePiZero2W IS
 
   SPI1_0  : CONSTANT Device.Designator := (1, 0);   -- Pin 24
   SPI1_1  : CONSTANT Device.Designator := (1, 1);   -- Pin 26
-
-  -- Define some aliases compatible with the Raspberry Pi Zero et al
-
-  GPIO2   : Device.Designator RENAMES GPIO264;      -- Pin 3
-  GPIO3   : Device.Designator RENAMES GPIO263;      -- Pin 5
-  GPIO4   : Device.Designator RENAMES GPIO260;      -- Pin 7
-  GPIO5   : Device.Designator RENAMES GPIO256;      -- Pin 29
-  GPIO6   : Device.Designator RENAMES GPIO268;      -- Pin 31
-  GPIO7   : Device.Designator RENAMES GPIO233;      -- Pin 26
-  GPIO8   : Device.Designator RENAMES GPIO229;      -- Pin 24
-  GPIO9   : Device.Designator RENAMES GPIO232;      -- Pin 21
-  GPIO10  : Device.Designator RENAMES GPIO231;      -- Pin 19
-  GPIO11  : Device.Designator RENAMES GPIO230;      -- Pin 23
-  GPIO12  : Device.Designator RENAMES GPIO267;      -- Pin 32
-  GPIO13  : Device.Designator RENAMES GPIO268;      -- Pin 33
-  GPIO14  : Device.Designator RENAMES GPIO224;      -- Pin 8
-  GPIO15  : Device.Designator RENAMES GPIO225;      -- Pin 10
-  GPIO16  : Device.Designator RENAMES GPIO76;       -- Pin 36
-  GPIO17  : Device.Designator RENAMES GPIO226;      -- Pin 11
-  GPIO18  : Device.Designator RENAMES GPIO257;      -- Pin 12
-  GPIO19  : Device.Designator RENAMES GPIO258;      -- Pin 35
-  GPIO20  : Device.Designator RENAMES GPIO260;      -- Pin 38
-  GPIO21  : Device.Designator RENAMES GPIO259;      -- Pin 40
-  GPIO22  : Device.Designator RENAMES GPIO261;      -- Pin 15
-  GPIO23  : Device.Designator RENAMES GPIO270;      -- Pin 16
-  GPIO24  : Device.Designator RENAMES GPIO228;      -- Pin 18
-  GPIO25  : Device.Designator RENAMES GPIO262;      -- Pin 22
-  GPIO26  : Device.Designator RENAMES GPIO272;      -- Pin 37
-  GPIO27  : Device.Designator RENAMES GPIO227;      -- Pin 13
 
 END OrangePiZero2W;
