364b428f28d2e17f4189b06670ca8bc7e1762be5
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-10-28 22:45:42 +0000

    Added TH02 I2C temperature/humidity sensor test. A--This line, and those below, will be ignored--
    A    munts-0018/test_th02
    A    munts-0018/test_th02/.vscode
    A    munts-0018/test_th02/.vscode/tasks.json
    A    munts-0018/test_th02/Makefile
    A    munts-0018/test_th02/Program.cs
    A    munts-0018/test_th02/test_th02.csproj
    A    munts-0018/test_th02/test_th02.sln
    
    
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@21373 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ csharp/programs/simpleio/munts-0018/test_th02/Program.cs
@@ -0,0 +1,48 @@
+// MUNTS-0018 Raspberry Pi Tutorial I/O Board TH02 Sensor Test
+
+// Copyright (C)2023, Philip Munts.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using static IO.Objects.SimpleIO.Platforms.MUNTS_0018;
+using static System.Console;
+using static System.Threading.Thread;
+
+WriteLine("\nMUNTS-0018 TH02 Sensor Test\n");
+
+// Create I2C bus object
+
+var bus = I2CBusFactory(J9I2C);
+
+// Create Grove TH02 sensor object
+
+var dev = new IO.Devices.Grove.Temperature_Humidity.Device(bus);
+
+// Display device ID
+
+WriteLine("Device ID: 0x" + dev.DeviceID.ToString("X2"));
+WriteLine();
+
+// Display temperature
+
+for (;;)
+{
+  WriteLine(dev.Celsius.ToString("F1") + "°C  " + dev.Humidity.ToString("F1") + "%");
+  Sleep(1000);
+}
