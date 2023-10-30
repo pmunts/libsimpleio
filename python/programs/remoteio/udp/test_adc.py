2d3d3df52decd7a8f56c95b68d9023720b5d6e5e
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-10-30 03:05:02 +0000

    Updated copyright message.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@21388 06ee2f96-5407-11de-8bab-f3d7589a4122

--- python/programs/remoteio/udp/test_adc.py
+++ python/programs/remoteio/udp/test_adc.py
@@ -2,7 +2,7 @@
 
 # Python Remote I/O Protocol Analog Input Test
 
-# Copyright (C)2021, Philip Munts, President, Munts AM Corp.
+# Copyright (C)2021-2023, Philip Muntsdba Munts Technologies.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
