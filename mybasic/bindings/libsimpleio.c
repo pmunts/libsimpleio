8f5728a8d9f62f66eb99901e740c800bc9eeeaa0
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2023-10-30 03:17:11 +0000

    Updated the copyright message.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@21391 06ee2f96-5407-11de-8bab-f3d7589a4122

--- mybasic/bindings/libsimpleio.c
+++ mybasic/bindings/libsimpleio.c
@@ -1,6 +1,6 @@
 // MY-BASIC bindings for libsimpleio (http://git.munts.com/libsimpleio)
 
-// Copyright (C)2018, Philip Munts, President, Munts AM Corp.
+// Copyright (C)2018-2023, Philip Munts dba Munts Technologies.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
