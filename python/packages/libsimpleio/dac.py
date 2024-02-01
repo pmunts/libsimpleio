ec38c4e6d3781f6467f00f7a4f6af22f1cf94648
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2024-02-01 23:13:22 +0000

    Added support for IIO analog outputs
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@22000 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ python/packages/libsimpleio/dac.py
@@ -0,0 +1,104 @@
+# Analog Output Services
+
+# Copyright (C)2024, Philip Munts dba Munts Technologies
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+__author__	= 'Philip Munts <phil@munts.net>'
+
+import ctypes
+import enum
+import math
+
+from libsimpleio.common import libsimpleio
+
+##############################################################################
+
+# Analog output class
+
+class Output:
+
+  # Constructor
+
+  def __init__(self, designator, resolution, Vref, signed = False):
+    chip    = int(designator[0])
+    channel = int(designator[1])
+    fd      = ctypes.c_int()
+    error   = ctypes.c_int()
+
+    libsimpleio.DAC_open(chip, channel, ctypes.byref(fd), ctypes.byref(error))
+
+    if error.value != 0:
+      raise IOError(error.value, 'DAC_open() failed')
+
+    # Save to private fields
+
+    self.__fd__         = fd.value
+    self.__resolution__ = resolution
+    self.__sample__     = 0
+    self.sample         = 4095
+
+    if signed:
+      self.__stepsize__ = float(Vref)/2.0**(resolution-1)
+    else:
+      self.__stepsize__ = float(Vref)/2.0**(resolution)
+
+  # Raw sample property getter
+
+  @property
+  def sample(self):
+    return self.__sample__
+
+  # Raw sample property setter
+
+  @sample.setter
+  def sample(self, S):
+    sample = ctypes.c_int()
+    error  = ctypes.c_int()
+
+    libsimpleio.DAC_write(self.__fd__, int(S), ctypes.byref(error))
+ 
+    if error.value != 0:
+      raise IOError(error.value, 'DAC_write() failed')
+
+    self.__sample__ = S
+
+  # Voltage property getter
+
+  @property
+  def voltage(self, V):
+    return self.__sample__*self.__stepsize__
+
+  # Voltage property setter
+
+  @voltage.setter
+  def voltage(self, V):
+    self.sample = int(math.floor(float(V)/self.__stepsize__))
+
+  # Resolution property getter
+
+  @property
+  def resolution(self):
+    return self.__resolution__
+
+  # File descriptor property getter
+
+  @property
+  def fd(self):
+    return self.__fd__
---
+++ python/programs/libsimpleio/test_dac.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+
+# Analog Output Test using libsimpleio
+
+# Copyright (C)2024, Philip Munts.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# Tested with MikroElektronika ADAC Click, with an AD5593R and AD5593R.dtbo
+
+import libsimpleio.dac
+import time
+
+from libsimpleio.common import irange
+
+print("\nAnalog Output Test using libsimpleio\n")
+
+outp = libsimpleio.dac.Output((1,0), 12, 2.5)
+
+print("fd         => " + str(outp.fd))
+print("resolution => " + str(outp.resolution) + " bits\n")
+
+while True:
+  for i in irange(0, 2499):
+    outp.voltage = i/1000.0
