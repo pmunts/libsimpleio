9a1f43cce26eb6d5472ea041bbb2e6ed6d200ded
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2024-02-17 04:48:33 +0000

    Added a comment.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@22185 06ee2f96-5407-11de-8bab-f3d7589a4122

--- elements/linux/objects/simpleio/watchdog.pas
+++ elements/linux/objects/simpleio/watchdog.pas
@@ -20,6 +20,8 @@
 { ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
 { POSSIBILITY OF SUCH DAMAGE.                                                 }
 
+{ Watchdog timer services using libsimpleio }
+
 namespace IO.Objects.SimpleIO.Watchdog;
 
 interface
