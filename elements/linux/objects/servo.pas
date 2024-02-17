7af80717f8feefea4034b9732aede9bdcdf4c929
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2024-02-17 05:20:09 +0000

    Added comment
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@22187 06ee2f96-5407-11de-8bab-f3d7589a4122

--- elements/linux/objects/servo.pas
+++ elements/linux/objects/servo.pas
@@ -18,6 +18,8 @@
 { ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  }
 { POSSIBILITY OF SUCH DAMAGE.                                                 }
 
+{ Servo output services using libsimpleio }
+
 namespace IO.Objects.Servo;
 
 interface
