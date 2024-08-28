c7983d21923130ef011ee7eaefebf21a568b6c0b
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2024-08-28 18:06:52 +0000

    Stripped trailing whitespace
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@22449 06ee2f96-5407-11de-8bab-f3d7589a4122

--- csharp/programs/remoteio/win64/dotnet/forms/test_led/MainWindow.Designer.cs
+++ csharp/programs/remoteio/win64/dotnet/forms/test_led/MainWindow.Designer.cs
@@ -30,18 +30,18 @@ namespace test_led
         {
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
+            //
             // button1
-            // 
+            //
             this.button1.Location = new System.Drawing.Point(140, 88);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 24);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            // 
+            //
             // MainWindow
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(400, 200);
