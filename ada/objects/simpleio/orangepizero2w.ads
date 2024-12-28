73e6fcd86e1471ccb6fd1cc2992cf7af90874026
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2024-12-28 21:59:59 +0000

    Folded package OrangePiZero2W.RaspberryPi into OrangePiZero2W.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@22617 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/objects/simpleio/orangepizero2w.ads
+++ ada/objects/simpleio/orangepizero2w.ads
@@ -24,6 +24,8 @@ WITH Device;
 
 PACKAGE OrangePiZero2W IS
 
+  ModelName : CONSTANT String := "OrangePi Zero2 W"; -- From /proc/device-tree/model
+
   PI8  : CONSTANT Device.Designator := (0, 264);  -- Pin 3  I2C1 SDA
   PI7  : CONSTANT Device.Designator := (0, 263);  -- Pin 5  I2C1 SCL
   PI13 : CONSTANT Device.Designator := (0, 269);  -- Pin 7  PWM3
@@ -55,34 +57,65 @@ PACKAGE OrangePiZero2W IS
 
   -- GPIO pin number aliases
 
-  GPIO264 : Device.Designator RENAMES PI8;        -- Pin 3  I2C1 SDA
-  GPIO263 : Device.Designator RENAMES PI7;        -- Pin 5  I2C1 SCL
-  GPIO269 : Device.Designator RENAMES PI13;       -- Pin 7  PWM3
-  GPIO226 : Device.Designator RENAMES PH2;        -- Pin 11
-  GPIO227 : Device.Designator RENAMES PH3;        -- Pin 13
-  GPIO261 : Device.Designator RENAMES PI5;        -- Pin 15
-  GPIO231 : Device.Designator RENAMES PH7;        -- Pin 19 SPI1 MOSI
-  GPIO232 : Device.Designator RENAMES PH8;        -- Pin 21 SPI1 MISO
-  GPIO230 : Device.Designator RENAMES PH6;        -- Pin 23 SPI1 CLK
-  GPIO266 : Device.Designator RENAMES PI10;       -- Pin 27 I2C2 SDA
-  GPIO256 : Device.Designator RENAMES PI0;        -- Pin 29
-  GPIO271 : Device.Designator RENAMES PI15;       -- Pin 31
-  GPIO268 : Device.Designator RENAMES PI12;       -- Pin 33 PWM2
-  GPIO258 : Device.Designator RENAMES PI2;        -- Pin 35
-  GPIO272 : Device.Designator RENAMES PI16;       -- Pin 37
+  GPIO76  : Device.Designator RENAMES PC12;       -- Pin 36
   GPIO224 : Device.Designator RENAMES PH0;        -- Pin 8  TXD0
   GPIO225 : Device.Designator RENAMES PH1;        -- Pin 10 RXD0
-  GPIO257 : Device.Designator RENAMES PI1;        -- Pin 12
-  GPIO270 : Device.Designator RENAMES PI14;       -- Pin 16 PWM4
+  GPIO226 : Device.Designator RENAMES PH2;        -- Pin 11
+  GPIO227 : Device.Designator RENAMES PH3;        -- Pin 13
   GPIO228 : Device.Designator RENAMES PH4;        -- Pin 18
-  GPIO262 : Device.Designator RENAMES PI6;        -- Pin 22
   GPIO229 : Device.Designator RENAMES PH5;        -- Pin 24 SPI1 SS0
+  GPIO230 : Device.Designator RENAMES PH6;        -- Pin 23 SPI1 CLK
+  GPIO231 : Device.Designator RENAMES PH7;        -- Pin 19 SPI1 MOSI
+  GPIO232 : Device.Designator RENAMES PH8;        -- Pin 21 SPI1 MISO
   GPIO233 : Device.Designator RENAMES PH9;        -- Pin 26 SPI1 SS1
-  GPIO265 : Device.Designator RENAMES PI9;        -- Pin 28 I2C2 SCL
-  GPIO267 : Device.Designator RENAMES PI11;       -- Pin 32 PWM1
-  GPIO76  : Device.Designator RENAMES PC12;       -- Pin 36
-  GPIO260 : Device.Designator RENAMES PI4;        -- Pin 38
+  GPIO256 : Device.Designator RENAMES PI0;        -- Pin 29
+  GPIO257 : Device.Designator RENAMES PI1;        -- Pin 12
+  GPIO258 : Device.Designator RENAMES PI2;        -- Pin 35
   GPIO259 : Device.Designator RENAMES PI3;        -- Pin 40
+  GPIO260 : Device.Designator RENAMES PI4;        -- Pin 38
+  GPIO261 : Device.Designator RENAMES PI5;        -- Pin 15
+  GPIO262 : Device.Designator RENAMES PI6;        -- Pin 22
+  GPIO263 : Device.Designator RENAMES PI7;        -- Pin 5  I2C1 SCL
+  GPIO264 : Device.Designator RENAMES PI8;        -- Pin 3  I2C1 SDA
+  GPIO265 : Device.Designator RENAMES PI9;        -- Pin 28 I2C2 SCL
+  GPIO266 : Device.Designator RENAMES PI10;       -- Pin 27 I2C2 SDA
+  GPIO267 : Device.Designator RENAMES PI11;       -- Pin 32 PWM1
+  GPIO268 : Device.Designator RENAMES PI12;       -- Pin 33 PWM2
+  GPIO269 : Device.Designator RENAMES PI13;       -- Pin 7  PWM3
+  GPIO270 : Device.Designator RENAMES PI14;       -- Pin 16 PWM4
+  GPIO271 : Device.Designator RENAMES PI15;       -- Pin 31
+  GPIO272 : Device.Designator RENAMES PI16;       -- Pin 37
+
+  -- Raspberry Pi compatibility GPIO pin number aliases
+
+  GPIO0   : Device.Designator RENAMES PI10;       -- Pin 27 I2C2 SDA
+  GPIO1   : Device.Designator RENAMES PI9;        -- Pin 28 I2C2 SCL
+  GPIO2   : Device.Designator RENAMES PI8;        -- Pin 3  I2C1 SDA
+  GPIO3   : Device.Designator RENAMES PI7;        -- Pin 5  I2C1 SCL
+  GPIO4   : Device.Designator RENAMES PI13;       -- Pin 7  PWM3
+  GPIO5   : Device.Designator RENAMES PI0;        -- Pin 29
+  GPIO6   : Device.Designator RENAMES PI15;       -- Pin 31
+  GPIO7   : Device.Designator RENAMES PH9;        -- Pin 26 SPI1 SS1
+  GPIO8   : Device.Designator RENAMES PH5;        -- Pin 24 SPI1 SS0
+  GPIO9   : Device.Designator RENAMES PH8;        -- Pin 21 SPI1 MISO
+  GPIO10  : Device.Designator RENAMES PH7;        -- Pin 19 SPI1 MOSI
+  GPIO11  : Device.Designator RENAMES PH6;        -- Pin 23 SPI1 CLK
+  GPIO12  : Device.Designator RENAMES PI11;       -- Pin 32 PWM1
+  GPIO13  : Device.Designator RENAMES PI12;       -- Pin 33 PWM2
+  GPIO14  : Device.Designator RENAMES PH0;        -- Pin 8  TXD0
+  GPIO15  : Device.Designator RENAMES PH1;        -- Pin 10 RXD0
+  GPIO16  : Device.Designator RENAMES PC12;       -- Pin 36
+  GPIO17  : Device.Designator RENAMES PH2;        -- Pin 11
+  GPIO18  : Device.Designator RENAMES PI1;        -- Pin 12
+  GPIO19  : Device.Designator RENAMES PI2;        -- Pin 35
+  GPIO20  : Device.Designator RENAMES PI4;        -- Pin 38
+  GPIO21  : Device.Designator RENAMES PI3;        -- Pin 40
+  GPIO22  : Device.Designator RENAMES PI5;        -- Pin 15
+  GPIO23  : Device.Designator RENAMES PI14;       -- Pin 16 PWM4
+  GPIO24  : Device.Designator RENAMES PH4;        -- Pin 18
+  GPIO25  : Device.Designator RENAMES PI6;        -- Pin 22
+  GPIO26  : Device.Designator RENAMES PI16;       -- Pin 37
+  GPIO27  : Device.Designator RENAMES PH3;        -- Pin 13
 
   I2C1    : CONSTANT Device.Designator := (0, 1); -- Pins 3  and 5
   I2C2    : CONSTANT Device.Designator := (0, 2); -- Pins 27 and 28
