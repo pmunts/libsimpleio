864c8d5033cf0ce64d6316879227e94352060c1a
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-01-20 20:20:33 +0000

    Resurrected test_query
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@22741 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ csharp/programs/remoteio/test_query/Program.cs
@@ -0,0 +1,96 @@
+// Remote I/O Device Information Query Test
+
+// Copyright (C)2018-2025, Philip Munts dba Munts Technologies.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+
+namespace test_query
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\nRemote I/O Device Information Query Test\n");
+
+            var remdev = new IO.Objects.RemoteIO.Device(null);
+
+            // Display some device information
+
+            Console.WriteLine(remdev.Version);
+            Console.WriteLine(remdev.Capabilities);
+            Console.WriteLine();
+
+            // Display the available ADC inputs
+
+            Console.Write("ADC inputs:  ");
+
+            foreach (int input in remdev.ADC_Available())
+                Console.Write(input.ToString() + " ");
+
+            Console.WriteLine();
+
+            // Display the available DAC outputs
+
+            Console.Write("DAC outputs: ");
+
+            foreach (int output in remdev.DAC_Available())
+                Console.Write(output.ToString() + " ");
+
+            Console.WriteLine();
+
+            // Display the available GPIO pins
+
+            Console.Write("GPIO Pins:   ");
+
+            foreach (int pin in remdev.GPIO_Available())
+                Console.Write(pin.ToString() + " ");
+
+            Console.WriteLine();
+
+            // Display the available I2C buses
+
+            Console.Write("I2C buses:   ");
+
+            foreach (int bus in remdev.I2C_Available())
+                Console.Write(bus.ToString() + " ");
+
+            Console.WriteLine();
+
+            // Display the available PWM outputs
+
+            Console.Write("PWM outputs: ");
+
+            foreach (int bus in remdev.PWM_Available())
+                Console.Write(bus.ToString() + " ");
+
+            Console.WriteLine();
+
+            // Display the available SPI devices
+
+            Console.Write("SPI devices: ");
+
+            foreach (int bus in remdev.SPI_Available())
+                Console.Write(bus.ToString() + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
