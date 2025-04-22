21772396726aaf5aaf74193f0dafbde6b9948255
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-04-22 03:01:58 +0000

    Addes spew tests
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@23153 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/programs/wio-e5/test_spew_rx.adb
@@ -0,0 +1,53 @@
+-- Wio-E5 LoRa Transceiver Receive Test
+
+-- Copyright (C)2025, Philip Munts dba Munts Technologies.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+WITH Wio_E5.Ham1;
+
+PROCEDURE test_spew_rx IS
+
+  PACKAGE LoRa IS NEW Wio_E5.Ham1(64); USE LoRa;
+
+  dev  : Device := Create("/dev/ttyAMA0", 115200, 915.0, "N7AHL   ", 66);
+  msg  : Packet;
+  len  : Natural;
+  src  : Wio_E5.Byte;
+  dst  : Wio_E5.Byte;
+  RSSI : Integer;
+  SNR  : Integer;
+
+BEGIN
+  New_Line;
+  Put_Line("Wio-E5 LoRa Transceiver Receive Test");
+  New_Line;
+
+  LOOP
+    dev.Receive(msg, len, src, dst, RSSI, SNR);
+
+    IF len > 0 THEN
+      Put_Line("Received => """ & ToString(msg, len) & """ from node" &
+        src'Image & " to node" & dst'Image & " RSSI: " & RSSI'Image &
+        " dBm SNR: " & SNR'Image & " dB");
+    END IF;
+  END LOOP;
+END test_spew_rx;
