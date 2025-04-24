04561b3a483ab56e75b515fa169f79aa8a97a392
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-04-24 04:06:07 +0000

    Added Alire sample projects
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@23170 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/programs/wio-e5/alire/test_signal_level_rx/src/test_signal_level_rx.adb
@@ -0,0 +1,65 @@
+-- Wio-E5 LoRa Transceiver Signal Level Test Responder
+
+-- Copyright (C)2025, Philip Munts dba Munts Technologies.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH libLinux;
+WITH Watchdog.libsimpleio;
+WITH Wio_E5.Ham1;
+
+PROCEDURE test_signal_level_rx IS
+
+  PACKAGE LoRa IS NEW Wio_E5.Ham1; USE LoRa;
+
+  err : Integer;
+  wd  : Watchdog.Timer;
+  dev : Device;
+  msg : Frame;
+  len : Natural;
+  src : Wio_E5.Byte;
+  dst : Wio_E5.Byte;
+  RSS : Integer;
+  SNR : Integer;
+
+BEGIN
+  libLinux.Detach(err);
+
+  -- Create a watchdog timer device object
+
+  wd := Watchdog.libsimpleio.Create;
+
+  -- Change the watchdog timeout period to 5 seconds
+
+  wd.SetTimeout(5.0);
+
+  -- Create a LoRa transceiver object
+
+  dev :=  Create("/dev/ttyAMA0", 115200, 915.0, "N7AHL   ", 2);
+
+  LOOP
+    dev.Receive(msg, len, src, dst, RSS, SNR);
+
+    IF len > 0 THEN
+      dev.Send("LEN:" & len'Image & " bytes RSS: " & RSS'Image & " dBm SNR: " & SNR'Image & " dB", src);
+    END IF;
+
+    wd.Kick;
+  END LOOP;
+END test_signal_level_rx;
