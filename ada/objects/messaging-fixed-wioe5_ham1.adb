844a95940a522c4e04cb63b4e11f5378219a51b9
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-07-04 17:18:53 +0000

    Started working on Message64 over Wio_E5.Ham1
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@23447 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/objects/messaging-fixed-wioe5_ham1.adb
@@ -0,0 +1,83 @@
+-- Fixed length message services using WioE5 Ham Radio Protocol #1
+--
+-- Must be instantiated for each message size.
+
+-- Copyright (C)2025, Philip Munts dba Munts Technologies.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Ada.Unchecked_Conversion;
+
+PACKAGE BODY Messaging.Fixed.WioE5_Ham1 IS
+
+  FUNCTION ToPayload IS NEW Ada.Unchecked_Conversion(Message, driver.Payload);
+
+  FUNCTION ToMessage IS NEW Ada.Unchecked_Conversion(driver.Payload, Message);
+
+  -- Constructor
+
+  FUNCTION Create
+   (dev  : driver.Device;
+    node : driver.NodeID) RETURN Messenger IS
+
+  BEGIN
+    RETURN NEW MessengerSubclass'(dev, node);
+  END Create;
+
+  -- Send a message
+
+  PROCEDURE Send(Self : MessengerSubclass; msg : Message) IS
+
+    len   : Natural;
+
+  BEGIN
+    len := msg'Length;
+
+    -- Optimize bandwidth by not sending trailing zeros.  For short
+    -- messages, this will greatly reduce channel bandwidth
+
+    WHILE (len > 0) AND (msg(len - 1) = 0) LOOP
+      len := len - 1;
+    END LOOP;
+
+    Self.mydev.Send(ToPayload(msg), len, Self.mynode);
+  END Send;
+
+  -- Receive a message
+
+  PROCEDURE Receive(Self : MessengerSubclass; msg : OUT Message) IS
+
+    pay : driver.Payload := (OTHERS => 0);
+    len : Natural;
+    src : driver.NodeID;
+    dst : driver.NodeID;
+    RSS : Integer;
+    SNR : Integer;
+
+  BEGIN
+    LOOP
+      Self.mydev.Receive(pay, len, src, dst, RSS, SNR);
+      EXIT WHEN len > 0;
+      DELAY 0.1;
+    END LOOP;
+
+    msg := ToMessage(pay);
+  END Receive;
+
+END Messaging.Fixed.WioE5_Ham1;
---
+++ ada/objects/messaging-fixed-wioe5_ham1.ads
@@ -0,0 +1,62 @@
+-- Fixed length message services using WioE5 Ham Radio Protocol #1
+--
+-- Must be instantiated for each message size.
+
+-- Copyright (C)2025, Philip Munts dba Munts Technologies.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+WITH Wio_E5.Ham1;
+
+GENERIC
+
+PACKAGE Messaging.Fixed.WioE5_Ham1 IS
+
+  PACKAGE driver IS NEW Wio_E5.Ham1(Message'Length);
+
+  -- Type definitions
+
+  TYPE MessengerSubclass IS NEW MessengerInterface WITH PRIVATE;
+
+  -- Constructor
+
+  FUNCTION Create
+   (dev  : driver.Device;
+    node : driver.NodeID) RETURN Messenger;
+
+  -- Send a message
+
+  PROCEDURE Send
+   (Self : MessengerSubclass;
+    msg  : Message);
+
+  -- Receive a message
+
+  PROCEDURE Receive
+   (Self : MessengerSubclass;
+    msg  : OUT Message);
+
+PRIVATE
+
+  TYPE MessengerSubclass IS NEW MessengerInterface WITH RECORD
+    mydev  : driver.Device;
+    mynode : driver.NodeID;
+  END RECORD;
+
+END Messaging.Fixed.WioE5_Ham1;
