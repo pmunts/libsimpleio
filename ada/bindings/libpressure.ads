ad017650ed0c9f0465b9869ccaef4302344ee395
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-09-13 21:33:28 +0000

    Added libHUMID and libPRESSURE
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@23752 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ ada/bindings/libpressure.ads
@@ -0,0 +1,58 @@
+-- Minimal Ada wrapper for the Linux Industrial I/O pressor sensor services
+-- implemented in libsimpleio.so
+
+-- Copyright (C)2025, Philip Munts dba Munts Technologies.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- * Redistributions of source code must retain the above copyright notice,
+--   this list of conditions and the following disclaimer.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+
+PACKAGE libPRESSURE IS
+  PRAGMA Link_With("-lsimpleio");
+
+  PROCEDURE GetName
+   (chip    : Integer;
+    name    : OUT String;
+    size    : Integer;
+    error   : OUT Integer);
+  PRAGMA Import(C, GetName, "PRESSURE_get_name");
+
+  PROCEDURE GetScale
+   (chip    : Integer;
+    channel : Integer;
+    scale   : OUT Long_Float;
+    error   : OUT Integer);
+  PRAGMA Import(C, GetScale, "PRESSURE_get_scale");
+
+  PROCEDURE Open
+   (chip    : Integer;
+    channel : Integer;
+    fd      : OUT Integer;
+    error   : OUT Integer);
+  PRAGMA Import(C, Open, "PRESSURE_open");
+
+  PROCEDURE Close
+   (fd      : Integer;
+    error   : OUT Integer);
+  PRAGMA Import(C, Close, "PRESSURE_close");
+
+  PROCEDURE Read
+   (fd      : Integer;
+    sample  : OUT Integer;
+    error   : OUT Integer);
+  PRAGMA Import(C, Read, "PRESSURE_read");
+END libPRESSURE;
