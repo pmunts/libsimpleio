c65da3cb32884ef1d24e6e7a4b5a4c470ef6c2a3
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-09-25 04:44:46 +0000

    Rename package libHUMID to libHumidity
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@23801 06ee2f96-5407-11de-8bab-f3d7589a4122

--- ada/bindings/libhumidity.ads
+++ ada/bindings/libhumidity.ads
@@ -21,7 +21,7 @@
 -- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
-PACKAGE libHUMID IS
+PACKAGE libHumidity IS
   PRAGMA Link_With("-lsimpleio");
 
   PROCEDURE GetName
@@ -55,4 +55,4 @@ PACKAGE libHUMID IS
     sample  : OUT Integer;
     error   : OUT Integer);
   PRAGMA Import(C, Read, "HUMID_read");
-END libHUMID;
+END libHumidity;
