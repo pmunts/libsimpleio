e0028c35b0a7dc3df6e5cd281d5ff40a1dd3f15e
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-10-18 16:17:17 +0000

    Reordered includes
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@23857 06ee2f96-5407-11de-8bab-f3d7589a4122

--- c++/linx-server/common.h
+++ c++/linx-server/common.h
@@ -1,6 +1,6 @@
 // Labview LINX Common Commands module
 
-// Copyright (C)2016-2023, Philip Munts dba Munts Technologies.
+// Copyright (C)2016-2025, Philip Munts dba Munts Technologies.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -24,10 +24,10 @@
 #define _COMMON_H_
 
 #include <errno.h>
-#include <libsimpleio/liblinx.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libsimpleio/liblinx.h>
 #ifdef __linux__
 #include <syslog.h>
 #endif
