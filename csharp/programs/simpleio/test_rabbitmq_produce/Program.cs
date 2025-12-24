58a4b054a7ad45acce9fa5e3bdcbf42480af9879
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2025-12-24 17:36:19 +0000

    Added test_rabbitmq_produce
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@24128 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ csharp/programs/simpleio/test_rabbitmq_produce/Program.cs
@@ -0,0 +1,57 @@
+// RabbitMQ Producer Test
+
+// Copyright (C)2025, Philip Munts dba Munts Technologies.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+using RabbitMQ.Client;
+using System.Text;
+using static System.Console;
+using static System.Environment;
+
+if (args.Length != 1)
+{
+  WriteLine("\nUsage: test_rabbitmq_produce <text message>\n");
+  Exit(1);
+}
+
+string GetEnv(string name, string default_value)
+{
+  var v = GetEnvironmentVariable(name);
+  return v is null ? default_value : v;
+}
+
+// Configure RabbitMQ.Client
+
+var factory         = new RabbitMQ.Client.ConnectionFactory();
+factory.UserName    = GetEnv("RABBITMQ_USER",     "guest");
+factory.Password    = GetEnv("RABBITMQ_PASS",     "guest");
+factory.HostName    = GetEnv("RABBITMQ_SERVER",   "localhost");
+factory.Port        = int.Parse(GetEnv("RABBITMQ_PORT", "5672"));
+factory.VirtualHost = GetEnv("RABBITMQ_VHOST",    "/");
+var exchange        = GetEnv("RABBITMQ_EXCHANGE", "amq.fanout");
+
+// Connect to the RabbitMQ server.
+
+var connection = await factory.CreateConnectionAsync();
+var channel    = await connection.CreateChannelAsync();
+
+// Send a message to the exchange
+
+await channel.BasicPublishAsync(exchange, "", Encoding.UTF8.GetBytes(args[0]));
