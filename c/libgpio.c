13a0fe8e64eca2dfe80540df726ff2760b957ae3
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-01-25 08:15:14 +0000

    Updated copyright year.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@24220 06ee2f96-5407-11de-8bab-f3d7589a4122

--- c/libgpio.c
+++ c/libgpio.c
@@ -1,6 +1,6 @@
 /* GPIO services for Linux */
 
-// Copyright (C)2016-2025, Philip Munts dba Munts Technologies.
+// Copyright (C)2016-2026, Philip Munts dba Munts Technologies.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
