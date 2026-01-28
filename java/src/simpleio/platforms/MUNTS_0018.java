20faffc02fb8a7bb4fdc32e59b4a3963257e3c8c
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-01-28 18:20:25 +0000

    Added more hardware platform classes.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@24275 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ java/src/simpleio/platforms/MUNTS_0018.java
@@ -0,0 +1,101 @@
+// Copyright (C)2026, Philip Munts dba Munts Technologies.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+// Each resource designator constant declared here is named relative to the
+// connector providing that resource.  Application programs should use the
+// designator constants declared here instead of literals or constants from
+// the RaspberryPi package.
+//
+// The following device tree overlay statements must be added to
+// /boot/config.txt:
+//
+// dtparam=i2c=on
+// dtparam=spi=on
+// dtoverlay=anyspi,spi0-1,dev="microchip,mcp3204",speed=1000000
+// dtoverlay=pwm-2chan,pin=12,func=4,pin2=19,func2=2
+//
+// GPIO12 and GPIO19 will be unavailable for GPIO, as they will be mapped
+// as PWM outputs.  If you need to use these pins for GPIO, omit the pwm-2chan
+// device tree overlay.
+
+package com.munts.libsimpleio.platforms;
+
+import com.munts.libsimpleio.objects.Designator;
+import com.munts.libsimpleio.platforms.RaspberryPi;
+
+public final class MUNTS_0018
+{
+  private MUNTS_0018()
+  {
+  }
+
+  // Servo headers
+
+  public static final Designator J2PWM  = RaspberryPi.PWM0;
+  public static final Designator J3PWM  = RaspberryPi.PWM1;
+
+  // Grove GPIO Connectors
+
+  public static final Designator J4D0   = RaspberryPi.GPIO23;
+  public static final Designator J4D1   = RaspberryPi.GPIO24;
+
+  public static final Designator J5D0   = RaspberryPi.GPIO5;
+  public static final Designator J5D1   = RaspberryPi.GPIO4;
+
+  public static final Designator J6D0   = RaspberryPi.GPIO12;
+  public static final Designator J6D1   = RaspberryPi.GPIO13;
+
+  public static final Designator J7D0   = RaspberryPi.GPIO19;
+  public static final Designator J7D1   = RaspberryPi.GPIO18;
+
+  // DC Motor control outputs
+
+  public static final Designator J6PWM  = RaspberryPi.PWM0;
+  public static final Designator J6DIR  = J6D1;
+
+  public static final Designator J7PWM  = RaspberryPi.PWM1;
+  public static final Designator J7DIR  = J7D1;
+
+  // Serial ports
+
+  public static final String     J8UART = "/dev/ttyAMA0";
+
+  // I2C buses
+
+  public static final Designator J9I2C  = RaspberryPi.I2C1;
+
+  // Grove ADC Connector J10
+
+  public static final Designator J10A0  = RaspberryPi.AIN2; // MCP3204 CH2
+  public static final Designator J10A1  = RaspberryPi.AIN3; // MCP3204 CH3
+
+  // Grove ADC Connector J11
+
+  public static final Designator J11A0  = RaspberryPi.AIN0; // MCP3204 CH0
+  public static final Designator J11A1  = RaspberryPi.AIN1; // MCP3204 CH1
+
+  // On board momentary switch
+
+  public static final Designator SW1    = RaspberryPi.GPIO6;
+
+  // On board LED indicator
+
+  public static final Designator D1     = RaspberryPi.GPIO26;
+}
---
+++ java/src/simpleio/platforms/RaspberryPi4.java
@@ -0,0 +1,88 @@
+// Copyright (C)2026, Philip Munts dba Munts Technologies.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package com.munts.libsimpleio.platforms;
+
+import com.munts.libsimpleio.objects.Designator;
+
+public final class RaspberryPi4
+{
+  private RaspberryPi4()
+  {
+  }
+
+  // Raspberry Pi boards don't have a built-in ADC (Analog to Digital
+  // Converter) subsystem, so the following analog input designators are
+  // placeholders for the first IIO (Industrial I/O) ADC device, which is
+  // often on a HAT like the Pi 3 Click Shield or an expansion board like
+  // the MUNTS-0018 Raspberry Pi Tutorial I/O Board.
+
+  public static final Designator AIN0   = new Designator(0,  0);
+  public static final Designator AIN1   = new Designator(0,  1);
+  public static final Designator AIN2   = new Designator(0,  2);
+  public static final Designator AIN3   = new Designator(0,  3);
+  public static final Designator AIN4   = new Designator(0,  4);
+  public static final Designator AIN5   = new Designator(0,  5);
+  public static final Designator AIN6   = new Designator(0,  6);
+  public static final Designator AIN7   = new Designator(0,  7);
+
+  public static final Designator GPIO2  = new Designator(0,  2);  // Pin 3  I2C1 SDA
+  public static final Designator GPIO3  = new Designator(0,  3);  // Pin 5  I2C1 SCL
+  public static final Designator GPIO4  = new Designator(0,  4);  // Pin 7
+  public static final Designator GPIO5  = new Designator(0,  5);  // Pin 29
+  public static final Designator GPIO6  = new Designator(0,  6);  // Pin 30
+  public static final Designator GPIO7  = new Designator(0,  7);  // Pin 26 SPI0 SS1
+  public static final Designator GPIO8  = new Designator(0,  8);  // Pin 24 SPI0 SS0
+  public static final Designator GPIO9  = new Designator(0,  9);  // Pin 21 SPI0 MISO
+  public static final Designator GPIO10 = new Designator(0, 10);  // Pin 19 SPI0 MOSI
+  public static final Designator GPIO11 = new Designator(0, 11);  // Pin 23 SPI0 SCLK
+  public static final Designator GPIO12 = new Designator(0, 12);  // Pin 32 PWM0
+  public static final Designator GPIO13 = new Designator(0, 13);  // Pin 33 PWM1
+  public static final Designator GPIO14 = new Designator(0, 14);  // Pin 8  UART0 TXD
+  public static final Designator GPIO15 = new Designator(0, 15);  // Pin 10 UART0 RXD
+  public static final Designator GPIO16 = new Designator(0, 16);  // Pin 36 SPI1 SS2
+  public static final Designator GPIO17 = new Designator(0, 17);  // Pin 11 SPI1 SS1
+  public static final Designator GPIO18 = new Designator(0, 18);  // Pin 12 PWM0 SPI1 SS0
+  public static final Designator GPIO19 = new Designator(0, 19);  // Pin 35 PWM1 SPI1 MISO
+  public static final Designator GPIO20 = new Designator(0, 20);  // Pin 38 SPI1 MOSI
+  public static final Designator GPIO21 = new Designator(0, 21);  // Pin 40 SPI1 SCLK
+  public static final Designator GPIO22 = new Designator(0, 22);  // Pin 15
+  public static final Designator GPIO23 = new Designator(0, 23);  // Pin 16
+  public static final Designator GPIO24 = new Designator(0, 24);  // Pin 18
+  public static final Designator GPIO25 = new Designator(0, 25);  // Pin 22
+  public static final Designator GPIO26 = new Designator(0, 26);  // Pin 37
+  public static final Designator GPIO27 = new Designator(0, 27);  // Pin 13
+
+  // The Raspberry Pi 4 has additional I2C bus controllers, which can be
+  // enabled by device tree overlays i2c3, i2c4, i2c5, or i2c6.
+
+  public static final Designator I2C1   = new Designator(0,  1);  // GPIO2/GPIO3
+  public static final Designator I2C3   = new Designator(0,  3);  // GPIO2/GPIO3   or GPIO4/GPIO5
+  public static final Designator I2C4   = new Designator(0,  4);  // GPIO6/GPIO7   or GPIO8/GPIO9
+  public static final Designator I2C5   = new Designator(0,  5);  // GPIO10/GPIO11 or GPIO12/GPIO13
+  public static final Designator I2C6   = new Designator(0,  6);  // GPIO0/GPIO1   or GPIO22/GPIO23
+
+  public static final Designator PWM0   = new Designator(0,  0);  // GPIO12 or GPIO18
+  public static final Designator PWM1   = new Designator(0,  1);  // GPIO13 or GPIO19
+
+  public static final Designator SPI1_0 = new Designator(1,  0);  // GPIO18/GPIO19/GPIO20/GPIO21
+  public static final Designator SPI1_1 = new Designator(1,  1);  // GPIO17/GPIO19/GPIO20/GPIO21
+  public static final Designator SPI1_2 = new Designator(1,  2);  // GPIO16/GPIO19/GPIO20/GPIO21
+}
