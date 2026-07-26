de18ddf85f1398b230feda490094c3d5a0f8f779
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2026-07-26 00:05:16 +0000

    Started working on Python 3 Remote I/O Protocol client library.
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/libsimpleio@25108 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ python/packages/munts/remoteio/zeromq.py
@@ -0,0 +1,85 @@
+# Copyright (C)2026, Philip Munts dba Munts Technologies.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+__author__	= "Philip Munts <phil@munts.net>"
+
+import zmq
+
+import munts.interfaces.remoteio
+
+class Server(munts.interfaces.remoteio.ServerInterface):
+    def __init__(self, hostname, port):
+        self.__context__ = zmq.Context()
+        self.__socket__  = self.__context__.socket(zmq.REQ)
+        self.__socket__.connect("tcp://" + hostname + ":" + str(port))
+        self.__seq__ = 0
+        cmd  = bytearray(64)
+        
+        # Fetch server version string
+    
+        cmd[0] = 2
+        resp = self.transaction(cmd)
+        self.__version__ = resp[3:].decode("utf-8").strip("\0")
+
+        # Fetch server capability string
+
+        cmd[0] = 4
+        resp = self.transaction(cmd)
+        self.__capability__ = resp[3:].decode("utf-8").strip("\0")
+
+    # Execute a Remote I/O Protocol transaction
+
+    def transaction(self, cmd, timeout = 1000):
+        self.__seq__ = (self.__seq__ + 113) % 256
+        cmd[1] = self.__seq__
+        
+        self.__socket__.send(cmd)
+
+        # Poll for received data available.
+
+        if self.__socket__.poll(timeout, zmq.POLLIN) == 0:
+            raise IOError("poll() timed out")
+        
+        resp = self.__socket__.recv()
+
+        # Check for errors
+        
+        if resp[0] != cmd[0] + 1:
+            raise IOError("Message type mismatch")
+
+        if resp[1] != cmd[1]:
+            raise IOError("Message number mismatch")
+
+        if resp[2] != 0:
+            raise IOError("Remote I/O operation failed, error " + str(resp[2]))
+
+        return resp
+
+    # version string getter
+
+    @property
+    def version(self):
+        return self.__version__
+
+    # capability string getter
+
+    @property
+    def capability(self):
+        return self.__capability__
